134593749d0d2fcd4920c1fd193c629258f2e804
Henry Lin <henry.lin@appdirect.com>
2019-02-16 07:03:51 -0800

    one away strings

---
+++ Strings/1_away_strings.py
@@ -0,0 +1,8 @@
+# One Away Strings: https://www.udemy.com/11-essential-coding-interview-questions/learn/v4/t/quiz/379168
+# Write a function that takes 2 strings and returns True if they are one away from each other
+# There are one away from each other if a single operation (changing a character, deleting a character
+# or adding a character) will change one of the strings to the other
+# Ex: "abcde" and "abcd" are one away (delteing a character)
+# "a" and "a" are one away (changing the only character 'a' to the equivalent character 'a').
+# "abc" and "bcc" are NOT one away. (2 operations away)
+
