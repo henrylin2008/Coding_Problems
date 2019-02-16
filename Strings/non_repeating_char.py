e5e42675c146bf60753416424e26af01800ca33e
Henry Lin <henry.lin@appdirect.com>
2019-02-16 06:59:06 -0800

    NRC

--- Strings/non_repeating_char.py
+++ Strings/non_repeating_char.py
@@ -1,9 +1,9 @@
+# Non-Repeating Character: https://www.udemy.com/11-essential-coding-interview-questions/learn/v4/t/quiz/380026
 # Implement a function that takes a string and returns the first character
 # that does not appear twice or more. 
 # Ex: "abacc" --> 'b'
 #     "xxyzx" ---> 'y' (first non-repeating character)
-# If there is no non-repeating character, return None. 
-def non_repeating(given_string):
+# If there is no non-repeating character, return None. def non_repeating(given_string):
     char_count = {}
     for c in given_string:
         if c in char_count:
