e1770e6215e1fd1ebbafbfec77c0f0a6b14c483a
Henry Lin <henry.lin@appdirect.com>
2019-02-17 08:13:21 -0800

    non-repeating

--- Strings/non_repeating_char.py
+++ Strings/non_repeating_char.py
@@ -5,16 +5,16 @@
 #     "xxyzx" ---> 'y' (first non-repeating character)
 # If there is no non-repeating character, return None. def non_repeating(given_string):
 def non_repeating(given_string):
-    char_count = {}
+    char_count = {} #Dictionary character count
     for c in given_string:
-        if c in char_count:
+        if c in char_count: # If c is already in char_count, increment c by 1, c += 1; ex, c: 2
             char_count[c] += 1
-        else:
+        else:  # Otherwise, first occurrence = 1, 
             char_count[c] = 1
-    for c in given_string:
+    for c in given_string: # Go through the string, if charcter appears once, then return the character
         if char_count[c] == 1:
             return c
-    return None
+    return None # if nothing found, return null/none
 
 # NOTE: The following input values will be used for testing your solution.
 non_repeating("abcab") # should return 'c'
