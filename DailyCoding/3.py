def6255b134397ebb47ed363914e373e95f3326b
Henry Lin <henry.lin@appdirect.com>
2019-03-19 07:49:36 -0700

    3

--- DailyCoding/3.py
+++ DailyCoding/3.py
@@ -15,12 +15,15 @@ The following test should pass:
 
 node = Node('root', Node('left', Node('left.left')), Node('right'))
 assert deserialize(serialize(node)).left.left.val == 'left.left'
-Solution
-There are many ways to serialize and deserialize a binary tree, so don't worry if your solution differs from this one. We will be only going through one possible solution.
 
-We can approach this problem by first figuring out what we would like the serialized tree to look like. Ideally, it would contain the minimum information required to encode all the necessary information about the binary tree. One possible encoding might be to borrow S-expressions from Lisp. The tree Node(1, Node(2), Node(3)) would then look like '(1 (2 () ()) (3 () ()))', where the empty brackets denote nulls.
+# Solution
+# There are many ways to serialize and deserialize a binary tree, so don't worry if your solution differs from this one. We will be only going through one possible solution.
 
-To minimize data over the hypothetical wire, we could go a step further and prune out some unnecessary brackets. We could also replace the 2-character '()' with '#'. We can then infer leaf nodes by their form 'val # #' and thus get the structure of the tree that way. Then our tree would look like 1 2 # # 3 # #.
+# We can approach this problem by first figuring out what we would like the serialized tree to look like. Ideally, it would contain the minimum information required to encode all the 
+# necessary information about the binary tree. One possible encoding might be to borrow S-expressions from Lisp. The tree Node(1, Node(2), Node(3)) would then look like '(1 (2 () ()) (3 () ()))', where the empty brackets denote nulls.
+
+# To minimize data over the hypothetical wire, we could go a step further and prune out some unnecessary brackets. We could also replace the 2-character '()' with '#'. We can then infer leaf nodes by their form 'val # #' 
+# and thus get the structure of the tree that way. Then our tree would look like 1 2 # # 3 # #.
 
 def serialize(root):
     if root is None:
@@ -38,4 +41,6 @@ def deserialize(data):
         return node
     vals = iter(data.split())
     return helper()
-This runs in O(N) time and space, since we iterate over the whole tree when serializing and deserializing.
+
+    
+# This runs in O(N) time and space, since we iterate over the whole tree when serializing and deserializing.
