edb0cc7c9a1ef0897f0b9a3f5aa952f22c377547
hlin <henrylin2008@yahoo.com>
2019-07-16 08:26:39 -0700

    263 update

--- DailyCoding/263.py
+++ DailyCoding/263.py
@@ -2,7 +2,8 @@
 # Problem
 # This problem was asked by Nest.
 #
-# Create a basic sentence checker that takes in a stream of characters and determines whether they form valid sentences. If a sentence is valid, the program should print it out.
+# Create a basic sentence checker that takes in a stream of characters and determines whether they form valid sentences.
+# If a sentence is valid, the program should print it out.
 #
 # We can consider a sentence valid if it conforms to the following rules:
 #
@@ -12,17 +13,23 @@
 # The sentence must end with a terminal mark immediately following a word.
 
 # Solution
-# Since the input is a stream, we will only have access to one character at a time. As a result, we must come up with some rules to determine, based on the data we have seen earlier, whether adding the current character creates a valid sentence.
+# Since the input is a stream, we will only have access to one character at a time. As a result, we must come up with
+# some rules to determine, based on the data we have seen earlier, whether adding the current character creates a valid sentence.
 #
-# We know that a sentence must end in a terminal punctuation mark, such as a period or question mark. So maybe if the previous character was lowercase and the current character is one of these, we should print out the sentence. But we must also know if a capital letter started the sentence. So maybe we can set a variable to True if a capital letter has been seen. But maybe two spaces appeared at some point, and ... it turns out we need a system for these rules.
+# We know that a sentence must end in a terminal punctuation mark, such as a period or question mark. So maybe if the
+# previous character was lowercase and the current character is one of these, we should print out the sentence. But we
+# must also know if a capital letter started the sentence. So maybe we can set a variable to True if a capital letter has
+# been seen. But maybe two spaces appeared at some point, and ... it turns out we need a system for these rules.
 #
-# As it turns out, one system that suits this problem well is a finite state machine. After processing each element in our stream, we can update the state to be one of the following:
+# As it turns out, one system that suits this problem well is a finite state machine. After processing each element in
+# our stream, we can update the state to be one of the following:
 #
 # 0: Begin
 # 1: Uppercase
 # 2: Lowercase
 # 3: Space
 # 4: Separator
+#
 # Depending on the current character and state, different rules will apply. For example, if the current state is 0, meaning the sentence has not started, only a capital letter can follow, moving us to state 1. If any other character is seen, we remain in state 0. If at any point when processing the stream we break one of the abovementioned rules, the state gets reset to 0, and the sentence is reset to the empty string.
 #
 # Finally, if we manage to reach a terminal punctuation mark, we yield the current sentence and reset both the state and sentence.
