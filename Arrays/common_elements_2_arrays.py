787406c5b387977ed1289af6c3d6b8d5294d5f67
hlin <henrylin2008@yahoo.com>
2019-09-11 06:37:39 -0700

    adding comments

--- Arrays/common_elements_2_arrays.py
+++ Arrays/common_elements_2_arrays.py
@@ -7,16 +7,22 @@
 def common_elements(list1, list2):
     p1 = 0
     p2 = 0
+    # an empty list
     result = []
     while p1 < len(list1) and p2 < len(list2):
+        # if first item in list1 and list2 are the same
+        # then add the item to the (result) list, and increment p1 and p2
         if list1[p1] == list2[p2]:
             result.append(list1[p1])
             p1 += 1
             p2 += 1
+        # if p1 in list1 is > p2 in list 2, then increment p2 by 1 (or to next item in list2)
         elif list1[p1] > list2[p2]:
             p2 += 1
+        # if opposite, then increment p1 by 1 (or the next item in list1)
         else:
             p1 += 1
+    # return result list 
     return result
 
 
