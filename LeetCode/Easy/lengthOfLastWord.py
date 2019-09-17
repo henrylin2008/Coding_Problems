de2a2ffeb701821442ab2a9001fdd2c2179436be
hlin <henrylin2008@yahoo.com>
2019-09-17 10:18:10 -0700

    Add problem info from LeetCode

--- LeetCode/Easy/lengthOfLastWord.py
+++ LeetCode/Easy/lengthOfLastWord.py
@@ -1,17 +1,25 @@
-def lengOfLastWord(s):
+# 58. Length of Last Word
+# Link: https://leetcode.com/problems/length-of-last-word/
+# Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+#
+# If the last word does not exist, return 0.
+#
+# Note: A word is defined as a character sequence consists of non-space characters only.
+#
+# Example:
+#
+# Input: "Hello World"
+# Output: 5
+
+
+def lengthOfLastWord(s):
     count = 0
     local_count = 0
 
     for i in range(len(s)):
-        if s[i] == ' ':
+        if s[i] = ' ':
             local_count = 0
-            # print()
-            # print("empty space:", s[i])
-            # print()
         else:
             local_count += 1
-            # print("local count: ", local_count)
-            # print("Word so far: ", s[i])
             count = local_count
-            # print("count: ", count)
     return count
