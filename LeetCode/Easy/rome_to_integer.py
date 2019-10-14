48a4a24e32f8c8b61df66896bca8c2c1deb89b40
hlin <henrylin2008@yahoo.com>
2019-10-13 21:11:55 -0700

    adding romanToInt function

--- LeetCode/Easy/rome_to_integer.py
+++ LeetCode/Easy/rome_to_integer.py
@@ -45,3 +45,8 @@
 # Input: "MCMXCIV"
 # Output: 1994
 # Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+def romanToInt(s):
+
+    numeral_map ={"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = 0 
