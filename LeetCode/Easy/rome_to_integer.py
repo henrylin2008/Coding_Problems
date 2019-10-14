3f9907a88f5c6e28a3b03d6a48e054635b42395a
hlin <henrylin2008@yahoo.com>
2019-10-13 21:35:19 -0700

    updates

--- LeetCode/Easy/rome_to_integer.py
+++ LeetCode/Easy/rome_to_integer.py
@@ -49,4 +49,12 @@
 def romanToInt(s):
 
     numeral_map ={"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    result = 0 
+    result = 0
+
+    for i in range(len(s)):
+        if i > 0 and numeral_map[s[i]] > numeral_map[s[i-1]]:
+            result += numeral_map[s[i]] -2 * numeral_map[s[i-1]]
+        else:
+            result += numeral_map[s[i]]
+
+    return result
