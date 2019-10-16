884cee8720429103f72a7a7bfb0fd406fcb71972
hlin <henrylin2008@yahoo.com>
2019-10-15 22:18:09 -0700

    adding comments

--- LeetCode/Easy/merge2array.py
+++ LeetCode/Easy/merge2array.py
@@ -17,9 +17,9 @@
 #
 #
 def merge(nums1, m, nums2, n):
-    while m > 0 and n > 0:
-        if nums1[m-1] < nums2[n-1]:
-            nums1[m-1+n] = nums2[n-1]
+    while m > 0 and n > 0: # when there's still number in m and n
+        if nums1[m-1] < nums2[n-1]: # if last value of nums1 is less than last value of nums 2
+            nums1[m-1+n] = nums2[n-1] 
             n = n-1
         else:
             nums1[m-1+n], nums1[m-1] = nums1[m-1], nums1[m-1+n]
