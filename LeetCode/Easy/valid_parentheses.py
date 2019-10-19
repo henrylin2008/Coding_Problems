7f4c26ceb70e79d66ac29e471b26db9a4f698823
hlin <henrylin2008@yahoo.com>
2019-10-18 22:50:16 -0700

    adding comment

--- LeetCode/Easy/valid_parentheses.py
+++ LeetCode/Easy/valid_parentheses.py
@@ -1,12 +1,12 @@
 # 20. Valid Parentheses
-Link: https://leetcode.com/problems/valid-parentheses/
-Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-
-An input string is valid if:
-
-Open brackets must be closed by the same type of brackets.
-Open brackets must be closed in the correct order.
-Note that an empty string is also considered valid.
+# Link: https://leetcode.com/problems/valid-parentheses/
+# Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+#
+# An input string is valid if:
+#
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Note that an empty string is also considered valid.
 
 Example 1:
 
