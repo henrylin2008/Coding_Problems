5af7c770012b40f19a8f7d7529272409c03dd050
hlin <henrylin2008@yahoo.com>
2019-10-21 21:48:33 -0700

    adding comments

--- LeetCode/Easy/moveZeroes.py
+++ LeetCode/Easy/moveZeroes.py
@@ -15,10 +15,18 @@
 
 def moveZeroes(nums):
     pos = 0
+    # swap 0s with ordered numbers
+    # Ex: [0,1,0,3,12]
+    # Loop1: [1, 1, 0, 3, 12]
+    # Loop2: [1, 3, 0, 3, 12]
+    # Loop3: [1, 3, 12, 3, 12]
+
     for i in range(len(nums)):
         if nums[i]:
             nums[pos] = nums[i]
             pos += 1
 
+    # Loop4: [1, 3, 12, 0, 12]
+    # Loop5: [1, 3, 12, 0, 0]
     for i in range(pos, len(nums)):
-        nums[i] = 0 
+        nums[i] = 0
