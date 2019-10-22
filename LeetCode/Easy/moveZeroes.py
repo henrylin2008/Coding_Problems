f136cb9115be841320f1d4aaedd82fb199f2d59d
hlin <henrylin2008@yahoo.com>
2019-10-21 23:03:36 -0700

    adding comments

--- LeetCode/Easy/moveZeroes.py
+++ LeetCode/Easy/moveZeroes.py
@@ -14,18 +14,18 @@
 # Minimize the total number of operations.
 
 def moveZeroes(nums):
-    pos = 0  # the index after swap integer (and 0)
+    pos = 0  # index of swapped integer,
     # go through the array, and move ordered integers to the front of the array and move zeros to the back
     # Ex: [0,1,0,3,12]
-    # Loop1: [1, 1, 0, 3, 12]
-    # Loop2: [1, 3, 0, 3, 12]
-    # Loop3: [1, 3, 12, 3, 12]
+    # Loop1: [1, 1, 0, 3, 12]; pos: 1
+    # Loop2: [1, 3, 0, 3, 12]; pos: 2
+    # Loop3: [1, 3, 12, 3, 12]; pos: 3
     for i in range(len(nums)):
-        if nums[i]:   #
-            nums[pos] = nums[i]
-            pos += 1
+        if nums[i]:   # if i (every integer) exist
+            nums[pos] = nums[i]  # swap pos and i
+            pos += 1 # increment by 1
 
     # Loop4: [1, 3, 12, 0, 12]
     # Loop5: [1, 3, 12, 0, 0]
-    for i in range(pos, len(nums)):
-        nums[i] = 0
+    for i in range(pos, len(nums)):  # from pos to len(nums)
+        nums[i] = 0     # set rest value to 0 
