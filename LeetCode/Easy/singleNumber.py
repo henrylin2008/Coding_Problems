421a8417068292dd625737805587a8b94a402c66
hlin <henrylin2008@yahoo.com>
2019-10-23 07:02:38 -0700

    adding comments

--- LeetCode/Easy/singleNumber.py
+++ LeetCode/Easy/singleNumber.py
@@ -16,9 +16,26 @@
 # Output: 4
 
 def singleNumber(nums):
-    ans = 0
+    output = 0  # set a output value
 
     for num in nums:
-        ans ^= num
+        output ^= num  # ex: [2,2,1]
 
-    return ans
+    return output
+        # loop 1: output=2
+        # loop 2: output=0
+        # loop 3: output=1
+
+# Bit Manipulation
+# Concept
+#
+# - If we take XOR of zero and some bit, it will return that bit
+#     * a ^ 0 = a
+# - If we take XOR of two same bits, it will return 0
+#     * a ^ a = 0
+#
+# -a ^ b ^ a = (a ^ a) ^ b = 0 ^ b = b
+
+# Time complexity : O(n)O(n). We only iterate through \text{nums}nums, so the time complexity is the number of elements in \text{nums}nums.
+#
+# Space complexity : O(1)O(1).
