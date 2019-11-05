02e0aafd4b6bf8251c2f8bd81ef0d2307bffaae9
hlin <henrylin2008@yahoo.com>
2019-11-05 10:54:46 -0800

    completed code

--- LeetCode/Medium/permutations.py
+++ LeetCode/Medium/permutations.py
@@ -16,3 +16,15 @@
 #   [3,2,1]
 # ]
 
+def permute(nums):
+
+    if len(nums) <= 1:
+        return [nums]
+
+    answer = []
+    for i, num in enumerate(nums):
+        n = nums[:i] + nums[i+1:]
+        for y in permute(n):
+            answer.append([num] + y)
+
+    return answer
