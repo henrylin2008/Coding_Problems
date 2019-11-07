230be42f185824e80ce30005a3de6a0b5bb01898
hlin <henrylin2008@yahoo.com>
2019-11-07 06:52:29 -0800

    update comments

--- LeetCode/Medium/permutations.py
+++ LeetCode/Medium/permutations.py
@@ -18,13 +18,13 @@
 
 def permute(nums):
 
-    if len(nums) <= 1: # return nums itself if the length = 1 or 0
+    if len(nums) <= 1: # base case: return nums itself if the length <= 1
         return [nums]
 
     answer = []
     for i, num in enumerate(nums):
-        n = nums[:i] + nums[i+1:]
-        for y in permute(n):
-            answer.append([num] + y)
+        n = nums[:i] + nums[i+1:] # leftover nums (in array) after picked num in i; ex: [1,2,3]: index(0), n = [2,3]
+        for y in permute(n):  # recursive call, for loop for reminder nums
+            answer.append([num] + y)  # combine result of reminder nums and result of first for loop 
 
     return answer
