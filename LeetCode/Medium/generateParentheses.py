48d2d5aa7be2fea24a7a408b5eee65172740099d
hlin <henrylin2008@yahoo.com>
2019-11-15 06:59:59 -0800

    adding code

--- LeetCode/Medium/generateParentheses.py
+++ LeetCode/Medium/generateParentheses.py
@@ -11,3 +11,21 @@
 #   "()(())",
 #   "()()()"
 # ]
+
+def generateParentheses(self, n):
+    if n == 0:
+        return []
+
+    result = []
+    self.helper(n, n, '', result)
+    return result
+
+def helper(self, l, r, item, result):
+    if l < r:
+        return
+    if l == 0 and r == 0:
+        result.append(item)
+    if l > 0:
+        self.helper(l-1, r, item + '(', result)
+    if r > 0:
+        self.helper(l, r-1, item + ')', result)
