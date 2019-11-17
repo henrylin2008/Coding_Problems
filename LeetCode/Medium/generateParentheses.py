7cc2d5a0477f8dcadabd91ae95a39bd682cee8d9
hlin <henrylin2008@yahoo.com>
2019-11-17 07:57:57 -0800

    updated code with a class

--- LeetCode/Medium/generateParentheses.py
+++ LeetCode/Medium/generateParentheses.py
@@ -12,20 +12,39 @@
 #   "()()()"
 # ]
 
-def generateParentheses(self, n):
-    if n == 0:  # return empty array if n == 0
-        return []
+# def generateParentheses(n):
+#     if n == 0:
+#         return []
+#
+#     result = [] #
+#     self.helper(n, n, '', result)
+#     return result
+#
+# def helper(r, item, result):
+#     if l < r:
+#         return
+#     if l == 0 and r == 0:
+#         result.append(item)
+#     if l > 0:
+#         self.helper(l-1, r, item + '(', result)
+#     if r > 0:
+#         self.helper(l, r-1, item + ')', result)
 
-    result = [] #
-    self.helper(n, n, '', result)  # recursive function, ("# of (", "# of )", 'current return value', 'append return value into result')
-    return result
 
-def helper(self, l, r, item, result):
-    if l < r:
-        return
-    if l == 0 and r == 0:
-        result.append(item)
-    if l > 0:
-        self.helper(l-1, r, item + '(', result)
-    if r > 0:
-        self.helper(l, r-1, item + ')', result)
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        if n == 0: # return empty array if n == 0
+            return []
+        result = []
+        self.helper(n, n, '', result) #recursive call, ("# of '('", "# of ')'", 'current return value', 'append return value into result')
+        return result
+
+    def helper(self, l, r, item, result):
+        if r < l:
+            return
+        if l == 0 and r == 0:
+            result.append(item)
+        if l > 0:
+            self.helper(l - 1, r, item + '(', result)
+        if r > 0:
+            self.helper(l, r - 1, item + ')', result)
