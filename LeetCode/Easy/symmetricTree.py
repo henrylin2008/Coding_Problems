dcba6e20f5419293226de7d24938f0afafee2aa4
hlin <henrylin2008@yahoo.com>
2019-11-18 22:36:45 -0800

    updated with code

--- LeetCode/Easy/symmetricTree.py
+++ LeetCode/Easy/symmetricTree.py
@@ -18,3 +18,31 @@
 #    2   2
 #     \   \
 #     3    3
+
+class Solution:
+    # def __init__(self, x):
+    #     self.val = x
+    #     self.left = None
+    #     self.right = None
+
+    def isSymmetric(self, root):
+
+        if root is None:
+            return True
+
+        return Solution.isSymmetricRec(root.left, root.right)
+
+    def isSymmetricRec(self, left, right):
+
+        if left is None and right is None:
+            return True
+
+        if left is None or right is None or left.val != right.val:
+            return False
+
+        return self.isSymmetricRec(left.left, right.right) and self.isSymmetricRec(left.right, right.left)
+
+
+
+# Solution.isSymmetric(1, [1,2,2,3,4,4,3])
+
