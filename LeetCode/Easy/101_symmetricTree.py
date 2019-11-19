5256968e594f13396beacb835f56c01cd05da8ae
hlin <henrylin2008@yahoo.com>
2019-11-19 11:08:52 -0800

    adding comments & updated code

--- LeetCode/Easy/101_symmetricTree.py
+++ LeetCode/Easy/101_symmetricTree.py
@@ -27,21 +27,27 @@ class Solution:
 
     def isSymmetric(self, root):
 
-        if root is None:
+        if root is None:  # if root is empty, then it's symmetric
             return True
 
-        return Solution.isSymmetricRec(root.left, root.right)
+        return self.isSymmetricRec(root.left, root.right)  # recursive call with 2 inputs (left, right)
 
-    def isSymmetricRec(self, left, right):
+    def isSymmetricRec(self, left, right): # Recursive function
 
-        if left is None and right is None:
+        if left is None and right is None: # if left and right is empty, then it's symmetric
             return True
 
         if left is None or right is None or left.val != right.val:
+            # False case 1: left is empty but not right;
+            # False case 2: right is empty but not left
+            # False case 3: left and right value are not equal
+
             return False
 
         return self.isSymmetricRec(left.left, right.right) and self.isSymmetricRec(left.right, right.left)
-
+        # return true if:
+        # left.left = right.right (ex above, tree[3] = tree[6], most left lower level = most right lower level
+        # left.right = right.left (ex above, tree[4] = tree[5], right of lowest level of left subtree = left of lowest level of right subtree
 
 
 # Solution.isSymmetric(1, [1,2,2,3,4,4,3])
