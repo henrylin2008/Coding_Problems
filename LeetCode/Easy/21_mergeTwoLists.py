0cf9aedc48ebfc901b859fee7c54d1a6fa407551
hlin <henrylin2008@yahoo.com>
2019-11-22 10:14:03 -0800

    adding comments

--- LeetCode/Easy/21_mergeTwoLists.py
+++ LeetCode/Easy/21_mergeTwoLists.py
@@ -19,17 +19,21 @@ class ListNode:
 class solution:
 
     def mergeTwoLists(self, l1, l2):
-        curr = dummp = ListNode(0)
+        curr = dummy = ListNode(0)
+        # curr: current pointer
+        # dummy: a dummy node, the goal is move the pointer from the end (when finished comparing) to the beginning of node
+        # ListNode(0): beginning of the node
 
         while l1 and l2:
-            if l1.val < l2.val:
-                curr.next = l1
-                l1 = l1.next
-            else:
-                curr.next = l2
-                l2 = l2.next
-            curr = curr.next
-        curr.next = l1 or l2
+            if l1.val < l2.val: # when l1 value is less than l2 value
+                curr.next = l1 # insert the smaller value (l1)
+                l1 = l1.next # move the pointer to next l1 value, since the current value has been inserted into the list
+            else:  # when l1 >= l2
+                curr.next = l2 # insert l2
+                l2 = l2.next # move the pointer to next (l2) value
+            curr = curr.next # move the pointer of output to next node
+        curr.next = l1 or l2 # for case: input: l1=[1,2,3,],l2=[4,5,6] or vice versa; as while loop (above) does not cover this case
+        # append l2 into the end of l1 or vice versa
 
-        return dummp.next
+        return dummy.next
 
