42176c64cd60b38953d6611878bc5656f2581d72
hlin <henrylin2008@yahoo.com>
2019-11-21 21:35:26 -0800

    update code

--- LeetCode/Easy/21_mergeTwoLists.py
+++ LeetCode/Easy/21_mergeTwoLists.py
@@ -11,4 +11,25 @@
 # Output: 1->1->2->3->4->4
 
 
-def mergeTwoLists(l1, l2):
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class solution:
+
+    def mergeTwoLists(self, l1, l2):
+        curr = dummp = ListNode(0)
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        curr.next = l1 or l2
+
+        return dummp.next
+
