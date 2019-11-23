bf4b55a929979a6fedc6feb1c08796b76dda8265
hlin <henrylin2008@yahoo.com>
2019-11-23 11:24:48 -0800

    adding code

--- LeetCode/Easy/66_plusOne.py
+++ LeetCode/Easy/66_plusOne.py
@@ -17,3 +17,16 @@
 # Input: [4,3,2,1]
 # Output: [4,3,2,2]
 # Explanation: The array represents the integer 4321.
+
+def plusOne(digits):
+
+    for i in reversed(range(len(digits))):
+        if digits[i] == 9:
+            digits[i] = 0
+        else:
+            digits[i] += 1
+            return digits
+    digits[0] = 1
+    digits.append(0)
+
+    return digits
