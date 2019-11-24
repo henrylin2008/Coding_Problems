ae2c7c18a35ba81ddec9ed722c67fc2a909dd5d1
hlin <henrylin2008@yahoo.com>
2019-11-24 09:17:43 -0800

    adding code

--- LeetCode/Easy/412_fizzBuzz.py
+++ LeetCode/Easy/412_fizzBuzz.py
@@ -27,3 +27,15 @@
 #     "FizzBuzz"
 # ]
 
+def fizzBuzz(n):
+    res = []
+    for i in range(1, n+1):
+        if i % 15 == 0:
+            res.append("FizzBuzz")
+        elif i % 3 == 0:
+            res.append("Fizz")
+        elif i % 5 == 0:
+            res.append("Buzz")
+        else:
+            res.append(str(i))
+    return res
