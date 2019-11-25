56acb8d3c7bd4cda4e6226dbcf6f849976677dbb
hlin <henrylin2008@yahoo.com>
2019-11-25 06:05:45 -0800

    adding code & comments

--- LeetCode/Easy/38_countAndSay.py
+++ LeetCode/Easy/38_countAndSay.py
@@ -27,3 +27,22 @@
 #
 #     Input: 4
 #     Output: "1211"
+
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        seq = "1"   # initial sequence (first line)
+        for i in range(n - 1): # locate item in nth line; range from 0 to n-1
+            seq = self.getNext(seq) 
+        return seq
+
+    def getNext(self, seq):
+        i, next_seq = 0, ""
+        while i < len(seq):
+            count = 1
+            while i < len(seq) - 1 and seq[i] == seq[i + 1]:
+                count += 1
+                i += 1
+            next_seq += str(count) + seq[i]
+            i += 1
+        return next_seq
+
