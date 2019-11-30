db0c9b71e39793d6d1e99f223eab0da73a270244
hlin <henrylin2008@yahoo.com>
2019-11-30 08:40:14 -0800

    solution #1

--- LeetCode/Easy/14_longestCommonPrefix.py
+++ LeetCode/Easy/14_longestCommonPrefix.py
@@ -16,3 +16,18 @@
 # Note:
 #
 # All given inputs are in lowercase letters a-z.
+
+# solution #1
+# def longestCommonPrefix(strs):
+#
+#     if not strs:
+#         return ""
+#
+#     for i in range(len(strs[0])):
+#         for string in strs[1:]:
+#             if i >= len(string) or string[i] != strs[0][i]:
+#                 return strs[0][:i]
+#
+#     return strs[0] # strs[0] == ""; string exist as empty str("")
+
+def longestCommonPrefix(strs):
