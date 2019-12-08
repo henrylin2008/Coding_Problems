d648dad6fb35dccba4ea40cc5b3155fe222a2277
hlin <henrylin2008@yahoo.com>
2019-12-07 22:59:43 -0800

    updated comments and the code

--- LeetCode/Easy/67_addBinary.py
+++ LeetCode/Easy/67_addBinary.py
@@ -14,43 +14,27 @@
 # Output: "10101"
 
 def addBinary(a, b):
-    # adding numbers from right to left, add carryover numbers
+    # adding numbers of both of (a and b) from the right to left, add carryover numbers
     result, carry, val = '', 0, 0
     # result = (string) output;
     # carry = carryover value; if 1 + 1 ==> 1 0
     # val: (current) result of addition of 2 values in current index
     print(a,b)
     for i in range(max(len(a), len(b))): # use the longest length as the range, so it goes over each character
-        # print("i:", i)
-        print("carry, val:", carry, val)
-        val = carry # check if there's carry(over) from last loop; if so, add it (in current loop)
+        val = carry # check if there's carry(over) from last loop; if so, add it into the value
         if i < len(a):
-            print("a[-(i+1)]:", a[-(i+1)])
-            # print("a[-(i+1)]:", a[-(i+1)])
-            # print("val before:", val)
             val += int(a[-(i+1)]) # int(): convert given string to int
             # a[-(i+1)]: move index from right to left; adding bits from right to the left
-            print("val after a:", val)
         if i < len(b):
-            print("b[-(i+1)]:", b[-(i+1)])
-            # print("(b[-(i+1)]):", (b[-(i+1)]))
-            val += int(b[-(i+1)]) # moving index from right to the left.
-            print("val after b:", val)
-        print("carry, val:", carry, val)
+            val += int(b[-(i+1)]) # moving index from right to the left
+        # (current) value = sum of (current index) carry, a, and b
         carry, val = val//2, val%2
         # carry: when val is greater than 2, there's a carryover; otherwise (if val < 2), then no carry
         # val: % (mod) 2, reminder (after carryover)
-        print("carry, val after:", carry, val)
-        print("result before:", result)
-        result += str(val) # append val (as string) into result
-        print("result after:", result)
-        print("")
-    if carry: # if there's carryover value haven't been counted yet; For ex 2: it requires a 5th number into the result
-        print("result in carry before", result)
-        result += str(1) # add one into the result
-        print("result in carry after:", result)
-    print(result[::-1])
+        result += str(val) # append val (as string) into result at the end
+    if carry: # if there's carryover value haven't been counted yet; For ex 2: it inserted 5th (index 1) into the result
+        result += str(1) # add one (at the end) of the result
     return result[::-1] # reverse order
 
-addBinary("1010", "1111")
+# addBinary("1010", "1111")
 
