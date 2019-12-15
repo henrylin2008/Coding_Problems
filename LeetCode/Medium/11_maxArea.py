debe8603fca26567d359a56b6fd2a23d0ca4ea3f
hlin <henrylin2008@yahoo.com>
2019-12-15 09:41:31 -0800

    adding code

--- LeetCode/Medium/11_maxArea.py
+++ LeetCode/Medium/11_maxArea.py
@@ -5,3 +5,21 @@
 # a container, such that the container contains the most water.
 #
 # Note: You may not slant the container and n is at least 2.
+
+def maxArea(height):
+    left = 0
+    right = len(height) -1
+    result = 0
+
+    while left < right:
+        area = min(height[left], height[right])*(right-left)
+
+    if area > result:
+        result = area
+
+    if height[left] < height[right]:
+        left += 1
+    else:
+        right -= 1
+
+    return result
