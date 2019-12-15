d5f6754287bed813bae717633e20437d45e9ab33
hlin <henrylin2008@yahoo.com>
2019-12-15 10:04:47 -0800

    update code and comments

--- LeetCode/Medium/11_maxArea.py
+++ LeetCode/Medium/11_maxArea.py
@@ -13,13 +13,18 @@ def maxArea(height):
 
     while left < right:
         area = min(height[left], height[right])*(right-left)
+        # calculate temporary area; By using lowest height b/t 2 pointers (=highest of water can be contained), then
+        # multiply it with the length (right-left)
 
-    if area > result:
-        result = area
+        if area > result: # if area is > result, then set the area as new result
+            result = area
 
-    if height[left] < height[right]:
-        left += 1
-    else:
-        right -= 1
-
+        if height[left] < height[right]: # determine which pointer to move (left or right)
+            # if the value of current (left) pionter is less than the right pointer
+            left += 1 # move left pointer one position to the right
+        else:
+            right -= 1 # move right pointer one position to the left
+    # print(result)
     return result
+
+# maxArea([1,8,6,2,5,4,8,3,7])
