6d7685e42bea39584de4d7d3c59c56f99ba24a9a
hlin <henrylin2008@yahoo.com>
2020-02-12 21:07:47 -0800

    adding comments

--- Depth_First_Search/dfs.py
+++ Depth_First_Search/dfs.py
@@ -5,7 +5,11 @@
 # Time Complexity on a tree: O(V)
 # V is the number of nodes
 # Its goal is to search as deeply as possible, connecting as many nodes in the graph as possible and branching
-# # where necessary
+# where necessary
+# https://www.educative.io/edpresso/how-to-implement-depth-first-search-in-python
+# The Algorithm
+# Pick any node. If it is unvisited, mark it as visited and recur on all its adjacent nodes.
+# Repeat until all the nodes are visited, or the node to be searched is found.
 
 # Using a Python dictionary to act as an adjacency list
 graph = {
@@ -19,12 +23,13 @@ graph = {
 
 visited = [] # Array to keep track of visited nodes.
 
-def dfs(visited, graph, node):
-    if node not in visited:
+def dfs(visited, graph, node): #graph in the form of a dictionary, and A, which is the starting node.
+    if node not in visited: #It first checks if the current node is unvisited - if yes,
         print node,
-        visited.append(node)
-        for neighbour in graph[node]:
-            dfs(visited, graph, neighbour)
+        visited.append(node) # it is appended in the visited array.
+        for neighbour in graph[node]: # for each neighbor of the current node,
+            dfs(visited, graph, neighbour) #the dfs function is invoked again
 
+# The base case is invoked when all the nodes are visited. The function then returns.
 # Driver Code
-dfs(visited, graph, 'A')
+dfs(visited, graph, 'A') # 'A' is starting node
