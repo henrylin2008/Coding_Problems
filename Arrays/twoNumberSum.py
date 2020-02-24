ff02efc2e48d0f692ac2161501bd35690ce78f34
hlin <henrylin2008@yahoo.com>
2020-02-24 10:46:42 -0800

    add description

--- Arrays/twoNumberSum.py
+++ Arrays/twoNumberSum.py
@@ -1,3 +1,15 @@
+# link: https://www.algoexpert.io/questions/Two%20Number%20Sum
+#
+# ​Two Number Sum
+#
+# Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum.
+# If any two numbers in the input array sum up to the target sum, the function should return them in an array.
+# If no two numbers sum up to the target sum, the function should return an empty array. Assume that there will be at
+# most one pair of numbers summing up to the target sum.
+#
+# Sample input: [3, 5, -4, 8, 11, 1, -1, 6], 10
+# Sample output: [-1, 11]
+
 # Method 1:
 # Time: O(n^2)
 # Space: O(1)
