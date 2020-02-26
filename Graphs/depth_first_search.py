628cf9e52e89652a31103de880e22a95e99f1559
hlin <henrylin2008@yahoo.com>
2020-02-26 08:08:06 -0800

    DFS

---
+++ Graphs/depth_first_search.py
@@ -0,0 +1,35 @@
+# link: https://www.algoexpert.io/questions/Depth-first%20Search
+# You are given a Node class that has a name and an array of optional children Nodes. When put together, Nodes form a
+# simple tree-like structure. Implement the depthFirstSearch method on the Node class, which takes in an empty array,
+# traverses the tree using the Depth-first Search approach (specifically navigating the tree from left to right), stores
+# all the of the Nodes' names in the input array, and returns it.
+#
+# sample input:
+#             A
+#            /|\
+#           B C D
+#          /\   /\
+#         E  F G  H
+#            /\ \
+#           I J  K
+#
+# Sample output: ["A","B","E","F","I","J","C","D","G","K","H"]
+# v: vertice (node)
+# e: edge (lines connect between nodes)
+# Time: O(v + e): vertices + edges
+# Space: O(v): level of depth/height of the tree
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+        for child in self.children:
+            child.depthFirstSearch(array)
+        return array
+
