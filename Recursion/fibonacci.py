93e61eadc6984ea5d081992edbddf50773f11ddc
hlin <henrylin2008@yahoo.com>
2020-02-26 06:44:15 -0800

    update comments

--- Recursion/fibonacci.py
+++ Recursion/fibonacci.py
@@ -16,7 +16,7 @@ def fibonacci(n):
 # Time: O(n)
 # Space: O(n)
 # set the base cases (1 & 2) in the hash table (memoize), calculate each fib once and stores it in the hash table,
-# when retriving the value from hash table is a constant operation [O(1)]
+# retriving the value from hash table is a constant operation [O(1)]
 def fib(n,  memoize={1:0, 2:1}):
     if n in memoize:
         return memoize[n]
@@ -25,15 +25,20 @@ def fib(n,  memoize={1:0, 2:1}):
         return memoize[n]
 
 # Method 3: iterative
-# Time: O(n)
-# Space: O(1)
-#
+# Time: O(n): runs through all values once
+# Space: O(1): only stores lastTwo/base case values [0, 1]
+# Concept: initialized first 2 base cases and a counter, next Fib = sum of lastTwo[0] and lastTwo[1], ex: 0 1 => 1(nextFib)
+# then lastTwo[1] replaces lastTwo[0], and next Fib replaces lastTwo[1]
+# count: [1,2,3,4,5,6,7,8,9,10]
+# Value: [0,1,1,2,3,5,8,13,21,34]
 def fib(n):
-    lastTwo = [0, 1] # initialize base case, store last 2 Fib values in the array
+    lastTwo = [0, 1] # initialize base case, store last 2 Fib values in an array
     counter = 3
     while counter <= n:
-        nextFib = lastTwo[0] + lastTwo[1]  # calculate next Fib
-        lastTwo[0] = lastTwo[1]  # throw away value at lastTwo[0], replace it with lastTwo[1]
-        lastTwo[1] = nextFib # replace lastTwo[1] with next value lastTwo[2] (nextFib) 
-        counter +=1
+        nextFib = lastTwo[0] + lastTwo[1]  # next Fib = sum of previous 2 values
+        lastTwo[0] = lastTwo[1]  # replace lastTwo[0] with lastTwo[1]
+        lastTwo[1] = nextFib # replace lastTwo[1] with nextFib
+        counter +=1 # increment the counter
     return lastTwo[1] if n > 1 else lastTwo[0] # edge case: n = 1, return lastTow[0]
+
+# fib(30s
