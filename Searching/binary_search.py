ec5733da70de3227e7273863227d1cdc7e04f857
hlin <henrylin2008@yahoo.com>
2020-02-27 22:36:30 -0800

    update format and comments

--- Searching/binary_search.py
+++ Searching/binary_search.py
@@ -7,26 +7,14 @@ def binarySearch(array, target):
 
 def binarySearchHelper(array, target, left, right):
     if left > right:
-        print("no match")
         return -1
-    mid = (left+right)//2
-    potentialMatch = array[mid]
+    mid = (left+right)//2   # middle pointer index, rounded down with //2
+    potentialMatch = array[mid]  # value at middle pointer
     if target == potentialMatch:
-        print("left, right", left, ",", right)
-        print('mid:', mid)
-        print("__")
-        return mid
-    elif target > potentialMatch:  # when target is > potnetialMatch, result is in second half of the array,
-                                     moving the mid pointer one position to the right
-        print("mid+1:", mid+1)
-        print("right:", right)
-        print('__')
+        return mid  # index of mid pointer
+    elif target > potentialMatch: # eliminate first half from the array, and set left pointer = mid (pointer) + 1
         return binarySearchHelper(array, target, mid+1, right)
-    else:                          # when target is < potnetialMatch, result is in first half of the array,
-                                   # moving the mid pointer one position to the left
-        print("left:", left)
-        print("mid-1:", mid-1)
-        print('__')
+    else:      # eliminate second half from the array, and set the right pointer = mid (pointer) - 1
         return binarySearchHelper(array, target, left, mid-1)
 
 # binarySearch([0,1,21,33,45,55,67,71,78,83], 37)
@@ -44,13 +32,13 @@ def binarySearchHelper(array, target, left, right):
         mid = (left+right) //2
         potentialMatch = array[mid]
         if target == potentialMatch:
-            print("match at:", mid)
+            # print("match at:", mid)
             return mid
         elif target < potentialMatch:
             right = mid - 1
         else:
             left = mid + 1
-    print("no match")
+    # print("no match")
     return -1
 
 # binarySearch([0,1,21,33,45,55,67,71,78,83,88,92], 55)
