6512efa9a2faff07f9d46d18f7ac820c446288fc
hlin <henrylin2008@yahoo.com>
2020-03-01 12:25:21 -0800

    adding comments

--- Searching/threeLargestNumbers.py
+++ Searching/threeLargestNumbers.py
@@ -9,19 +9,23 @@
 def findThreeLargestNumbers(array):
     threeLargest = [None, None, None]
     for num in array:
-        calThreeLargest(threeLargest, num)
-    print(threeLargest)
+        calThreeLargest(threeLargest, num)  # helper method
+    # print(threeLargest)
     return threeLargest
 
 def calThreeLargest(threeLargest, num):
-    if threeLargest[2] is None or num > threeLargest[2]:
+    if threeLargest[2] is None or num > threeLargest[2]: # calculate last index value if it's None or num > [2]
         shiftUpdate(threeLargest, num, 2)
-    elif threeLargest[1] is None or num > threeLargest[1]:
+    elif threeLargest[1] is None or num > threeLargest[1]: # calculate 2nd index value if it's None or num > [1]
         shiftUpdate(threeLargest, num, 1)
-    elif threeLargest[0] is None or num > threeLargest[0]:
+    elif threeLargest[0] is None or num > threeLargest[0]: # calculate first index value if it's None or num > [0]
         shiftUpdate(threeLargest, num, 0)
 
 def shiftUpdate(array, num, index):
+    # Objective: if i == (last) index , then set the num = array[i] (array[2] in this case)
+    # else: shift index one position to the left
+    # [0, 1, 2]    ==>      [0, 1, 2]
+    # [x, y, z]    ==>      [y, z, num]
     for i in range(index+1):
         if i == index:
             array[i] = num
