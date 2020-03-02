bb459d26687c0ccb2788a0f3ec1437d4b7578f25
hlin <henrylin2008@yahoo.com>
2020-03-02 11:21:48 -0800

    adding code and comments

--- Sorting/insertionSort.py
+++ Sorting/insertionSort.py
@@ -9,17 +9,21 @@
 # Sample input: [8, 5, 2, 9, 5, 6, 3]
 # Sample output: [2, 3, 5, 5, 6, 8, 9]
 
+# Solution: go through a (forward) loop once, and go through a backward loops to compare values
 # Best: O(n) time | O(1) space - where n is the length of the input array
 # Average: O(n^2) time | O(1) space - where n is the length of the input array
 # Worst: O(n^2) time | O(1) space - where n is the length of the input array
 
 def insertionSort(array):
-    for i in range(1, len(array)):
-        j = i
-        while j > 0 and array[j] < array[j-1]:
-            swap(j, j-1, array)
-            j -= 1
+    for i in range(1, len(array)):  # forward loop from index 1 (temp array) to last item
+        j = i   # set second index (for reverse loop)
+        while j > 0 and array[j] < array[j-1]: # while second index (j) > 0, and current (index) value < previous value
+            swap(j, j-1, array)   # swap 2 nums: current and previous
+            j -= 1   # decrement index
+    print(array)
     return array
 
 def swap(i, j, array):
-    array[i], array[j] = array[j], array[i]
+    array[i], array[j] = array[j], array[i]  # swap values in 2 indexes 
+
+insertionSort([8, 5, 2, 9, 5, 6, 3, 15, 20, -3])
