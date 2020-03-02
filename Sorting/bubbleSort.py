0936d1040342e86276027cd9693410922e6d9170
hlin <henrylin2008@yahoo.com>
2020-03-02 06:43:10 -0800

    Bubble sort

---
+++ Sorting/bubbleSort.py
@@ -0,0 +1,7 @@
+# Bubble Sort:
+# Write a function that takes in an array of integers and returns a sorted version of that array. Use the Bubble Sort
+# algorithm to sort the array.
+# If you're unfamiliar with Bubble Sort, we recommend watching the Conceptual Overview section of this question's video
+# explanation before starting to code.
+# Sample input: [8, 5, 2, 9, 5, 6, 3]
+# Sample output: [2, 3, 5, 5, 6, 8, 9]
