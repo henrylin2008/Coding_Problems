6a91d344dbc3882822d0e59c3e94beb1cee37819
hlin <henrylin2008@yahoo.com>
2020-03-01 21:36:53 -0800

    updates

--- Searching/threeLargestNumbers.py
+++ Searching/threeLargestNumbers.py
@@ -1,16 +1,17 @@
 # Link: https://www.algoexpert.io/questions/Find%20Three%20Largest%20Numbers
-# # Find Three Largest Numbers
-# # Write a function that takes in an array of integers and returns a sorted array of the three largest integers in the
-# # input array. Note that the function should return duplicate integers if necessary; for example, it should return
-# # [10, 10, 12] for an input array of [10, 5, 9, 10, 12].
-# # Sample input: [141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]
-# # Sample output: [18, 141, 541]
+# Find Three Largest Numbers
+# Write a function that takes in an array of integers and returns a sorted array of the three largest integers in the
+# input array. Note that the function should return duplicate integers if necessary; for example, it should return
+# [10, 10, 12] for an input array of [10, 5, 9, 10, 12].
+# Sample input: [141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]
+# Sample output: [18, 141, 541]
+# Idea: set an list with 3 empty values, compares the values from the given array with the new list;
 
 def findThreeLargestNumbers(array):
     threeLargest = [None, None, None]
     for num in array:
         calThreeLargest(threeLargest, num)  # helper method
-    # print(threeLargest)
+    print(threeLargest)
     return threeLargest
 
 def calThreeLargest(threeLargest, num):
@@ -33,4 +34,4 @@ def shiftUpdate(array, num, index):
             array[i] = array[i+1]
 
 
-findThreeLargestNumbers([141, 1, 17, -7, -17, -27, 183, 541, 988, 7, 7])
+# findThreeLargestNumbers([141, 1, 17, -7, -17, -27, 183, 541, 98, 7, 7])
