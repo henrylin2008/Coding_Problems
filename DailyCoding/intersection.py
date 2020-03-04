d265c1840cb37b70d7ee98759d81c8b68d318a44
hlin <henrylin2008@yahoo.com>
2020-03-04 08:09:56 -0800

    update file name

--- DailyCoding/intersection.py
+++ DailyCoding/intersection.py
@@ -1,8 +1,8 @@
-Daily Coding Problem #20
-Problem
-This problem was asked by Google.
-
-Given two singly linked lists that intersect at some point, find the intersecting node. The lists are non-cyclical.
+# Daily Coding Problem #20
+# Problem
+# This problem was asked by Google.
+#
+# Given two singly linked lists that intersect at some point, find the intersecting node. The lists are non-cyclical.
 
 # For example, given A = 3 -> 7 -> 8 -> 10 and B = 99 -> 1 -> 8 -> 10, return the node with value 8.
 #
