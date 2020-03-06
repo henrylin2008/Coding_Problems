937442cc1922b10b7caf38c34f3e875feecf7640
hlin <henrylin2008@yahoo.com>
2020-03-06 15:37:13 -0800

    update comments

--- Strings/caesarCipherEncryptor.py
+++ Strings/caesarCipherEncryptor.py
@@ -13,28 +13,31 @@
 # Time: O(n)
 # Space: O(n)
 def caesarCipherEncryptor(string, key):
-    newLetters = []
+    newLetters = [] # array for new letters
     newKey = key % 26
-    for letter in string:
+    for letter in string: # for every letter, apply helper function that shifts letters and calculate the letter code
         newLetters.append(getNewLetter(letter, newKey))
-    return "".join(newLetters)
+    return "".join(newLetters) # join array to a string
 
 def getNewLetter(letter, key):
-    newLetterCode = ord(letter) + key
+    newLetterCode = ord(letter) + key # orc(): get the unicode value of letter
     return chr(newLetterCode) if newLetterCode <= 122 else chr(96 + newLetterCode % 122)
+        # chr() returns a character (a string) from an integer (represents unicode code point of the character).
+        # unicode for lower letters from a to z: 97 - 122
 
 
 # Method 2:
 # Time: O(n)
 # Space: O(n)
 def caesarCipherEncryptor(string, key):
-    newLetters = []
-    newKey = key % 26
+    newLetters = [] # new array to store letters
+    newKey = key % 26 # 26 letters in alphabet
     alphabet = list("abcdefghijklmnopqrstuvwxyz")
     for letter in string:
-        newLetters.append(getNewLetter(letter, newKey, alphabet))
-    return "".join(newLetters)
+        newLetters.append(getNewLetter(letter, newKey, alphabet)) # append every letter to the string
+    return "".join(newLetters) # combine all the letters
 
 def getNewLetter(letter, key, alphabet):
-    newLetterCode = alphabet.index(letter) + key
+    newLetterCode = alphabet.index(letter) + key # getting the index of letter's new location
     return alphabet[newLetterCode] if newLetterCode <= 25 else alphabet[-1 + newLetterCode % 25]
+    # newLetterCode <= 25 if that's within index of alphabet or before the wrapper; other calculate its new index 
