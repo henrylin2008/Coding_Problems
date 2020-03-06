6b0bdb35a0074a5f69152f92f63129045e6ea992
hlin <henrylin2008@yahoo.com>
2020-03-05 17:23:34 -0800

    Method 1

--- Strings/caesarCipherEncryptor.py
+++ Strings/caesarCipherEncryptor.py
@@ -8,3 +8,23 @@
 
 # Sample input: "xyz", 2
 # Sample output: "zab"
+
+# Method 1:
+# Time: O(n)
+# Space: O(n)
+def caesarCipherEncryptor(string, key):
+    newLetters = []
+    newKey = key % 26
+    for letter in string:
+        newLetters.append(getNewLetter(letter, newKey))
+    return "".join(newLetters)
+
+def getNewLetter(letter, key):
+    newLetterCode = ord(letter) + key
+    return chr(newLetterCode) if newLetterCode <= 122 else chr(96 + newLetterCode % 122)
+
+
+# Method 2:
+# Time: O(n)
+# Space: O(n)
+def ca
