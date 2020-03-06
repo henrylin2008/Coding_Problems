518104246e6081072725822892f4bce5d0a26b02
hlin <henrylin2008@yahoo.com>
2020-03-05 22:57:26 -0800

    2nd solution

--- Strings/caesarCipherEncryptor.py
+++ Strings/caesarCipherEncryptor.py
@@ -27,4 +27,14 @@ def getNewLetter(letter, key):
 # Method 2:
 # Time: O(n)
 # Space: O(n)
-def ca
+def caesarCipherEncryptor(string, key):
+    newLetters = []
+    newKey = key % 26
+    alphabet = list("abcdefghijklmnopqrstuvwxyz")
+    for letter in string:
+        newLetters.append(getNewLetter(letter, newKey, alphabet))
+    return "".join(newLetters)
+
+def getNewLetter(letter, key, alphabet):
+    newLetterCode = alphabet.index(letter) + key
+    return alphabet[newLetterCode] if newLetterCode <= 25 else alphabet[-1 + newLetterCode % 25]
