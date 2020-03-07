b5132369ba77334ec5451998072198cbe0752da2
hlin <henrylin2008@yahoo.com>
2020-03-07 09:09:13 -0800

    Move element to end

---
+++ Arrays/moveElementToEnd.py
@@ -0,0 +1,22 @@
+# https://www.algoexpert.io/questions/Move%20Element%20To%20End
+# Move Element To End
+# You are given an array of integers and an integer. Write a function that moves all instances of that integer in the
+# array to the end of the array. The function should perform this in place and does not need to maintain the order of the
+# other integers.
+#
+# Sample input: [2, 1, 2, 2, 2, 3, 4, 2], 2
+# Sample output: [1, 3, 4, 2, 2, 2, 2, 2] (the numbers 1, 3, and 4 could be ordered differently)
+
+# Time: O(n); n = length of the array
+# Space O(1)
+# 
+def moveElementToEnd(array, toMove):
+    i = 0
+    j = len(array) - 1
+    while i < j:
+        while i < j and array[j] = toMove:
+            j -= 1
+        if array[i] = toMove:
+            array[i], array[j] = array[j], array[i]
+        i += 1
+    return array
