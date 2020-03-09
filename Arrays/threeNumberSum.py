627807a762629b32adc0334cb6742f15487bda5b
hlin <henrylin2008@yahoo.com>
2020-03-09 10:31:57 -0700

    update time, space, and comments

--- Arrays/threeNumberSum.py
+++ Arrays/threeNumberSum.py
@@ -8,25 +8,25 @@
 # Sample input: [12, 3, 1, 2, -6, 5, -8, 6], 0
 # Sample output: [[-8, 2, 6], [-8, 3, 5], [-6, 1, 5]]
 
-# Time: O(n^2);
-# space: O(n);
+# Time: O(n^2); one n is the for loop; 2 pointers (left & right) go through the array another loop
+# space: O(n); n: worse case is to pair all number in the array
 def threeNumberSum(array, targetSum):
-    array.sort()
-    triplets = []
-    for i in range(len(array) - 2):
-        left = i + 1
+    array.sort()  # sort the array
+    triplets = []  # set an empty array
+    for i in range(len(array) - 2): # range(len(array)-2): b/c look for 3 numbers, 2 pointers at the last number indexes
+        left = i + 1 # next value next to i
         right = len(array) - 1
-        while left < right:
-            currentSum = array[i] + array[left] + array[right]
+        while left < right: # before left and right pointers meet
+            currentSum = array[i] + array[left] + array[right] # sum = current index value + left pointer + right pointer
             if currentSum == targetSum:
-                triplets.append([array[i], array[left], array[right]])
+                triplets.append([array[i], array[left], array[right]]) # store 3 values to triplets array
                 left += 1
                 right -= 1
             elif currentSum < targetSum:
-                left += 1
+                left += 1 # incrase left pointer would give a greater currentSum
             elif currentSum > targetSum:
-                right -= 1
-    print(triplets)
+                right -= 1 # decrease right pointer would give a smaller currentSum
+    # print(triplets)
     return triplets
 
 # threeNumberSum([12, 3, 1, 2, -6, 5, -8, 6], 0)
