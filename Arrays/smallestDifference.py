f4ffa8d38c9095a73ea917a9b31e57c2876326fe
hlin <henrylin2008@yahoo.com>
2020-03-10 09:33:51 -0700

    Smallest Difference

---
+++ Arrays/smallestDifference.py
@@ -0,0 +1,9 @@
+# https://www.algoexpert.io/questions/Smallest%20Difference
+# Smallest Difference
+# Write a function that takes in two non-empty arrays of integers. The function should find the pair of numbers (one from
+# the first array, one from the second array) whose absolute difference is closest to zero. The function should return an
+# array containing these two numbers, with the number from the first array in the first position. Assume that there will
+# only be one pair of numbers with the smallest difference.
+#
+# Sample input: [-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]
+# Sample Output: [28, 26]
