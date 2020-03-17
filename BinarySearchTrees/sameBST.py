a15594903756b30d923addc70ad9ef9029527860
hlin <henrylin2008@yahoo.com>
2020-03-17 08:06:23 -0700

    Same BST

---
+++ BinarySearchTrees/sameBST.py
@@ -0,0 +1,9 @@
+# https://www.algoexpert.io/questions/Same%20BSTs
+# Same BST
+# An array of integers is said to represent the Binary Search Tree (BST) obtained by inserting each integer in the array
+# (from left to right) into the BST. Write a function that takes in two arrays of integers and returns a boolean
+# representing whether or not these arrays represent the same BST. Note that you are not allowed to construct any BSTs in
+# your code. A BST is a Binary Tree that consists only of BST nodes. A node is said to be a BST node if and only if it
+# satisfies the BST property: its value is strictly greater than the values of every node to its left; its value is less
+# than or equal to the values of every node to its right; and both of its children nodes are either BST nodes themselves
+# or None (null) values.
