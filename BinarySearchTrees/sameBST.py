88c8d6e8b444bc0b492b5d26304a13dfff4f767e
hlin <henrylin2008@yahoo.com>
2020-03-18 08:47:21 -0700

    update comments

--- BinarySearchTrees/sameBST.py
+++ BinarySearchTrees/sameBST.py
@@ -8,7 +8,7 @@
 # than or equal to the values of every node to its right; and both of its children nodes are either BST nodes themselves
 # or None (null) values.
 
-# Recursive calls
+# Method #1: recursive
 # Time: O(n^2)
 # Space: O(n^2)
 def sameBsts(arrayOne, arrayTwo):
@@ -21,6 +21,7 @@ def sameBsts(arrayOne, arrayTwo):
     if arrayOne[0] != arrayTwo[0]: # first element of 2 arrays must be the same (according to description)
         return False
 
+    # O(4N) and n times ==> O(n^2) time; same thing for space complexity
     leftOne = getSmaller(arrayOne) # left subtree of arrayOne, anything < root
     leftTwo = getSmaller(arrayTwo) # left subtree of arrayTwo
     rightOne = getBiggerOrEqual(arrayOne) # right subtree of arrayOne, anything >= root
@@ -29,14 +30,14 @@ def sameBsts(arrayOne, arrayTwo):
     return sameBsts(leftOne, leftTwo) and sameBsts(rightOne, rightTwo) # return True only if subtrees of left and right
                                             # are the same
 
-def getSmaller(array):
-    smaller = []
+def getSmaller(array): # array to store every value less than array[0]
+    smaller = []  # new array
     for i in range(1, len(array)):
         if array[i] < array[0]:
             smaller.append(array[i])
     return smaller
 
-def getBiggerOrEqual(array):
+def getBiggerOrEqual(array): # array to store every value >= array[0]
     biggerOrEqual = []
     for i in range(1, len(array)):
         if array[i] >= array[0]:
