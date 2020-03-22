5e5dd7eca9e76c102614722f2c70fc98372f058c
hlin <henrylin2008@yahoo.com>
2020-03-22 11:15:56 -0700

    update comments

--- BinaryTrees/branchSums.py
+++ BinaryTrees/branchSums.py
@@ -1,4 +1,6 @@
+# Branch Sums
 # https://www.algoexpert.io/questions/Branch%20Sums
+# Level: Easy
 # Write a function that takes in a Binary Tree and returns a list of its branch sums (ordered from leftmost branch sum to
 # rightmost branch sum). A branch sum is the sum of all values in a Binary Tree branch. A Binary Tree branch is a path of
 # nodes in a tree that starts at the root node and ends at any leaf node. Each Binary Tree node has a value stored in a
