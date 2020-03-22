56f767f57fb045dfb5f3d5ee7efc2cba3dabae8f
hlin <henrylin2008@yahoo.com>
2020-03-21 17:27:57 -0700

    update code and the comments

--- BinarySearchTrees/sameBST.py
+++ BinarySearchTrees/sameBST.py
@@ -8,7 +8,7 @@
 # than or equal to the values of every node to its right; and both of its children nodes are either BST nodes themselves
 # or None (null) values.
 
-# Method #1: recursive; easiness on reading the code
+# Method #1: recursive; easiness on reading the code; using array to store
 # Time: O(n^2); n is the number of nodes in each array
 # Space: O(n^2); n is the number of nodes in each array
 def sameBsts(arrayOne, arrayTwo):
@@ -31,14 +31,14 @@ def sameBsts(arrayOne, arrayTwo):
                                             # are the same
 
 def getSmaller(array): # array to store every value < array[0]
-    smaller = []  # new array
+    smaller = []  # new array to store every value smaller than first value of the array
     for i in range(1, len(array)):
         if array[i] < array[0]:
             smaller.append(array[i])
     return smaller
 
 def getBiggerOrEqual(array): # array to store every value >= array[0]
-    biggerOrEqual = []
+    biggerOrEqual = [] # new array to store every value that's equal or greater than first value of the array
     for i in range(1, len(array)):
         if array[i] >= array[0]:
             biggerOrEqual.append(array[i])
@@ -54,34 +54,41 @@ def getBiggerOrEqual(array): # array to store every value >= array[0]
 # Space: O(d); d is the depth of BST tree;
 
 def sameBsts(arrayOne, arrayTwo):
-    return areSameBSTs(arrayOne, arrayTwo, rootIdxOne, rootIdxTwo, float('-inf'), float('inf'))
+    return areSameBSTs(arrayOne, arrayTwo, 0, 0, float('-inf'), float('inf'))
 
 def areSameBSTs(arrayOne, arrayTwo, rootIdxOne, rootIdxTwo, minVal, maxVal):
-    if arrayOne[rootIdxOne] != arrayTwo[rootIdxTwo]:
-        return False
-
-    if rootIdxOne == -1 or rootIdxTwo == -1:
+    if rootIdxOne == -1 or rootIdxTwo == -1: # if no more root on one array, then no more root on the other array
         return rootIdxOne == rootIdxTwo
 
-    leftRootIdxOne = getIdxOfFirstSmaller(arrayOne, rootIdxOne, minVal)
+    if arrayOne[rootIdxOne] != arrayTwo[rootIdxTwo]: # if first value of both arrays are not the same, return false
+        return False
+
+    leftRootIdxOne = getIdxOfFirstSmaller(arrayOne, rootIdxOne, minVal) # get the index of first value smaller than the root
     leftRootIdxTwo = getIdxOfFirstSmaller(arrayTwo, rootIdxTwo, minVal)
     rightRootIdxOne = getIdxOfFirstBiggerOrEqual(arrayOne, rootIdxOne, maxVal)
+        # get the index of first value of equal or greater than the root
     rightRootIdxTwo = getIdxOfFirstBiggerOrEqual(arrayTwo, rootIdxOne, maxVal)
 
-    currentValue = arrayOne[rootIdxOne]
+    currentValue = arrayOne[rootIdxOne] # currentValue = current root value
     leftAreSame = areSameBSTs(arrayOne, arrayTwo, leftRootIdxOne, leftRootIdxTwo, minVal, currentValue)
     rightAreSame = areSameBSTs(arrayOne, arrayTwo, rightRootIdxOne, rightRootIdxTwo, currentValue, maxVal)
 
-    return leftAreSame and rightAreSame
+    return leftAreSame and rightAreSame # return True when left and right are the same
 
 
 def getIdxOfFirstSmaller(array, startingIndx, minVal):
+    # Find the index of the first smaller value after the startingIdx. Make sure that this value is greater than or equal
+    # to the minVal, which is the value of the previous parent node in the BST. If it isn't, then that value is located
+    # in the left subtree of the previous parent node.
     for i in range(startingIndx+1, len(array)):
         if array[i] < array[startingIndx] and array[i] >= minVal:
             return i
     return -1
 
 def getIdxOfFirstBiggerOrEqual(array, startingIdx, maxVal):
+    # Find the index of the first bigger/equal value after the startingIdx,. Make sure that this value is smaller than
+    # maxVal, which is the value of the previous parent node in the BST. If it isn't, then that value is located in the
+    # right subtree of the previous parent node.
     for i in range(startingIdx+1, len(array)):
         if array[i] >= array[startingIdx] and array[i] < maxVal:
             return i
