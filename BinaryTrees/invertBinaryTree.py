6a4cabd1c7da09fd01aca00c36f82997187221d5
hlin <henrylin2008@yahoo.com>
2020-03-22 10:45:46 -0700

    update comments

--- BinaryTrees/invertBinaryTree.py
+++ BinaryTrees/invertBinaryTree.py
@@ -1,4 +1,5 @@
 # Invert Binary Tree
+# Level: Medium
 # https://www.algoexpert.io/questions/Invert%20Binary%20Tree
 # Write a function that takes in a Binary Tree and inverts it. In other words, the function should swap every left node
 # in the tree for its corresponding (mirrored) right node. Each Binary Tree node has a value stored in a property called
@@ -23,7 +24,7 @@
 #              / \
 #             9   8
 
-# Method 1: iterative | Breadth first search, go through nodes level by level, swap left and right nodes, and add it
+# Method 1: iterative | Breadth first search, go through nodes level by level, swap left and right nodes, then append it
 # to the queue
 # Time: O(n)
 # Space: O(n)
@@ -31,12 +32,14 @@
 def invertBinaryTree(tree):
     queue = [tree]  # using queue to store nodes
     while len[queue]:   # if there's still node/s in the queue
-        current = queue.pop(0)  # current node is first node in the queue
-        if current is None:     # skip if the node is None
+        current = queue.pop(0)  # current is first node in the queue
+        if current is None:     # skip if the node is null node
             continue
-        swapLeftAndRight(current)   # helper function to swap left and right node
+        swapLeftAndRight(current)   # call helper function to swap left and right nodes
         queue.append(current.left)  # add left node to the queue
         queue.append(current.right) # add right node to the queue
 
 def swapLeftAndRight(tree):     # helper function that swap left and right nodes
     tree.left, tree.right = tree.right, tree.left
+
+
