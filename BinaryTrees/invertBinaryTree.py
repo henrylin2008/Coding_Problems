168c6a65d3124d2f2e8b2a9ffcf4ede728b14075
hlin <henrylin2008@yahoo.com>
2020-03-22 10:24:53 -0700

    update comments

--- BinaryTrees/invertBinaryTree.py
+++ BinaryTrees/invertBinaryTree.py
@@ -23,19 +23,20 @@
 #              / \
 #             9   8
 
-# Method 1: iterative | Breadth first search,
+# Method 1: iterative | Breadth first search, go through nodes level by level, swap left and right nodes, and add it
+# to the queue
 # Time: O(n)
 # Space: O(n)
 
 def invertBinaryTree(tree):
-    queue = [tree]
-    while len[queue]: # if queue is not empty, continue execute following code
-        current = queue.pop(0)
-        if current is None:
+    queue = [tree]  # using queue to store nodes
+    while len[queue]:   # if there's still node/s in the queue
+        current = queue.pop(0)  # current node is first node in the queue
+        if current is None:     # skip if the node is None
             continue
-        swapLeftAndRight(current)
-        queue.append(current.left)
-        queue.append(current.right)
+        swapLeftAndRight(current)   # helper function to swap left and right node
+        queue.append(current.left)  # add left node to the queue
+        queue.append(current.right) # add right node to the queue
 
-def swapLeftAndRight(tree):
-    tree.left, tree.right = tree.right, tree.left  # swap left and right nodes
+def swapLeftAndRight(tree):     # helper function that swap left and right nodes
+    tree.left, tree.right = tree.right, tree.left
