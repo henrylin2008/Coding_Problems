04b393be3d17340b77193dde2d22d75483b0d443
hlin <henrylin2008@yahoo.com>
2020-03-26 22:23:37 -0700

    Number of ways to Make Change code

--- Dynamic Programming/numberOfWaysToMakeChange.py
+++ Dynamic Programming/numberOfWaysToMakeChange.py
@@ -7,3 +7,14 @@
 
 # Sample input: 6, [1, 5]
 # Sample output: 2 (1x1 + 1x5 and 6x1)
+
+# Time: O(nd)
+# Space: O(n)
+def numberOfWaysToMakeChange(n, denoms):
+    ways = [0 for amount in range(n+1)]
+    way[0] = 1
+    for denom in denoms:
+        for amount in range(1, n+1):
+            if denom <= amount:
+                ways[amount] += ways[amount - denom]
+    return ways[n]
