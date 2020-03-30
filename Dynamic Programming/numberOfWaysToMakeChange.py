a961c90cd88d80271d2d57f0ff9df36b18d82eec
hlin <henrylin2008@yahoo.com>
2020-03-30 08:45:55 -0700

    adding description for each line

--- Dynamic Programming/numberOfWaysToMakeChange.py
+++ Dynamic Programming/numberOfWaysToMakeChange.py
@@ -8,13 +8,21 @@
 # Sample input: 6, [1, 5]
 # Sample output: 2 (1x1 + 1x5 and 6x1)
 
-# Time: O(nd)
-# Space: O(n)
+
+# Time: O(nd); d is number of denomination
+# Space: O(n); n is target amount
+
 def numberOfWaysToMakeChange(n, denoms):
-    ways = [0 for amount in range(n+1)]
-    way[0] = 1
-    for denom in denoms:
-        for amount in range(1, n+1):
-            if denom <= amount:
-                ways[amount] += ways[amount - denom]
+    ways = [0 for amount in range(n+1)] # build an array (0) represents ways
+    ways[0] = 1 # only one way to get 0, doing nothing
+    for denom in denoms: # for every value in denoms
+        for amount in range(1, n+1): # for every amount upto n (given amount)
+            if denom <= amount: # when the denom is less or equal to the amount
+                # print('ways[amount], ways[denom], ways[amount-denom]:', ways[amount], ways[denom], ways[amount-denom])
+                ways[amount] += ways[amount - denom] # ways of current amount + ways for previous amount
+                # print('ways[amount]:', ways[amount])
+                # print()
+    # print("ways[n]", ways[n])
     return ways[n]
+
+# numberOfWaysToMakeChange(6, [1,5])
