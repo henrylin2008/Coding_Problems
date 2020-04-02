3feca000c2cb3c4d6de98247ffae957005ddeaab
hlin <henrylin2008@yahoo.com>
2020-04-02 08:53:59 -0700

    Number of ways to Make Change

---
+++ Dynamic Programming/minNumberOfCoinsForChange.py
@@ -0,0 +1,19 @@
+# Min Numbers of Coins For change;
+# https://www.algoexpert.io/questions/Min%20Number%20Of%20Coins%20For%20Change
+# Difficutly: Medium
+
+# Given an array of positive integers representing coin denominations and a
+# single no n -negative integer n representing a target amount of
+# money, write a function that returns the smallest number of coins needed to
+# make change for that target amount using the given coin denominations.
+# If it's impossible to make change for the target amount, return -1
+#
+# Note that an unlimited amount of coins is at your disposal.
+#
+# Sample Input:
+# n = 7
+# denoms = [1, 5, 10]
+
+# Sample Output:
+# 3
+
