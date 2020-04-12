58c6d8e9bbf1b2aae624a67f3815cc115f6f0173
hlin <henrylin2008@yahoo.com>
2020-04-12 10:05:57 -0700

    update comments

--- Stack/asteroidCollision.py
+++ Stack/asteroidCollision.py
@@ -46,11 +46,23 @@
 
 # Time: O(N); N is number of asteroid
 # Space: O(N); where stack (ans) space
+#
 # Idea: if new item is > 0, push it to stack, b/c it will always going to the right and not gonna collide with last item
 # if abs(new) < res[last]: remove new item
 # if abs(new) == res[last], remove both items
 # if abs(new) > res[last], remove last item (in while loop to go through rest of values in the stack)
 # append anything except abs(new) >= res[-1]
+
+# 方法：栈
+# 如果不会发生碰撞那么一排小行星是处于稳定的状态。若在右边增加一个新的小行星后，在它再次稳定之前，可能会发生更多的碰撞，而所有的这些碰撞（如果发生的话）
+# 都必须从右到左发生。这种情况非常适合用栈解决。
+#
+# 算法：
+#
+# 假设栈中顶部元素为 top，一个新的小行星 new 进来了。如果 new 向右移动（new>0），或者 top 向左移动（top<0），则不会发生碰撞。
+# 否则，如果 abs(new) < abs(top)，则新小行星 new 将爆炸；如果 abs(new) == abs(top)，则两个小行星都将爆炸；如果 abs(new) > abs(top)，
+# 则 top 小行星将爆炸（可能还会有更多小行星爆炸，因此我们应继续检查）
+
 
 class Solution(object):
     def asteroidCollision(self, asteroids):
