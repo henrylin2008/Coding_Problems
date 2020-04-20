34f5839fd17d8548127bbb8675ad17e7fcf50c68
hlin <henrylin2008@yahoo.com>
2020-04-19 17:31:41 -0700

    adding comments

--- Stack/MinMaxStack.py
+++ Stack/MinMaxStack.py
@@ -30,30 +30,50 @@
 # getMax(): 5
 # peek(): 5
 
+# O(1) time | O(1) space
+# idea: using 2 stacks, one stack is keep track of current stack, the other one (minMaxStack) keeps track of min and max
+# at any given point;
+
 class MinMaxStack:
 
     def __init__(self):
         self.minMaxStack = []
         self.stack = []
 
-    def peek(self):
+    # O(1) time | O(1) space
+    def peek(self):  # check last(est) item in the stack
         return self.stack[len(self.stack) - 1]
 
-    def pop(self):
+    # O(1) time | O(1) space
+    def pop(self): # remove last item from the stack
         self.minMaxStack.pop()
         return self.stack.pop()
 
-    def push(self, number):
-        newMinMax = {"min": number, "max": number}
-        if len(self.minMaxStack):
-            lastMinMax = self.minMaxStack[len(self.minMaxStack) - 1]
-            newMinMax["min"] = min(lastMinMax["min"], number)
-            newMinMax["max"] = max(lastMinMax["max"], number)
-        self.minMaxStack.append(newMinMax)
-        self.stack.append(number)
+    # O(1) time | O(1) space
+    def push(self, number): # adding new item to the stack
+        newMinMax = {"min": number, "max": number} # use a directory/Hash table to store min and max values; it's true
+                    # only if it's pushing first number to the stack, or number is first number pushing into the stack
+        if len(self.minMaxStack): # if minMaxStack is not empty or there're values in the stack
+            lastMinMax = self.minMaxStack[len(self.minMaxStack) - 1] # peeking last value of minMaxStack
+            newMinMax["min"] = min(lastMinMax["min"], number) # find min by comparing min of lastMinMax value and the number
+            newMinMax["max"] = max(lastMinMax["max"], number) # find max by comparing max of lastMinMax value and the number
+        self.minMaxStack.append(newMinMax) # if it's an empty stack, add newMinMax value to the minMaxStack
+        self.stack.append(number) # if it's an empty stack, insert the number into the main stack
 
+    # O(1) time | O(1) space
     def getMin(self):
         return self.minMaxStack[len(self.minMaxStack) - 1]["min"]
 
+    # O(1) time | O(1) space
     def getMax(self):
         return self.minMaxStack[len(self.minMaxStack) - 1]["max"]
+
+#
+# MinMaxStack.push(1,5)
+# MinMaxStack.push(1,10)
+# MinMaxStack.push(1,4)
+# MinMaxStack.pop(4)
+# MinMaxStack.getMin()
+# print(MinMaxStack)
+# MinMaxStack.getMax()
+# print(MinMaxStack)
