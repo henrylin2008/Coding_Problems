b0c0420a53e7184f3224a2d66f3568a064710c05
hlin <henrylin2008@yahoo.com>
2020-04-20 22:34:12 -0700

    adding code and comments

--- Stack/balancedBrackets.py
+++ Stack/balancedBrackets.py
@@ -15,3 +15,33 @@
 #
 # Sample Output:
 # true
+
+# O(n) time | O(n) space
+# 3 cases unbalance:
+# 1. if stack is not empty by end of string traversal, the string is unbalance, or there are opening brackets
+# 2. traverse the string and a closing bracket left, there's nothing to match against, therefore it's unbalanced
+# 3. final opening bracket does not match with the closing bracket
+
+def balancedBrackets(string):
+    openingBrackets = "{[("  # string/array of all opening brackets
+    closingBrackets = "}])"  # string/array of all closing brackets
+    matchingBrackets = {")":"(", "]":"[", "}":"{"} # use a dictionary to map closing bracket with corresponding opening bracket
+    stack = [] # empty array
+    for char in string:
+        if char in openingBrackets: # if current char is in openingBrackets, then store it to the stack
+            stack.append(char)
+        elif char in closingBrackets: # if current char in closingBrackets:
+            if len(stack) == 0:  # if stack is empty: return False
+                print("False")
+                return False
+            elif stack[-1] == matchingBrackets[char]: # if top item in stack matches corresponding closing bracket
+                stack.pop() # pop top item from the stack
+            else:  # otherwise, it's a unbalanced stack, then return False
+                print("False")
+                return False
+    print("true")
+    return len(stack) == 0 # return True if the stack is empty; otherwise there's unmatched bracket/s
+
+
+# balancedBrackets("(()[[{{}}]})")
+# balancedBrackets("()[]{}{")
