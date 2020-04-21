e30e62626b605d9d984f427d4448480a37eae510
hlin <henrylin2008@yahoo.com>
2020-04-21 11:24:58 -0700

    added code and comments

--- FamousAlgorithms/maxSubarraySum.py
+++ FamousAlgorithms/maxSubarraySum.py
@@ -11,11 +11,23 @@
 # Sample Output
 # 19
 
+# Kadane's Algorithm
 # maxEndingHere = max(maxEndingHere + num, num)
 # maxSoFar = max(maxSoFar, maxEndingHere)
 
-# Sample input:  [3,5,-9,|1,3,-2,3,4,7,2,-9,6,3,1,| -5,4]
-# maxEndingHere: [3,8,-1,|1,4,2,5,9,16,18,9,15,18,19,14,18]
-# maxSoFar:      [3,8, 8, 8,8,8,8,9,16,18,18,18,18,19,19,19]
+# Sample input:  [3,5,-9,|1,3,-2,3,4,7,2,-9,6,3,1,|    -5,4]
+# maxEndingHere: [3,8,-1,|1,4,2,5,9,16,18,9,15,18,19,| 14,18]
+# maxSoFar:      [3,8, 8, 8,8,8,8,9,16,18,18,18,18,19, 19,19]
 
+# Time: O(N); N = length of array
+# Space: O(1); only stores maxEndingHere and maxSoFar values
 def maxSubarraySum(array):
+    maxEndingHere = array[0] # first item in array as maxEndingHere
+    maxSoFar = array[0] # first item in array as maxSoFar
+    for num in array[1:]:
+        maxEndingHere = max(num, maxEndingHere + num) # max of current num and sum of current num and last maxEndingHere value
+        maxSoFar = max(maxSoFar, maxEndingHere)
+    # print(maxSoFar)
+    return maxSoFar
+
+# maxSubarraySum([2,3,5,-15,3,2,3,4,-7])
