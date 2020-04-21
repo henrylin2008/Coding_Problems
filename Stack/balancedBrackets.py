eecb09d262d5cb06d02ba8e2397f60e931ff0498
hlin <henrylin2008@yahoo.com>
2020-04-20 21:10:30 -0700

    Balanced Brackets

---
+++ Stack/balancedBrackets.py
@@ -0,0 +1,17 @@
+# Balanced Brackets:
+# https://www.algoexpert.io/questions/Balanced%20Brackets
+# Difficulty: Medium
+#
+# Write a function that takes in a string made up brackets ((,[,{,),],and})) and other oeprational characters. The function should return
+# a boolean representing wheather the string is balanced with regards to brackets.
+#
+# A string is said to be balanced if it has as many opening brackets or a certain type as it has closing brackets of that
+# type and if no bracket is unmatched. Note that an opening bracket can't match a corresponding closing bracket that comes
+# before it, and similarly, a closing bracket can't match a corresponding opening bracket that comes after it. Also, brackets
+# can't overlap each other as in [(])
+
+# Sample Input:
+# string = "([])(){}(())()()"
+#
+# Sample Output:
+# true
