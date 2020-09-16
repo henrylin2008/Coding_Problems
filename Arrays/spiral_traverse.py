4d529a2ceac1ad09fd8aa56603df9792031e8c4a
hlin <henrylin2008@yahoo.com>
2020-09-15 23:22:50 -0700

    adding recursive solution

--- Arrays/spiral_traverse.py
+++ Arrays/spiral_traverse.py
@@ -17,39 +17,76 @@
 # Sample Output:
 # [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 
-# Solution #1
+# Solution #1: iterative
 # O(n) time | O(n) space - where n is the total number of elements in the array
-def spiralTraverse(array):
+# def spiralTraverse(array):
+#     result = [] # Empty list
+#     startRow, endRow = 0, len(array) - 1
+#     startCol, endCol = 0, len(array[0]) - 1 # 0, inner array
+#
+#     while startRow <= endRow and startCol <= endCol:
+#         for col in range(startCol, endCol + 1): # first row from the left to the right
+#             result.append(array[startRow][col])
+#
+#         for row in range(startRow + 1, endRow + 1): # last column from second row to last row
+#             result.append(array[row][endCol])
+#
+#         for col in reversed(range(startCol, endCol)):   # last row: from second to last column to the first column
+#             # Handle the edge case when there's a single row in the middle
+#             # of the matrix. In this case, we don't want to double-count the values
+#             # in this row, which we've already counted in the first for loop above.
+#             if startRow == endRow: # avoid double-counting single row
+#                 break
+#             result.append(array[endRow][col])
+#
+#         for row in reversed(range(startRow + 1, endRow)):   # first column: from second to last row to the second row
+#             # Handle the edge case when there's a single column in the middle
+#             # of the matrix. In this case, we don't want to double-count the values
+#             # in this column, which we've already counted in the second for loop above.
+#             if startCol == endCol: # avoid double-counting single column
+#                 break
+#             result.append(array[row][startCol])
+#
+#         # pushing the boundaries inward
+#         startRow += 1 # move to second row
+#         endRow -= 1 # move to the row before last
+#         startCol += 1   # move to second column
+#         endCol -= 1 # move last column to the column before
+#
+#     return result
+
+
+# Solution #2: Recursive
+# O(n) time | O(n) space
+def spiralTraverse2(array):
     result = []
-    startRow, endRow = 0, len(array) - 1
-    startCol, endCol = 0, len(array[0]) - 1
-
-    while startRow <= endRow and startCol <= endCol:
-        for col in range(startCol, endCol + 1):
-            result.append(array[startRow][col])
-
-        for row in range(startRow + 1, endRow + 1):
-            result.append(array[row][endCol])
-
-        for col in reversed(range(startCol, endCol)):
-            # Handle the edge case when there's a single row in the middle
-            # of the matrix. In this case, we don't want to double-count the values
-            # in this row, which we've already counted in the first for loop above.
-            if startRow == endRow:
-                break
-            result.append(array[endRow][col])
-
-        for row in reversed(range(startRow + 1, endRow)):
-            # Handle the edge case when there's a single column in the middle
-            # of the matrix. In this case, we don't want to double-count the values
-            # in this column, which we've already counted in the second for loop above.
-            if startCol == endCol:
-                break
-            result.append(array[row][startCol])
-
-        startRow += 1
-        endRow -= 1
-        startCol += 1
-        endCol -= 1
-
+    spiralFill(array, 0, len(array) - 1, 0, len(array[0])-1, result)
     return result
+
+def spiralFill(array, startRow, endRow, startCol, endCol, result):
+    if startRow > endRow or startCol > endCol:
+        return
+
+    for col in range(startCol, endCol + 1): # first row from the left to the right
+        result.append(array[startRow][col])
+
+    for row in range(startRow + 1, endRow + 1): # last column from second row to last row
+        result.append(array[row][endCol])
+
+    for col in reversed(range(startCol, endCol)):   # last row: from second to last column to the first column
+        # Handle the edge case when there's a single row in the middle
+        # of the matrix. In this case, we don't want to double-count the values
+        # in this row, which we've already counted in the first for loop above.
+        if startRow == endRow: # avoid double-counting single row
+            break
+        result.append(array[endRow][col])
+
+    for row in reversed(range(startRow + 1, endRow)):   # first column: from second to last row to the second row
+        # Handle the edge case when there's a single column in the middle
+        # of the matrix. In this case, we don't want to double-count the values
+        # in this column, which we've already counted in the second for loop above.
+        if startCol == endCol: # avoid double-counting single column
+            break
+        result.append(array[row][startCol])
+
+    spiralFill(array, startRow+1, endRow-1, startCol+1, endCol-1, result)
