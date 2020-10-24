ccd51fffd1f90ad6edda7cd2006ddc5608b7fdd1
hlin <henrylin2008@yahoo.com>
2020-10-24 11:03:01 -0700

    added the comments

--- Arrays/longestPeak.py
+++ Arrays/longestPeak.py
@@ -16,25 +16,30 @@
 # sample output
 # 6 // [0, 10, 6, 5, -1, -3]
 
-# Time: O(N)
+# Time: O(n)
 # Space: O(1)
+# logic: find the peak, then expanding to the left and expanding to the right, and calculate longest Peak length
 def longestPeak(array):
-    longestPeakLength = 0
-    i = 1
+    longestPeakLength = 0  # initialize the longest length
+    i = 1  # initial starting point at index 1, per definition of Peak
     while i < len(array) - 1:
-        isPeak = array[i-1] < array[i] and array[i] > array[i+1]
-        if not isPeak:
+        isPeak = array[i - 1] < array[i] and array[i] > array[i + 1]  # compare current value with adjacent values
+        if not isPeak:  # if it's not a peak, moving along
             i += 1
             continue
 
-        leftIdx = i - 2
+        # expanding to the left from the peak
+        leftIdx = i - 2  # left starting point from the peak
         while leftIdx >= 0 and array[leftIdx] < array[leftIdx + 1]:
-            leftIdx -= 1
-        rightIdx = i + 2
+            # leftIdx is still inbound, left value < right value
+            leftIdx -= 1  # continue expanding to the left
+        # expanding to the right from the peak
+        rightIdx = i + 2  # right starting point from the peak
         while rightIdx < len(array) and array[rightIdx] < array[rightIdx - 1]:
-            rightIdx += 1
+            # rightIdx still inbound, left value > right value
+            rightIdx += 1  # continue expanding to the right
 
-        currentPeakLength = rightIdx - leftIdx - 1
-        longestPeakLength = max(longestPeakLength, currentPeakLength)
-        i = rightIdx
+        currentPeakLength = rightIdx - leftIdx - 1  # current longest Peak length
+        longestPeakLength = max(longestPeakLength, currentPeakLength)  # set the longest Peak length
+        i = rightIdx    # new starting point (no need to explore visited values)
     return longestPeakLength
