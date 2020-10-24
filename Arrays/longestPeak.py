84a4f2b291d0c6c73a11b6b522d8ece9504bc423
hlin <henrylin2008@yahoo.com>
2020-10-24 11:04:03 -0700

    updated the format

--- Arrays/longestPeak.py
+++ Arrays/longestPeak.py
@@ -2,10 +2,10 @@
 # Category: Arrays
 # Link: https://www.algoexpert.io/questions/Longest%20Peak
 
-# Longest Peak
-# Write a function that takes in an array of integers and returns the lenght of the longest peak in the array
-# A peak is defined as adjacent integers in the array that are strictly increasing until they reach a tip (the heightest
-# value in the peak), at which point they become strictly decreasing. At least three integers are required to from a peak.
+# Longest Peak Write a function that takes in an array of integers and returns the length of the longest peak in the
+# array A peak is defined as adjacent integers in the array that are strictly increasing until they reach a tip (the
+# greatest value in the peak), at which point they become strictly decreasing. At least three integers are required
+# to from a peak.
 
 # For example, the integer 1, 4, 10, 2 form a peak, but the integers 4, 0, 10 don't and neither do the integers 1,2,2,0.
 # Similarly, the integers 1,2,3 don't form a peak because there aren't any strictly decreasing integers after the 3.
