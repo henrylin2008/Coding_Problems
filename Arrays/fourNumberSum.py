70f92c05e9c36b695bd4b84507ef242f08cb3f8e
hlin <henrylin2008@yahoo.com>
2020-10-25 23:13:31 -0700

    added code, time and space complexity

--- Arrays/fourNumberSum.py
+++ Arrays/fourNumberSum.py
@@ -14,3 +14,23 @@
 # Sample Output
 # [[7, 6, 4, -1], [7, 6, 1, 2]] // the quadruplets could be ordered differently
 
+# Time: O(n^2)
+# Space: O(n^2)
+
+def fourNumberSum(array, targetSum):
+    allPairSums = {}
+    quadruplets = []
+    for i in range(1, len(array) - 1):
+        for j in range(i+1, len(array)):
+            currentSum = array[i] + array[j]
+            difference = targetSum - currentSum
+            if difference in allPairSums:
+                for pair in allPairSums[difference]:
+                    quadruplets.append(pair + [array[i], array[j]])
+        for k in range(0, i):
+            currentSum = array[i] + array[k]
+            if currentSum not in allPairSums:
+                allPairSums[currentSum] = [[array[k], array[i]]]
+            else:
+                allPairSums[currentSum].append([array[k], array[i]])
+    return quadruplets
