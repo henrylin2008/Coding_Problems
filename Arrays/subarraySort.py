e27151b4deb3b1c34e5383134c49269cd0bfac69
hlin <henrylin2008@yahoo.com>
2020-10-28 08:58:31 -0700

    subarray sort

---
+++ Arrays/subarraySort.py
@@ -0,0 +1,14 @@
+# Subarray Sort
+# Difficulty: Hard
+# Link: https://www.algoexpert.io/questions/Subarray%20Sort
+
+# Write a function that takes in an array of at least two integers and that returns an array of the starting and ending
+# indices of the smallest subarray in the input array that needs to be sorted in place in order for the entire input
+# array to be sorted (in ascending order).
+# If the input array is already sorted, the function should return [-1, 1].
+
+# Sample input:
+# array = [1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19]
+
+# sample output:
+# [3, 9]
