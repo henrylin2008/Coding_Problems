5e59943af2069ddc30f39886bfc88ae6527b5caf
hlin <henrylin2008@yahoo.com>
2020-10-27 22:44:42 -0700

    Validate subsequence

---
+++ Arrays/isValidSubsequence.py
@@ -0,0 +1,21 @@
+# Validate Subsequence
+# Difficulty: Easy
+# link: https://www.algoexpert.io/questions/Validate%20Subsequence
+
+# Given two non-empty arrays of integers, write a function that determines whether the second array is a subsequence
+# of the first one.
+# A subsequence of an array is a set of numbers that aren't necessarily adjacent in the array but that they are in the
+# same order as they appear in the array. For instance, the numbers [1, 3, 4] form a subsequence of the array [1,2,3,4],
+# and so do the numbers [2, 44]. Note that a single number in an array and the array itself are both valid subsequences
+# of the array.
+
+# Sample input:
+# array = [5, 1, 22, 25, 6, -1, 8, 10]
+# sequence = [1, 6, -1, 10]
+
+# Sample output
+# true
+
+# Time:
+# Space:
+def isValidSubsequence(array, sequence):
