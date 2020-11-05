4904feb7c408bb1697a001c827a07cd9ca8a7fb5
hlin <henrylin2008@yahoo.com>
2020-11-05 09:37:43 -0800

    added comments

--- Arrays/largestRange.py
+++ Arrays/largestRange.py
@@ -18,30 +18,33 @@
 # Sample Output
 # [0, 7]
 
-# Time: O(n)
-# Space: O(n)
+# Time: O(n); 2 for loops, each loop is O(n) time, 2O(n) ==> O(n)
+# Space: O(n); store every num in the hash table (nums)
+# Solution: store all the values from the array into a hash table, and set every value as True (not visited) initially;
+# Once the value has been explored, set the value to False (visited); return the first and last value in the range
 def largestRange(array):
-    bestRange = []
-    longestLength = 0
-    nums = {}
+    bestRange = []  # largest range that holds first num and last num in the range
+    longestLength = 0   # longest length
+    nums = {}   # hash table to hold all the numbers in the array
     for num in array:
-        nums[num] = True
+        nums[num] = True    # set every num to True
     for num in array:
-        if not nums[num]:
-            continue
-        nums[num] = False
-        currentLength = 1
-        left = num - 1
-        right = num + 1
-        while left in nums:
-            nums[left] = False
-            currentLength += 1
-            left -= 1
-        while right in nums:
-            nums[right] = False
-            currentLength += 1
-            right += 1
+        if not nums[num]:   # check if the num has been explored, if current num == False
+            continue    # skip
+        nums[num] = False   # set the value to False
+        currentLength = 1   # current length = current num so far
+        left = num - 1  # checking/exploring the num on the left
+        right = num + 1  # checking the num on the right
+        while left in nums:  # while left num is in the hash table (True)
+            nums[left] = False  # set the left num to False
+            currentLength += 1  # increase the currentLength
+            left -= 1   # continue expanding to the left
+        while right in nums:    # while right num is in the hash table (True)
+            nums[right] = False  # set the right num to False
+            currentLength += 1  # increase the currentLength
+            right += 1  # continue expanding to the right
         if currentLength > longestLength:
-            longestLength = currentLength
-            bestRange = [left+1, right-1]
+            longestLength = currentLength   # set new longest length = current length
+            bestRange = [left + 1, right - 1]   # left + 1: b/c left -= 1 in while (left in nums) loop
+            # ex: [0, 7], left = 0 - 1 ==> -1 (exploring the left) ==> left += 1
     return bestRange
