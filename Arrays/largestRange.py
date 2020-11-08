4e59dd5692a966fb2afceb898dc5b6f746fc7d36
hlin <henrylin2008@yahoo.com>
2020-11-07 22:29:19 -0800

    update format

--- Arrays/largestRange.py
+++ Arrays/largestRange.py
@@ -24,27 +24,27 @@
 # Once the value has been explored, set the value to False (visited); return the first and last value in the range
 def largestRange(array):
     bestRange = []  # largest range that holds first num and last num in the range
-    longestLength = 0   # longest length
-    nums = {}   # hash table to hold all the numbers in the array
+    longestLength = 0  # longest length
+    nums = {}  # hash table to hold all the numbers in the array
     for num in array:
-        nums[num] = True    # set every num to True
+        nums[num] = True  # set every num to True
     for num in array:
-        if not nums[num]:   # check if the num has been explored, if current num == False
-            continue    # skip
-        nums[num] = False   # set the value to False
-        currentLength = 1   # current length = current num so far
+        if not nums[num]:  # check if the num has been explored, if current num == False
+            continue  # skip
+        nums[num] = False  # set the value to False
+        currentLength = 1  # current length = current num so far
         left = num - 1  # checking/exploring the num on the left
         right = num + 1  # checking the num on the right
         while left in nums:  # while left num is in the hash table (True)
             nums[left] = False  # set the left num to False
             currentLength += 1  # increase the currentLength
-            left -= 1   # continue expanding to the left
-        while right in nums:    # while right num is in the hash table (True)
+            left -= 1  # continue expanding to the left
+        while right in nums:  # while right num is in the hash table (True)
             nums[right] = False  # set the right num to False
             currentLength += 1  # increase the currentLength
             right += 1  # continue expanding to the right
         if currentLength > longestLength:
-            longestLength = currentLength   # set new longest length = current length
-            bestRange = [left + 1, right - 1]   # left + 1: b/c left -= 1 in while (left in nums) loop
+            longestLength = currentLength  # set new longest length = current length
+            bestRange = [left + 1, right - 1]  # left + 1: b/c left -= 1 in while (left in nums) loop
             # ex: [0, 7], left = 0 - 1 ==> -1 (exploring the left) ==> left += 1
     return bestRange
