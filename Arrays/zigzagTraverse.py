dedff6ecc0ec01383df12d358eb3984366386675
hlin <henrylin2008@yahoo.com>
2020-11-10 10:03:00 -0800

    added the comments

--- Arrays/zigzagTraverse.py
+++ Arrays/zigzagTraverse.py
@@ -18,38 +18,42 @@
 # Sample Output:
 # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
-# Time: O(n)
-# Space: O(n), where n is the total number of elements in the two-dimenseional array
+# Time: O(n), where n is the total number of elements in the two-dimensional array
+# Space: O(n), where n is the total number of elements in the two-dimensional array
 def zigzagTraverse(array):
     height = len(array) - 1
-    width = len(array[0]) - 1
-    result = []
-    row, col = 0, 0
-    goingDown = True
-    while not isOutOfBounds(row, col, height, width):
-        result.append(array[row][col])
-        if goingDown:
-            if col == 0 or row == height:
-                goingDown = False
-                if row == height:
-                    col += 1
-                else:
-                    row += 1
-            else:
-                row += 1
-                col -= 1
-        else:
-            if row == 0 or col == width:
-                goingDown = True
-                if col == width:
-                    row += 1
-                else:
-                    col += 1
-            else:
-                row -= 1
-                col += 1
+    width = len(array[0]) - 1   # length of first subarray - 1
+    result = []     # append the values into this array
+    row, col = 0, 0     # current/starting position
+    goingDown = True    # goingDown direction
+    while not isOutOfBounds(row, col, height, width):   # while is not out of bounds
+        result.append(array[row][col])  # append current value to the result array (since it's inbound)
+        if goingDown:   # if it's going down
+            if col == 0 or row == height:   # if first column or last row
+                goingDown = False   # changing direction, going up
+                if row == height:   # if we're at the bottom of the array
+                    col += 1    # moves to the right
+                else:   #
+                    row += 1    # keeps going down
+            else:   # not first column or last row, but going down
+                row += 1    # down 1
+                col -= 1    # left 1
+        else:   # if going up
+            if row == 0 or col == width:    # if top row or last column
+                goingDown = True    # going down
+                if col == width:    # if in final column
+                    row += 1    # going down by 1
+                else:   # if col != width: first row but not on the top right corner
+                    col += 1    # going right by 1
+            else:   # in middle of the array, not on
+                row -= 1    # going up by 1
+                col += 1    # going right by 1
     return result
 
 
 def isOutOfBounds(row, col, height, width):
     return row < 0 or row > height or col < 0 or col > width
+    # row < 0: too high up, or row before the first row
+    # row > height: too low, or out of bound from the bottom row
+    # col < 0: too far from the left, or col left to the first column
+    # col > width: too far from the right, or column after the last column
