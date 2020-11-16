3f390c4556e7c5209044cd7f843382e3abe47398
hlin <henrylin2008@yahoo.com>
2020-11-15 20:25:02 -0800

    added code and the comments

--- Arrays/product_of_all_other_elements.py
+++ Arrays/product_of_all_other_elements.py
@@ -18,14 +18,39 @@
 # before i and the product of numbers after i. Then we could multiply those two numbers to get our desired product.
 #
 # In order to find the product of numbers before i, we can generate a list of prefix products. Specifically, the ith
-# element in the list would be a product of all numbers including i. Similarly, we would generate the list of suffix products.
+# element in the list would be a product of all numbers including i. Similarly, we would generate the list of suffix
+# products. Finally, for each index we can multiply the appropriate prefix and suffix values to obtain our solution.
 
-def products(nums):
+# Time: O(n)
+# Space: O(n)
+# since iterating over the input arrays takes O(N) time and creating the prefix and suffix arrays take up O(N) space.
+def productsExceptSelf(nums):
     # Generate prefix products
-
+    prefix_products = []
+    for n in nums:
+        if prefix_products:
+            prefix_products.append(prefix_products[-1] * n)
+        else:
+            prefix_products.append(n)
 
     # Generate suffix products
+    suffix_products = []
+    for n in reversed(nums):
+        if suffix_products:
+            suffix_products.append(suffix_products[-1] * n)
+        else:
+            suffix_products.append(n)
+    suffix_products = list(reversed(suffix_products))
 
     # Generate result
+    result = []
+    for i in range(len(nums)):
+        if i == 0:
+            result.append(suffix_products[i + 1])
+        elif i == len(nums) - 1:
+            result.append(prefix_products[i - 1])
+        else:
+            result.append(prefix_products[i - 1] * suffix_products[i + 1])
+    return result
 
-# This runs in O(N) time and space, since iterating over the input arrays takes O(N) time and creating the prefix and suffix arrays take up O(N) space.
+
