be072c6573e719e2398ae480701f9dd347b442d7
hlin <henrylin2008@yahoo.com>
2020-11-19 22:47:59 -0800

    added 2 solutions

--- Arrays/Min_rewards.py
+++ Arrays/Min_rewards.py
@@ -20,3 +20,32 @@
 # Sample Output:
 #   25 // you would give out the following rewards: [4, 3, 2, 1, 2, 3, 4, 5, 1]
 
+
+# Solution #2
+# Time: O(n)
+# Space: O(n) - where n is the length of the input array
+def minRewards(scores):
+    rewards = [1 for _ in scores]
+    for i in range(1, len(scores)):
+        if scores[i] > scores[i-1]:
+            rewards[i] = rewards[i - 1] + 1
+    for i in reversed((range(len(scores) - 1))):
+        if scores[i] > scores[i+1]:
+            rewards[i] = max(rewards[i], rewards[i + 1]+ 1)
+    return sum(rewards)
+
+# Solution #1
+# Time: O(n^2)
+# Space: O(n); where n is the length of the input array
+# def minRewards(scores):
+#     rewards = [1 for _ in scores]
+#     for i in range(1, len(scores)):
+#         j = i - 1
+#         if scores[i] > scores[j]:
+#             rewards[i] = rewards[j] + 1
+#         else:
+#             while j >= 0 and scores[j] > scores[j + 1]:
+#                 rewards[j] = max(rewards[j], rewards[j + 1] + 1)
+#                 j -= 1
+#     return sum(rewards)
+
