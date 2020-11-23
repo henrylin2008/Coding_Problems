bb8780e0f8375edb11ba43dfe6454492c04c5367
hlin <henrylin2008@yahoo.com>
2020-11-22 22:52:18 -0800

    test for staticmethod

--- cls_vs_static.py
+++ cls_vs_static.py
@@ -17,14 +17,20 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
 
-    @classmethod
+    @classmethod    # like a constructor, build on top of inits
     def set_raise_amt(cls, amount):
         cls.raise_amt = amount
 
-    @classmethod
+    @classmethod    # constructor to split input string with '-'
     def from_string(cls, emp_str):
         first, last, pay = emp_str.split('-')
         return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
 
 
 emp1 = Employee('C', 'S', 50000)
@@ -34,12 +40,14 @@ e1 = 'John-Doe-70000'
 e2 = 'Steve-Jobs-30000'
 e3 = 'Steph-Curry-50000'
 
-new_e1 = Employee.from_string(e1)
+# new_e1 = Employee.from_string(e1)  //test from classmethod
 
-print(new_e1.email)
-print(new_e1.pay)
+# print(new_e1.email)   //test from classmethod
+# print(new_e1.pay) //test from classmethod
 # Employee.set_raise_amt(1.05)
-#
 # print(Employee.raise_amt)
 # print(emp1.raise_amt)
 # print(emp2.raise_amt)
+# import datetime                       //test for statismethod
+# my_date = datetime.date(2016, 7, 10)  //test for statismethod
+# print(Employee.is_workday(my_date))   //test for statismethod
