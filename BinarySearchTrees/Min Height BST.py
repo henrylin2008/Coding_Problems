7f9346459320407a9277d4549f870db757c56d0e
hlin <henrylin2008@yahoo.com>
2020-11-23 23:22:50 -0800

    added solution

--- BinarySearchTrees/Min Height BST.py
+++ BinarySearchTrees/Min Height BST.py
@@ -33,3 +33,39 @@
 #              2     7  13    22
 #             /           \
 #            1             14
+
+# Time: O(nlog(n))
+# Space: O(n)
+def minHeightBst(array):
+    return constructMinHeightBst(array, None, 0, len(array)-1)
+
+def constructMinHeightBST(array, bst, startIdx, endIdx):
+    if endIdx < startIdx:
+        return
+    midIdx = (startIdx + endIdx) // 2
+    valueToAdd =  array[midIdx]
+    if bst is None:
+        bst = BST(valueToAdd)
+    else:
+        bst.insert(valueToAdd)
+    constructMinHeightBST(array, bst, startIdx, midIdx -1)
+    constructMinHeightBST(array, bst, midIdx+1, endIdx)
+    return bst
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BST(value)
+            else:
+                self.right.insert(value)
