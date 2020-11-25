b17602497719888ae54f39b621a3680c7dc96f10
hlin <henrylin2008@yahoo.com>
2020-11-24 22:14:06 -0800

    added Solution #3

--- BinarySearchTrees/Right Smaller Than.py
+++ BinarySearchTrees/Right Smaller Than.py
@@ -34,55 +34,103 @@ def rightSmallerThan(array):
 
 # Solution #2:
 # Average case: when the created BST is balanced
-# Time: O(nlog(n)) |
+# Time: O(nlog(n))
 # Space O(n): where n is the length of the array
 # ---
 # Worst case: when the created BST is like a linked list
 # Time: O(n^2)
 # Space: O(n)
-def rightSmallerThan(array):
-    if len(array) == 0:
-        return []
+# def rightSmallerThan(array):
+#     if len(array) == 0:
+#         return []
+#
+#     lastIdx = len(array) - 1
+#     bst = SpecialBST(array[lastIdx], lastIdx, 0)
+#     for i in reversed(range(len(array) - 1)):
+#         bst.insert(array[i], i)
+#
+#     rightSmallerCounts = array[:]
+#     getRightSmallerCounts(bst, rightSmallerCounts)
+#     return rightSmallerCounts
+#
+#
+# def getRightSmallerCounts(bst, rightSmallerCounts):
+#     if bst is None:
+#         return
+#     rightSmallerCounts[bst.idx] = bst.numSmallerAtInsertTime
+#     getRightSmallerCounts(bst.left, rightSmallerCounts)
+#     getRightSmallerCounts(bst.right, rightSmallerCounts)
+#
+#
+# class SpecialBST:
+#     def __init__(self, value, idx, numSmallerAtInsertTime):
+#         self.value = value
+#         self.idx = idx
+#         self.numSmallerAtInsertTime = numSmallerAtInsertTime
+#         self.leftSubtreeSize = 0
+#         self.left = None
+#         self.right = None
+#
+#     def insert(self, value, idx, numSmallerAtInsertTime=0):
+#         if value < self.value:
+#             self.leftSubtreeSize += 1
+#             if self.left is None:
+#                 self.left = SpecialBST(value, idx, numSmallerAtInsertTime)
+#             else:
+#                 self.left.insert(value, idx, numSmallerAtInsertTime)
+#         else:
+#             numSmallerAtInsertTime += self.leftSubtreeSize
+#             if value > self.value:
+#                 numSmallerAtInsertTime += 1
+#             if self.right is None:
+#                 self.right = SpecialBST(value, idx, numSmallerAtInsertTime)
+#             else:
+#                 self.right.insert(value, idx, numSmallerAtInsertTime)
 
-    lastIdx = len(array) - 1
-    bst = SpecialBST(array[lastIdx], lastIdx, 0)
-    for i in reversed(range(len(array) - 1)):
-        bst.insert(array[i], i)
 
-    rightSmallerCounts = array[:]
-    getRightSmallerCounts(bst, rightSmallerCounts)
-    return rightSmallerCounts
-
-
-def getRightSmallerCounts(bst, rightSmallerCounts):
-    if bst is None:
-        return
-    rightSmallerCounts[bst.idx] = bst.numSmallerAtInsertTime
-    getRightSmallerCounts(bst.left, rightSmallerCounts)
-    getRightSmallerCounts(bst.right, rightSmallerCounts)
-
-
-class SpecialBST:
-    def __init__(self, value, idx, numSmallerAtInsertTime):
-        self.value = value
-        self.idx = idx
-        self.numSmallerAtInsertTime = numSmallerAtInsertTime
-        self.leftSubtreeSize = 0
-        self.left = None
-        self.right = None
-
-    def insert(self, value, idx, numSmallerAtInsertTime=0):
-        if value < self.value:
-            self.leftSubtreeSize += 1
-            if self.left is None:
-                self.left = SpecialBST(value, idx, numSmallerAtInsertTime)
-            else:
-                self.left.insert(value, idx, numSmallerAtInsertTime)
-        else:
-            numSmallerAtInsertTime += self.leftSubtreeSize
-            if value > self.value:
-                numSmallerAtInsertTime += 1
-            if self.right is None:
-                self.right = SpecialBST(value, idx, numSmallerAtInsertTime)
-            else:
-                self.right.insert(value, idx, numSmallerAtInsertTime)
+# Solution #3:
+# Average case: when the created BST is balanced
+# Time: O(nlog(n))
+# Space O(n): where n is the length of the array
+# ---
+# Worst case: when the created BST is like a linked list
+# Time: O(n^2)
+# Space: O(n)
+# def rightSmallerThan(array):
+#     if len(array) == 0:
+#         return []
+#
+#     rightSmallerCounts = array[:]
+#     lastIdx = len(array) - 1
+#     bst = SpecialBST(array[lastIdx])
+#     rightSmallerCounts[lastIdx] = 0
+#     for i in reversed(range(len(array) - 1)):
+#         bst.insert(array[i], i, rightSmallerCounts)
+#
+#     return rightSmallerCounts
+#
+#
+# class SpecialBST:
+#     def __init__(self, value):
+#         self.value = value
+#         self.leftSubtreeSize = 0
+#         self.left = None
+#         self.right = None
+#
+#     def insert(self, value, idx, rightSmallerCounts, numSmallerAtInsertTime=0):
+#         if value < self.value:
+#             self.leftSubtreeSize += 1
+#             if self.left is None:
+#                 self.left = SpecialBST(value)
+#                 rightSmallerCounts[idx] = numSmallerAtInsertTime
+#             else:
+#                 self.left.insert(value, idx, rightSmallerCounts, numSmallerAtInsertTime)
+#         else:
+#             numSmallerAtInsertTime += self.leftSubtreeSize
+#             if value > self.value:
+#                 numSmallerAtInsertTime += 1
+#             if self.right is None:
+#                 self.right = SpecialBST(value)
+#                 rightSmallerCounts[idx] = numSmallerAtInsertTime
+#             else:
+#                 self.right.insert(value, idx, rightSmallerCounts, numSmallerAtInsertTime)
