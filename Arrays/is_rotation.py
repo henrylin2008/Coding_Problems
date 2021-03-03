a396e127580dd66de395ff4a21de20e664684c72
hlin <henrylin2008@yahoo.com>
2021-03-02 23:18:06 -0800

    updated comments and tested the code

--- Arrays/is_rotation.py
+++ Arrays/is_rotation.py
@@ -2,44 +2,35 @@
 # Write a function that returns True if one array is a notation of another
 # ex: [1,2,3,4,5,6,7] is a notation of [4,5,6,7,1,2,3].
 # Note: there are no duplicates in each of these arrays
-# Reminder: We're going to use lists instead of arrays in Python for simplity
-
-def is_rotation(list1, list2):
-    # if list1.length != list2.length: return False
-    if len(list1) != len(list2):
-        return False
-    # First element in list1 (assumption)
-    key = list1[0]
-    # key index in list2
-    key_loc = -1
-
-    #for i from 0 to list2.length - 1:
-    #   if list2[i] == key:
-    #       key_i = i
-    #       break
-    for i in range(len(list2)):
-        if list2[i] == key:
-            key_loc = i
-            break
-
-    # if key_i == -1: return False
-    if key_loc == -1:
-        return False
-
-    # for i from 0 to list1.length - 1:
-    #   j = (key_i + i)%list1.length
-    #   if list1[i] != list2[j]: return False
-    for i in range(len(list1)):
-        j = (key_loc + i) % len(list1)
-        if list1[i] != list2[j]:
-            return False
-    return True
+# Reminder: We're going to use lists instead of arrays in Python for simplicity
 
 # Run Time: O(n)
+def is_rotation(list1, list2):
+    # if length of 2 lists don't match: return False
+    if len(list1) != len(list2):
+        return False
+
+    key = list1[0]  # First element in list1
+    key_loc = -1  # index of same item (from list1) on list2
+    for i in range(len(list2)):  # loop through list2
+        if list2[i] == key:  # if same item found in list2
+            key_loc = i  # put index on key_loc
+            break
+    if key_loc == -1:  # if not matching is found on list2
+        return False
+
+    for i in range(len(list1)):  # loop through list1
+        j = (key_loc + i) % len(list1)  # find index of same value (list1) from list2
+        # ex: list1: [1,2,3,4,5,6,7]; list2: [4,5,6,7,1,2,3]
+        # 5 in list1 (index: 4); (4 + 4) % 7 = 1 (index on list2)
+        if list1[i] != list2[j]:
+            return False
+    print("True")
+    return True
 
 
 # NOTE: The following input values will be used for testing your solution.
-list1 = [1, 2, 3, 4, 5, 6, 7]
+list1a = [1, 2, 3, 4, 5, 6, 7]
 list2a = [4, 5, 6, 7, 8, 1, 2, 3]
 # is_rotation(list1, list2a) should return False.
 list2b = [4, 5, 6, 7, 1, 2, 3]
@@ -52,3 +43,5 @@ list2e = [4, 5, 6, 7, 0, 2, 3]
 # is_rotation(list1, list2e) should return False.
 list2f = [1, 2, 3, 4, 5, 6, 7]
 # is_rotation(list1, list2f) should return True.
+
+is_rotation(list1a, list2f)
