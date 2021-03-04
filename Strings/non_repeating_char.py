535ac6d784218d8284c43f5de4cd0657facdc013
hlin <henrylin2008@yahoo.com>
2021-03-04 09:57:35 -0800

    added solution logic

--- Strings/non_repeating_char.py
+++ Strings/non_repeating_char.py
@@ -4,20 +4,28 @@
 # Ex: "abacc" --> 'b'
 #     "xxyzx" ---> 'y' (first non-repeating character)
 # If there is no non-repeating character, return None. def non_repeating(given_string):
+
+# Solution: use a dictionary to store seem characters, and add counter to each seem character; first loop to add counter
+# for each character; second loop to check if character only appears once, if counter of a character is == 1, return
+# that character; otherwise return None (if no single character)
+
 def non_repeating(given_string):
-    char_count = {} #Dictionary character count
+    char_count = {}             # Dictionary character count
     for c in given_string:
-        if c in char_count: # If c is already in char_count, increment c by 1, c += 1; ex, c: 2
+        if c in char_count:     # If c is already in char_count, increment c by 1, c += 1; ex, c: 2
             char_count[c] += 1
-        else:  # Otherwise, first occurrence = 1, 
+        else:                   # Otherwise, first occurrence = 1,
             char_count[c] = 1
-    for c in given_string: # Go through the string, if charcter appears once, then return the character
+    for c in given_string:      # Go through the string, if character appears once, then return the character
         if char_count[c] == 1:
+            # print(c)
             return c
-    return None # if nothing found, return null/none
+    # print("None")
+    return None                  # if nothing found, return null/none
+
 
 # NOTE: The following input values will be used for testing your solution.
-non_repeating("abcab") # should return 'c'
-non_repeating("abab") # should return None
-non_repeating("aabbbc") # should return 'c'
-non_repeating("aabbdbc") # should return 'd'
+non_repeating("abcab")      # should return 'c'
+non_repeating("abab")       # should return None
+non_repeating("aabbbc")     # should return 'c'
+non_repeating("aabbdbc")    # should return 'd'
