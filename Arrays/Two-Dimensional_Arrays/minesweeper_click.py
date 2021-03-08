7729c56b5aaaf22b4a82b36599219e867d397297
hlin <henrylin2008@yahoo.com>
2021-03-08 08:07:57 -0800

    added the description of the problem

--- Arrays/Two-Dimensional_Arrays/minesweeper_click.py
+++ Arrays/Two-Dimensional_Arrays/minesweeper_click.py
@@ -1,3 +1,40 @@
+# Find where to expand in Minesweeper
+# Implement a function that turns revealed cells into -2 given a location the user wants to click.
+# For simplicity, only reveal cells that have 0 in them. If the user clicks on any other type of cell (for example,
+# -1 /bomb or 1, 2, or 3), just ignore the click and return the original field. If a user clicks 0, reveal all other 0's
+# that are connected to it.
+
+# Ex 1:
+# Given field:
+# [[0, 0, 0, 0, 0],
+#  [0, 1, 1, 1, 0],
+#  [0, 1, -1, 1, 0]]
+# click location (2, 2:row index=2, column index = 2)
+# Resulting field:
+# [[0, 0, 0, 0, 0],
+#  [0, 1, 1, 1, 0],
+#  [0, 1, -1, 1, 0]]  (same as the given field)
+
+# Ex 2:
+# Given field:
+# [[-1, 1, 0, 0],
+#  [1, 1, 0, 0],
+#  [0, 0, 1, 1],
+#  [0, 0, 1, -1]]
+# click location (1, 3:row index = 1, column index = 3)
+# Resulting field:
+# [[-1, 1, -2, -2],
+#  [1, 1, -2, -2],
+#  [-2, -2, 1, 1],
+#  [-2, -2, 1, -1]]
+
+# Your function should take:
+#   -field: the given field as a 2D array
+#   -num_rows/numRows: the number of rows in the given field
+#   -num_cols/numCols: the number of columns in the given field
+#   -given_i/givenI: the row index of the cell the user wants to click
+#   -given_j/givenJ: the column index of the cell the user wants to click
+
 # Breadth-First search
 # Auxiliary Space: 2(n+m) ==> O(n+m); n=rows, m=columns
 # Time: O(n*m)
