655cd4427e454b8ca8031ae61625e6d43782e633
hlin <henrylin2008@yahoo.com>
2021-03-07 23:19:36 -0800

    updated comments

--- Arrays/Two-Dimensional_Arrays/minesweeper_click.py
+++ Arrays/Two-Dimensional_Arrays/minesweeper_click.py
@@ -8,19 +8,19 @@ def click(field, num_rows, num_cols, given_i, given_j):
     import queue
     to_check = queue.Queue()  # new queue: check 8 cells surrounding clicked cell
     if field[given_i][given_j] == 0:  # if clicked cell is 0
-        field[given_i][given_j] = -2  # set the cell (0) to -2; -2 as has been reviewed
-        to_check.put((given_i, given_j))  # store clicked cell (w/ updated value -2) to the to_check queue
+        field[given_i][given_j] = -2  # set the value of clicked cell (from 0) to -2; -2 as a marker for reviewed
+        to_check.put((given_i, given_j))  # store clicked cell as a tuple/array to the to_check queue
     else:  # else if clicked cell is not 0
-        print("Field: ", field)
+        # print("Field: ", field)
         return field  # return the field
     while not to_check.empty():  # while to_check queue is not empty
         (current_i, current_j) = to_check.get()  # next item in to_check queue to check
         for i in range(current_i - 1, current_i + 2):  # range of rows to check: i-1 to i+1
             for j in range(current_j - 1, current_j + 2):  # range of cols to check: j-1 to j+1
-                if 0 <= i < num_rows and 0 <= j < num_cols and field[i][j] == 0:  # within range and cell is 0
-                    field[i][j] = -2        # set the field to -2
-                    to_check.put((i, j))    # store it to to_check queue to check later
-    print("field: ", field)
+                if 0 <= i < num_rows and 0 <= j < num_cols and field[i][j] == 0:  # within the range and cell is 0
+                    field[i][j] = -2        # set the current field to -2
+                    to_check.put((i, j))    # store it to to_check queue for review later
+    # print("field: ", field)
     return field
 
 
