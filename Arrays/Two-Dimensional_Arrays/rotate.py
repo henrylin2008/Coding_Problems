4024ca775e38b4574bae5b4286b990f4a3cc2d53
hlin <henrylin2008@yahoo.com>
2021-03-08 22:52:11 -0800

    added out-of-space solution (copy of array)

--- Arrays/Two-Dimensional_Arrays/rotate.py
+++ Arrays/Two-Dimensional_Arrays/rotate.py
@@ -10,4 +10,40 @@
 # Solve this problem in place, means that your function won't create a new array to solve this problem. Instead, modify
 # the content of the given array with O(1) extra space.
 
+# Out-of-space solution: create another copy of array
+import copy
 
+
+# n = # rows = # columns in the given 2d array
+def rotate(given_array, n):
+    rotated = copy.deepcopy(given_array)     # new array, copy of the original array
+    for i in range(n):                       # i: row index
+        for j in range(n):                   # j: column index
+            (new_i, new_j) = rotate_sub(i, j, n)        # find new coordinates by using rotate_sub function
+            rotated[new_i][new_j] = given_array[i][j]   # new position
+    print("Rotated:", rotated)
+    return rotated
+
+
+# return new coordinates of the rotated position
+def rotate_sub(i, j, n):    # i: original row index, j: original column index; n = number of rows/columns
+    return j, n - 1 - i     # new_i = j (new row index), new_j = n-1-i (new column index)
+
+
+a1 = [[1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]]
+rotate(a1, 3)   # should return:
+# [[7, 4, 1],
+#  [8, 5, 2],
+#  [9, 6, 3]]
+
+a2 = [[1, 2, 3, 4],
+      [5, 6, 7, 8],
+      [9, 10, 11, 12],
+      [13, 14, 15, 16]]
+rotate(a2, 4)  # should return:
+# [[13, 9, 5, 1],
+#  [14, 10, 6, 2],
+#  [15, 11, 7, 3],
+#  [16, 12, 8, 4]]
