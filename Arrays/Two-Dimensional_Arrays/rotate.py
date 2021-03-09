032262720bca0d38ffd27edcbaf40e4f531b9d16
hlin <henrylin2008@yahoo.com>
2021-03-08 22:22:12 -0800

    rotating a 2D array by 90 degrees

---
+++ Arrays/Two-Dimensional_Arrays/rotate.py
@@ -0,0 +1,13 @@
+# Rotating a 2D array by 90 degrees
+# A 2-dimensional array is an array of arrays
+# Implement a function that takes a square 2D array (# columns = # rows) and rotates it by 90 degrees
+
+# Ex:
+# [[1, 2, 3],                   [[7, 4, 1],
+#  [4, 5, 6],       -->          [8, 5, 2],
+#  [7, 8, 9]]                    [9, 6, 3]]
+
+# Solve this problem in place, means that your function won't create a new array to solve this problem. Instead, modify
+# the content of the given array with O(1) extra space.
+
+
