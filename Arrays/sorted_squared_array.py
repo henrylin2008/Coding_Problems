5b8e53f7e620eae245f9b83953473350f1536a79
hlin <henrylin2008@yahoo.com>
2021-03-21 22:16:29 -0700

    sorted squared array

---
+++ Arrays/sorted_squared_array.py
@@ -0,0 +1,13 @@
+# https://www.algoexpert.io/questions/Sorted%20Squared%20Array
+# Sorted Squared Array
+# Difficulty: Easy
+# Write a function that takes in a non-empty array of integers that are sorted in ascending order and returns a new
+# array of the same length with the squares of the original integers also sorted in ascending order.
+#
+# Sample Input:
+# array = [1, 2, 3, 4, 5, 6, 8, 9]
+
+# Sample Output:
+# [1, 4, 9, 25, 36, 64, 81]
+
+def sortedSquaredArray(array):
