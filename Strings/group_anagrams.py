0a075c9b40c397eedd1ac50532d460251c279ae0
hlin <henrylin2008@yahoo.com>
2021-04-01 10:28:40 -0700

    Group anagrams

---
+++ Strings/group_anagrams.py
@@ -0,0 +1,13 @@
+# link: https://www.algoexpert.io/questions/Group%20Anagrams
+# Difficulty: Medium
+# Group Anagrams
+# Write a function that takes in an array of strings and groups anagrams together. Anagrams are strings made up of
+# exactly the same letters, where order doesn't matter. For example, "cinema" and "iceman" are anagrams; similarly,
+# "foo" and "ofo" are anagrams.
+# Your function should return a list fo anagram groups in no particular order.
+
+# Sample Input:
+# words = ["yo", "act", "flop", "tac", "foo", "cat", "oy", "olfp"]
+
+# Sample Output:
+# [["yo", "oy"], ["flop", "olfp"], ["act", "tac", "cat"], ["foo"]]
