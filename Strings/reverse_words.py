77765eb8a64f3c29771fff783ff5a22729bfd0c7
hlin <henrylin2008@yahoo.com>
2021-04-02 12:14:56 -0700

    added solution

--- Strings/reverse_words.py
+++ Strings/reverse_words.py
@@ -17,3 +17,30 @@
 # "best! the is AlgoExpert"
 
 
+# Time: O(n)
+# Space: O(n); n: length of the string
+def reverseWords(string):
+    words = []  # list to store all words from string, including ' '
+    startOfWord = 0  # starting index of a new word
+    for idx in range(len(string)):
+        character = string[idx]     # character at current index
+
+        if character == " ":    # if current character is an empty string " "
+            words.append(string[startOfWord:idx])   # add last word to the words list
+            startOfWord = idx   # reset starting index
+        elif string[startOfWord] == " ":    # if start of word is an empty string " "
+            words.append(" ")   # add an empty string to the words list
+            startOfWord = idx   # reset starting index
+
+    words.append(string[startOfWord:])  # add last word into the words list
+
+    reverseList(words)   # reverse the words in words list
+    return "".join(words)   # join all the words together
+
+# function to reverse the words in a list
+def reverseList(list):
+    start, end = 0, len(list) - 1
+    while start < end:
+        list[start], list[end] = list[end], list[start]
+        start += 1
+        end -= 1
