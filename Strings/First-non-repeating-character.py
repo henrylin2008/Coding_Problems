a33a49d46db93bbe4b1f7901e168ea107fc63905
hlin <henrylin2008@yahoo.com>
2021-04-03 10:17:17 -0700

    first non-repeating character

---
+++ Strings/First-non-repeating-character.py
@@ -0,0 +1,13 @@
+# link: https://www.algoexpert.io/questions/First%20Non-Repeating%20Character
+# First Non-Repeating Character
+# Difficulty: Easy
+# Write a function that takes in a string of lowercase English-alphabet letter and returns the index of the string's
+# first non-repeating character.
+# The first non-repeating character is the first character in a string that occurs only once.
+
+# Sample Input:
+# string = "abcdcaf"
+
+# Sample Output:
+# The first non-repeating character is "b" and is found at index 1
+
