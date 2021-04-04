8940533092f23440f447b4d5e2e3e6194c4f70eb
hlin <henrylin2008@yahoo.com>
2021-04-04 14:09:48 -0700

    Longest Substring without Duplication

---
+++ Strings/longest-substring-no-duplication.py
@@ -0,0 +1,11 @@
+# link: https://www.algoexpert.io/questions/Longest%20Substring%20Without%20Duplication
+# Longest Substring without Duplication
+# Difficulty: hard
+# Write a function that takes in a string and returns its longest substring without duplicate characters.
+# You can assume that there will only be one longest substring without duplication.
+
+# Sample Input:
+# string = "clementisacap"
+
+# Sample Output:
+# "mentisac"
