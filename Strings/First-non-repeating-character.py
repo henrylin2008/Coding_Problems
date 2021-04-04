45ccdac12111237b395f904e6053787569e8bbcb
hlin <henrylin2008@yahoo.com>
2021-04-04 09:53:04 -0700

    added brute-force solution

--- Strings/First-non-repeating-character.py
+++ Strings/First-non-repeating-character.py
@@ -6,8 +6,27 @@
 # The first non-repeating character is the first character in a string that occurs only once.
 
 # Sample Input:
-# string = "abcdcaf"
+string = "abcdcaf"
 
 # Sample Output:
 # The first non-repeating character is "b" and is found at index 1
 
+
+# Brute-force solution:
+# time: O(n^2); 2 loops; each loop takes O(n) time, thus O(n) x O(n) -> O(n^2)
+# space: O(1); not using any additional space
+# Logic: nested traversal (2 loops), outer loop goes through each index/character, set a boolean variable; the inner
+# loop to identify non duplicate character appeared, and return the index of first non-repeating character.
+def firstNonRepeatingCharacter(string):
+    for idx1 in range(len(string)):
+        foundDuplicate = False
+        for idx2 in range(len(string)):
+            if string[idx1] == string[idx2] and idx1 != idx2:  # if same string found at a different index
+                foundDuplicate = True
+
+        if not foundDuplicate:  # if non duplicate string is found, return the index from outer loop
+            return idx1
+    return -1
+
+
+firstNonRepeatingCharacter(string)
