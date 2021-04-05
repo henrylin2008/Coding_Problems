80f8c0ff951bd5506ba54f6f00634de9dacdab1a
hlin <henrylin2008@yahoo.com>
2021-04-05 10:05:55 -0700

    added comments

--- Strings/longest-substring-no-duplication.py
+++ Strings/longest-substring-no-duplication.py
@@ -11,22 +11,26 @@ string = "clementisacap"
 # "mentisac"
 
 
-# Time: O(n)
-# Space: O(min(n, a))
+# Time: O(n); n is length of the input string; iterate through the string, at any point, we only update start index,
+# character and its index in lastSeen hashmap, and compare the length of 2 strings, these operations are constant
+# Space: O(min(n, a)), n: length of the string; a: set of unique letters from given string
 def longestSubstringWithoutDuplication(string):
-    lastSeen = {}   # hash map that keeps track of letters (and its index) that have seem so far
-    longest = [0, 1]
-    startIdx = 0
-    for i, char in enumerate(string):
-        if char in lastSeen:
-            # startIdx is starting index of a longest substring;
-            # max(startIdx, lastSeen[char] + 1): compares current start index and new start index (index of duplicate
-            # letter)
+    lastSeen = {}   # hash map that keeps track of letter and its index that have seem so far
+    longest = [0, 1]  # longest substring: [starting index, ending index]
+    startIdx = 0    # start index at the beginning
+    for i, char in enumerate(string):   # keep track of the index and the character at current index
+        if char in lastSeen:  # update startIdx if character is in lastSeen hashmap
+            # max(startIdx, lastSeen[char] + 1): startIdx: initial start index;
+            # lastSeen[char] + 1: index right after the first occurrence of the duplicate letter
+            # ex: e at 2, e at 4, lastSeen[char] + 1 = 2 + 1 = 3; max(0, 3) => 3 (new starting index)
             startIdx = max(startIdx, lastSeen[char] + 1)
-        if longest[1] - longest[0] < i + 1 - startIdx:
-            longest = [startIdx, i + 1]
-        lastSeen[char] = i
-    return string[longest[0]: longest[1]]
+        if longest[1] - longest[0] < i + 1 - startIdx:  # compare the length of current longest substring and new one
+            # i + 1 - startIdx: potential new longest substring length
+            # i + 1 b/c longest is set [0, 1], which is the index after i, i+1 is equivalent to longest[1]
+            # startIdx equivalent to longest[0]
+            longest = [startIdx, i + 1]     # update longest substring if new length is greater than existing longest
+        lastSeen[char] = i  # update/add index of the character
+    return string[longest[0]: longest[1]]   # longest[1] is excluded
 
 
 longestSubstringWithoutDuplication(string)
