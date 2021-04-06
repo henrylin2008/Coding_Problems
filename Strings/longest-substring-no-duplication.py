e4df17bcd01a99b56a07a79165ea4ae6acdb6193
hlin <henrylin2008@yahoo.com>
2021-04-06 10:37:35 -0700

    added logic and updated comment

--- Strings/longest-substring-no-duplication.py
+++ Strings/longest-substring-no-duplication.py
@@ -14,6 +14,11 @@ string = "clementisacap"
 # Time: O(n); n is length of the input string; iterate through the string, at any point, we only update start index,
 # character and its index in lastSeen hashmap, and compare the length of 2 strings, these operations are constant
 # Space: O(min(n, a)), n: length of the string; a: set of unique letters from given string
+# Logic: use a hashmap to store the letter and its index, set a longest substring array and set a starting index; while
+# looping through the given string, check if current character is stored in the lastSeen hashmap, if the character is in
+# the hashmap, then compare current startIdx and index of seem character (+1), which return the new starting index; next
+# compare the length of existing longest substring and new length (starting index til current index + 1), update the
+# longest substring if new length is longer than exiting longest substring. Then return letters of longest substring
 def longestSubstringWithoutDuplication(string):
     lastSeen = {}   # hash map that keeps track of letter and its index that have seem so far
     longest = [0, 1]  # longest substring: [starting index, ending index]
@@ -29,7 +34,7 @@ def longestSubstringWithoutDuplication(string):
             # i + 1 b/c longest is set [0, 1], which is the index after i, i+1 is equivalent to longest[1]
             # startIdx equivalent to longest[0]
             longest = [startIdx, i + 1]     # update longest substring if new length is greater than existing longest
-        lastSeen[char] = i  # update/add index of the character
+        lastSeen[char] = i  # update index or add new char and its index into lastSeen hash map
     return string[longest[0]: longest[1]]   # longest[1] is excluded
 
 
