488abe6ca1ea4d87d43369cbe10ded52c444afbe
hlin <henrylin2008@yahoo.com>
2021-04-07 09:32:50 -0700

    updated comment and added the logic

--- Linked List/remove-duplicates.py
+++ Linked List/remove-duplicates.py
@@ -22,14 +22,17 @@ class LinkedList:
 
 
 # Time: O(n); one pass through all nodes in linked list
-# Space: O(1)
+# Space: O(1); only modify the given linked list
+# Logic: since the given linked list is sorted, thus we just need to find next distinct value (from current node), then
+# set the next pointer of current node to the distinct node, until we reached the end (node with None value)
 def removeDuplicatesFromLinkedList(linkedList):
     currentNode = linkedList    # first/current node
-    while currentNode is not None:  # while current node has a value; None is the end of linked list
+    while currentNode is not None:  # while current node has a value (not end of linked list)
         nextDistinctNode = currentNode.next  # next distinct node is next node after current node
+        # nextDistinctNode is not empty and current node matches next node
         while nextDistinctNode is not None and nextDistinctNode.value == currentNode.value:
-            nextDistinctNode = nextDistinctNode.next
-
+            nextDistinctNode = nextDistinctNode.next    # move to the following node
+        # now we found a distinct node, set the pointer of next node to nextDistinctNode
         currentNode.next = nextDistinctNode  # point to next node after current node
         currentNode = nextDistinctNode  # update currentNode
 
