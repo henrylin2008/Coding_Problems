580d5908b2bdba12a63eebc2c1be623f7bf2b3bb
hlin <henrylin2008@yahoo.com>
2021-04-06 22:18:13 -0700

    Remove Duplicates From Linked List

---
+++ Linked List/remove-duplicates.py
@@ -0,0 +1,16 @@
+# link: https://www.algoexpert.io/questions/Remove%20Duplicates%20From%20Linked%20List
+# Remove Duplicates From Linked List
+# Difficulty: Easy
+
+# You're given the head of a Singly Linked List whose nodes are in sorted order with respect to their values. Write a
+# function that returns a modified version of the Linked List that doesn't contain any nodes with duplicate values. The
+# Linked List should be modified in place (i.e., you shouldn't create a brand new list), and the modified Linked List
+# should still have its nodes sorted with respect to their values.
+# Each LinkedList node has an integer value as well as a next node pointing to the next node in the list or to None/null
+# if it's the tail of the list.
+
+# Sample Input:
+# linkedList = 1 -> 1 -> 3 -> 4 -> 4 -> 4 -> 5 -> 6 -> 6 // the head node with value 1
+
+# Sample Output:
+# 1 -> 3 -> 4 -> 5 -> 6  // the head node with value 1
