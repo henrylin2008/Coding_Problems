007cf4f7d7bc4b5a5d2f07f9d8ed220875bb0a8d
hlin <henrylin2008@yahoo.com>
2021-04-10 23:15:00 -0700

    added Time/Space complexity and comments

--- Linked List/sum_of_linked_lists.py
+++ Linked List/sum_of_linked_lists.py
@@ -27,25 +27,28 @@ class LinkedList:
         self.next = None
 
 
+# Time: O(max(n, m)); n, m: length of 2 nodes; max(n, m): longest length between 2 nodes
+# Space: O(max(n, m))
 def sumOfLinkedLists(linkedListOne, linkedListTwo):
-    newLinkedListHead = LinkedList(0)
-    currentNode = newLinkedListHead
-    carry = 0
+    newLinkedListHead = LinkedList(0)   # new linked list head pointer
+    currentNode = newLinkedListHead     # current node
+    carry = 0       # carry over value
 
-    nodeOne = linkedListOne
-    nodeTwo = linkedListTwo
+    nodeOne = linkedListOne  # pointer 1, loop through Linked list one, keep track of node that's currently on
+    nodeTwo = linkedListTwo  # pointer 2, loop through linked list Two, keep track of node that's currently on
+    # keep looping if we have a nodeOne, or a nodeTwo, or some carry is not equal 0
     while nodeOne is not None or nodeTwo is not None or carry != 0:
-        valueOne = nodeOne.value if nodeOne is not None else 0
-        valueTwo = nodeTwo.value if nodeTwo is not None else 0
-        sumOfValues = valueOne + valueTwo + carry
+        valueOne = nodeOne.value if nodeOne is not None else 0  # get nodeOne value, return 0 if it's a None node
+        valueTwo = nodeTwo.value if nodeTwo is not None else 0  # get nodeTwo value, return 0 if it's a None node
+        sumOfValues = valueOne + valueTwo + carry   # sum of nodes at same positions
 
         newValue = sumOfValues % 10  # reminder, get the value on one's position
-        newNode = LinkedList(newValue)
-        currentNode.next = newNode
-        currentNode = newNode
+        newNode = LinkedList(newValue)  # create new linked list node
+        currentNode.next = newNode  # set pointer of currentNode to the new node
+        currentNode = newNode   # set current node to new node
 
-        carry = sumOfValues // 10   # carry over value, if sum of 2 nodes > 10
-        nodeOne = nodeOne.next if nodeOne is not None else None
-        nodeTwo = nodeTwo.next if nodeTwo is not None else None
+        carry = sumOfValues // 10   # carry over value;  if sum of 2 nodes > 10
+        nodeOne = nodeOne.next if nodeOne is not None else None  # next linked list node
+        nodeTwo = nodeTwo.next if nodeTwo is not None else None  # next linked list node
 
-    return newLinkedListHead.next
+    return newLinkedListHead.next   # return next node after new linked list
