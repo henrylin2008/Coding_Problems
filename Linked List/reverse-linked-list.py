5a203ee4656bb7d0da8f7049c487369a665ba461
hlin <henrylin2008@yahoo.com>
2021-04-13 00:13:11 -0700

    reverse Linked list

---
+++ Linked List/reverse-linked-list.py
@@ -0,0 +1,16 @@
+# link: https://www.algoexpert.io/questions/Reverse%20Linked%20List
+# Reverse Linked List
+# Difficulty: Hard
+
+# Write a function that takes in the head of a Singly Linked List, reverses the list in place (i.e., doesn't create a
+# brand new list), and returns its new head.
+# Each LinkedList node has an integer value as well as a next node pointing to the next node in the list or to None/null
+# if it's the tail fo the list.
+# You can assume that the input Linked List will always have at least one node; in other words, the head will never be
+# None/null.
+
+# Sample Input:
+# head = 0 -> 1 -> 2 -> 3 -> 4 -> 5 // the head node with value 0
+
+# Sample Output:
+# 5 -> 4 -> 3 -> 2 -> 1 -> 0  // the new head node with value 5
