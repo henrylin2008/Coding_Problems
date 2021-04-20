9d5c26d2dccd73e20842cec6b343876ccc087b7a
hlin <henrylin2008@yahoo.com>
2021-04-20 09:00:18 -0700

    added iterative solution

--- Linked List/merge-linked-lists.py
+++ Linked List/merge-linked-lists.py
@@ -16,3 +16,31 @@
 
 # Sample Output:
 # 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 -> 10 // the new head node with value 1
+
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+# Iterative Solution
+# Time: O(n + m); n, m: length of both linked lists
+# Space: O(1); Only using pointers
+def mergeLinkedLists(headOne, headTwo):
+    p1 = headOne
+    p1Prev = None  # p1 previous pointer
+    p2 = headTwo
+    while p1 is not None and p2 is not None:
+        if p1.value < p2.value:
+            p1Prev = p1
+            p1 = p1.next
+        else:
+            if p1Prev is not None:
+                p1Prev.next = p2
+            p1Prev = p2
+            p2 = p2.next
+            p1Prev.next = p1
+    if p1 is None:
+        p1Prev.next = p2    # point next node to p2
+    return headOne if headOne.value < headTwo.value else headTwo
+
