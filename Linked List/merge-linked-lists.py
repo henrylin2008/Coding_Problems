98861161f0c430b35849d8c3ce6edb2a21a92f13
hlin <henrylin2008@yahoo.com>
2021-04-21 09:48:53 -0700

    added comments

--- Linked List/merge-linked-lists.py
+++ Linked List/merge-linked-lists.py
@@ -26,23 +26,31 @@ class LinkedList:
 # Iterative Solution
 # Time: O(n + m); n, m: length of both linked lists
 # Space: O(1); Only using pointers
+# Logic: set 3 pointers: p1 -> headOne, p2 -> headTwo, p1Prev -> the node before p1 (None as starting);
+# p1Prev is use to keep track of the original p1/p2 pointer and .next pointer, while p1/p2 moving to the next node;
+# Run while loop to compare 2 non-empty nodes between 2 linked lists, if p1.value < p2.value, then move p1Prev node to
+# p1, and move p1 to p1.next; elif p1.value > p2.value, move p1Prev(or p1Prev.next) to p2, p2 to p2.next, and point
+# p1Prev.next to p1. When reach to the end of a linked list, update p1Prev.next pointer to p2|p1 (depending which linked
+# list runs out); lastly, return the head node that has a smaller value.
 def mergeLinkedLists(headOne, headTwo):
-    p1 = headOne
-    p1Prev = None  # p1 previous pointer
-    p2 = headTwo
-    while p1 is not None and p2 is not None:
-        if p1.value < p2.value:
-            p1Prev = p1
-            p1 = p1.next
-        else:
-            if p1Prev is not None:
-                p1Prev.next = p2
-            p1Prev = p2
-            p2 = p2.next
-            p1Prev.next = p1
-    if p1 is None:
-        p1Prev.next = p2    # point next node to p2
-    return headOne if headOne.value < headTwo.value else headTwo
+    p1 = headOne   # pointer 1 points to headOne
+    p1Prev = None  # p1 previous node
+    p2 = headTwo   # pointer 2 points to headTwo
+    while p1 is not None and p2 is not None:  # when p1, p2 nodes are not empty nodes
+        if p1.value < p2.value:  # in this case, keep moving p1 from linked list 1
+            p1Prev = p1     # must move p1Prev to p1 position first
+            p1 = p1.next    # then we can move p1 to p1.next position
+        else:   # p1.value > p2.value,
+            if p1Prev is not None:  # if p1Prev is not at the very beginning (p1Prev starts as None)
+                p1Prev.next = p2    # points p1Prev.next to p2;
+            # elif p1Prev is None: p2 is the head of the linked list 2
+            p1Prev = p2    # must move p1Prev to p2 position first
+            p2 = p2.next   # then we can move p2 to p2.next position
+            p1Prev.next = p1    # points p1Prev.next (original p2.next) to p1
+    if p1 is None:  # p1 reached the end of linked list 1, then links p1Prev (final node) to linked list 2
+        p1Prev.next = p2   # point p1Prev.next (final node on linked list 1) to p2 (linked list 2)
+    # if p2 is None: no further actions needed, as every node in linked list 1 are in the correct order
+    return headOne if headOne.value < headTwo.value else headTwo  # return the head with the smaller value
 
 
 # Recursive Solution:
