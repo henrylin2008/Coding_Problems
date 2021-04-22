c025865dec812c8d4bec38cd30f5452fb87dfc93
hlin <henrylin2008@yahoo.com>
2021-04-21 23:12:09 -0700

    updated format

--- Graphs/depth_first_search.py
+++ Graphs/depth_first_search.py
@@ -1,4 +1,7 @@
 # link: https://www.algoexpert.io/questions/Depth-first%20Search
+# Depth First Search
+# Difficulty: Easy
+
 # You are given a Node class that has a name and an array of optional children Nodes. When put together, Nodes form a
 # simple tree-like structure. Implement the depthFirstSearch method on the Node class, which takes in an empty array,
 # traverses the tree using the Depth-first Search approach (specifically navigating the tree from left to right), stores
@@ -16,6 +19,8 @@
 # Sample output: ["A","B","E","F","I","J","C","D","G","K","H"]
 # v: vertice (node)
 # e: edge (lines connect between nodes)
+
+
 # Time: O(v + e): vertices + edges
 # Space: O(v): level of depth/height of the tree
 # Recursive call, get
@@ -30,9 +35,9 @@ class Node:
 
     def depthFirstSearch(self, array):
         array.append(self.name)  # append the name to the array
-        for child in self.children: # for each child in children
-            child.depthFirstSearch(array) # call depthFirstSearch() function
-        return array # useful on first row
+        for child in self.children:  # for each child in children
+            child.depthFirstSearch(array)  # call depthFirstSearch() function
+        return array  # useful on first row
 
 
 #########################################################################################################
@@ -53,27 +58,28 @@ class Node:
 # Pick any node. If it is unvisited, mark it as visited and recur on all its adjacent nodes.
 # Repeat until all the nodes are visited, or the node to be searched is found.
 graph = {
-    'A' : ['B','C'],
-    'B' : ['D', 'E'],
-    'C' : ['F'],
-    'D' : [],
-    'E' : ['F'],
-    'F' : []
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
 }
 
-visited = [] # Array to keep track of visited nodes.
+visited = []  # Array to keep track of visited nodes.
 
-def dfs(visited, graph, node): #graph in the form of a dictionary, and A, which is the starting node.
-    if node not in visited: #It first checks if the current node is unvisited - if yes,
+
+def dfs(visited, graph, node):  # graph in the form of a dictionary, and A, which is the starting node.
+    if node not in visited:  # It first checks if the current node is unvisited - if yes,
         print(node)
-        visited.append(node) # it is appended in the visited array.
-        for neighbour in graph[node]: # for each neighbor of the current node,
-            dfs(visited, graph, neighbour) #the dfs function is invoked again
+        visited.append(node)  # it is appended in the visited array.
+        for neighbour in graph[node]:  # for each neighbor of the current node,
+            dfs(visited, graph, neighbour)  # the dfs function is invoked again
+
 
 # The base case is invoked when all the nodes are visited. The function then returns.
 # Driver Code
-dfs(visited, graph, 'A') # 'A' is starting node
-
+dfs(visited, graph, 'A')  # 'A' is starting node
 
 #########################################################################################################
 # The implementation below uses the stack data-structure to build-up and return a set of vertices that are accessible
@@ -98,6 +104,7 @@ def dfs(graph, start):
             stack.extend(graph[vertex] - visited)
     return visited
 
+
 dfs(graph, 'A')
 
 
@@ -118,5 +125,6 @@ def dfs(graph, start, visited=None):
     for nxt in graph[start] - visited:
         dfs(graph, nxt, visited)
     return visited
+
 
 dfs(graph, 'A')
