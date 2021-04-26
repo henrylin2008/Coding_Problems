0f183196f752c328102f9487203a6019399dbdfa
hlin <henrylin2008@yahoo.com>
2021-04-26 10:14:10 -0700

    updated the logic

--- Graphs/Single-Cycle-Check.py
+++ Graphs/Single-Cycle-Check.py
@@ -22,9 +22,13 @@
 # Space: O(1)
 # Logic: set a variable numElementsVisited to keep track number of elements have visited, and a variable currentIdx to
 #        keep track of current index; the goal is while moving around the array, it should return to the first index
-#        last. Edge case to check is when there's a loop that it returns to first index before reaching all elements in
-#        the array. Calculating wrap around case for moving forward (currentIdx + jump) % len(array), else if moving
-#        backward: nextIdx + len(array). Lastly the function returns true if currentIdx is 0 (first index).
+#        last. Edge cases to check when there's a loop that it returns to first index before reaching all elements in
+#        the array. Run a while loop before less than the length of array, an edge case where if it returns to the first
+#        index before reaching all elements in the array, then return False; next increment numElementsVisit by 1, and
+#        calculate the next index; use a separate function to find out the next index, which it covers the wrap
+#        around cases in the array. For case that is moving forward, next index is calculated as
+#        (currentIdx + jump) % len(array); if moving backward, the formula to find out next index:
+#        (currentIdx + jump) % len(array) + len(array). Then return true if currentIdx is  at index 0 (starting point)
 # conditions:
 #       * number of elements visited == len(array)
 #       * last Index is back to the initial index (0)
