e7072e8998952479daefce515b607383bd6a3313
hlin <henrylin2008@yahoo.com>
2021-04-28 23:47:50 -0700

    added comments

--- Graphs/river-sizes.py
+++ Graphs/river-sizes.py
@@ -35,46 +35,46 @@
 
 # Time: O(wh); w: width, h: height
 # Space: O(wh); w: width, h: height
-def riverSizes(matrix):
+def riverSizes(matrix):   # main function
     sizes = []   # array to return
     visited = [[False for value in row] for row in matrix]  # set every node to False
     for i in range(len(matrix)):  # iterate through every element in the matrix
         for j in range(len(matrix[i])):
             if visited[i][j]:   # if it's a visited node
                 continue    # skip
-            traverseNode(i, j, matrix, visited, sizes)
+            traverseNode(i, j, matrix, visited, sizes)  # dealing with unvisited nodes
     return sizes
 
 
 def traverseNode(i, j, matrix, visited, sizes):
     currentRiverSize = 0
-    nodesToExplore = [[i, j]]   # stack for DFS
+    nodesToExplore = [[i, j]]   # stack for DFS to explore neighbor nodes
     # apply DFS
-    while len(nodesToExplore):
-        currentNode = nodesToExplore.pop()
+    while len(nodesToExplore):  # while still have nodes to explore
+        currentNode = nodesToExplore.pop()  # pop out the final value
         i = currentNode[0]
         j = currentNode[1]
         if visited[i][j]:   # if visited node
             continue        # skip
-        visited[i][j] = True
+        visited[i][j] = True    # set as visited
         if matrix[i][j] == 0:   # if it's land, then skip
             continue
-        currentRiverSize += 1
+        currentRiverSize += 1   # update river size
         unvisitedNeighbors = getUnvisitedNeighbors(i, j, matrix, visited)   # get all unvisited neighbor nodes
         for neighbor in unvisitedNeighbors:
             nodesToExplore.append(neighbor)   # add unvisited nodes to nodesToExplore
     if currentRiverSize > 0:
-        sizes.append(currentRiverSize)
+        sizes.append(currentRiverSize)  # add current river size to the sizes array
 
 
 def getUnvisitedNeighbors(i, j, matrix, visited):
     unvisitedNeighbors = []
-    if i > 0 and not visited[i - 1][j]:  # neighbor above current row
-        unvisitedNeighbors.append([i - 1, j])
-    if i < len(matrix) - 1 and not visited[i + 1][j]:   # not at bottom row and neighbor below current row
-        unvisitedNeighbors.append([i + 1, j])
+    if i > 0 and not visited[i - 1][j]:  # neighbor above current row and not visited
+        unvisitedNeighbors.append([i - 1, j])   # add it to unvisited neighbors array
+    if i < len(matrix) - 1 and not visited[i + 1][j]:   # not at bottom row and not visited
+        unvisitedNeighbors.append([i + 1, j])       # add it to unvisited neighbors array
     if j > 0 and not visited[i][j - 1]:     # if not at left most column and not visited neighbor
-        unvisitedNeighbors.append([i, j - 1])
+        unvisitedNeighbors.append([i, j - 1])   # add it to unvisited neighbors array
     if j < len(matrix[0]) - 1 and not visited[i][j + 1]:  # if at right most column and not visited neighbor
-        unvisitedNeighbors.append([i, j + 1])
+        unvisitedNeighbors.append([i, j + 1])   # add it to unvisited neighbors array
     return unvisitedNeighbors
