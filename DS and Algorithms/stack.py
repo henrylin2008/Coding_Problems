83d04c0b4699fac9224fad1eb1a374d3f7284e30
hlin <henrylin2008@yahoo.com>
2021-05-06 23:23:52 -0700

    added push and pop

--- DS and Algorithms/stack.py
+++ DS and Algorithms/stack.py
@@ -38,9 +38,9 @@ class Stack(object):
         self.ll = LinkedList(top)
 
     def push(self, new_element):
-        "Push (add) a new element onto the top of the stack"
-        pass
+        """Push (add) a new element onto the top of the stack"""
+        self.ll.insert_first(new_element)
 
     def pop(self):
-        "Pop (remove) the first element off the top of the stack and return it"
-        pass
+        """Pop (remove) the first element off the top of the stack and return it"""
+        return self.ll.delete_first()
