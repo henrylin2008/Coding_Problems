2b5f56fe8f9747f7b7bd4768f84817091381f9f9
hlin <henrylin2008@yahoo.com>
2021-05-07 23:08:02 -0700

    String methods and set methods

---
+++ DS and Algorithms/string_set.py
@@ -0,0 +1,23 @@
+# Strings methods
+# Strings are immutable
+# Method    Use                 Explanation
+# center    astring.center(w)   returns a string centered in a field of size w
+# count     astring.count(item) returns the number of occurrences of item in the string
+# ljust     astring.ljust(w)    returns a string left_justified in a field of size w
+# lower     astring.lower()     returns a string in all lowercase
+# rjust     astring.rjust(w)    returns a string right-justified in a field of size w
+# find      astring.find(item)  returns the index of the first occurrence of item
+# split     astring.split(s)    split a string into substrings at s
+# upper     astring.upper()     returns a string in all uppercase
+
+
+# Set methods
+# Method        Use                  Explanation
+# union         set.union(set2)      returns a new set with all elements from both sets
+# intersection  set.intersection(s2) returns a new set with only those elements common to both sets
+# difference    set.difference(s2)   returns a new set with all items from first set not in second
+# issubset      set.issubset(s2)     ask whether all elements of one set are in the other
+# add           set.add(item)        adds item to the set
+# remove        set.remove(item)     removes item from the set
+# pop           set.pop()            removes an arbitrary element from the set
+# clear         set.clear()          removes all elements from the set
