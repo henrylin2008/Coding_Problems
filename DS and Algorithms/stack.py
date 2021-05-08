684305cc8a788d9ebeed33566fc20ee3fe3ae577
hlin <henrylin2008@yahoo.com>
2021-05-07 22:13:04 -0700

    added stack methods

--- DS and Algorithms/stack.py
+++ DS and Algorithms/stack.py
@@ -1,3 +1,13 @@
+# Stack methods:
+# Stack() creates a new stack that is empty. It needs no parameters and returns an empty stack.
+# push(item) adds a new item to the top of the stack. It needs the item and returns nothing.
+# pop() removes the top item from the stack. It needs no parameters and returns the item. The stack is modified.
+# peek() returns the top item from the stack but does not remove it. It needs no parameters. The stack is not modified.
+# isEmpty() tests to see whether the stack is empty. It needs no parameters and returns a boolean value.
+# size() returns the number of items on the stack. It needs no parameters and returns an integer.
+
+
+# Build a stack with a LinkedList 
 class Element(object):
     def __init__(self, value):
         self.value = value
