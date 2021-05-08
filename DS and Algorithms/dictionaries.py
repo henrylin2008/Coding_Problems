48f340e6766c82c28b92b78a36310b02145d6bac
hlin <henrylin2008@yahoo.com>
2021-05-07 23:32:44 -0700

    dictionaries

---
+++ DS and Algorithms/dictionaries.py
@@ -0,0 +1,12 @@
+# Dictionaries
+# Operator   Use            Explanation
+# []         dict[k]        returns the value associated with k, otherwise its an error
+# in         key in dict    returns True if key is in the dictionary, False otherwise
+# del        del dict[k]    removes the entry from the dictionary
+
+# Method     Use             Explanation
+# keys       dict.keys()     returns the keys of the dictionary in a dict_keys object
+# values     dict.values()   returns the values of the dictionary in a dict_values object
+# items      dict.items()    returns the key-value pairs in a dict_items object
+# get        dict.get(k)     returns the value associated with k, None otherwise
+# get        dict.get(k,alt) returns the value associated with k, alt otherwise
