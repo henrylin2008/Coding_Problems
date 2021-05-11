307686e71fe2e5bc021426344a877800d41f51b9
hlin <henrylin2008@yahoo.com>
2021-05-10 22:59:47 -0700

    bubble sort

---
+++ DS and Algorithms/bubble_sort.py
@@ -0,0 +1,8 @@
+# average case: O(n^2)
+# worst case: O(n^2)
+# Best case: O(n^2)
+# Space: O(1)
+
+# Logic: compare 2 values side by side, move the greater value to the right side, then compare the greater value to the
+# next value in the array, swap their position if the new value is less than the previous greater value; repeat the
+# same process until it reaches to the end of the array.
