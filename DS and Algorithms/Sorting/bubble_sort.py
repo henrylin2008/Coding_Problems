6e617022d7aeffc44c87a629e7dca00471b81f70
hlin <henrylin2008@yahoo.com>
2021-05-16 08:08:55 -0700

    added short bubble implementation

--- DS and Algorithms/Sorting/bubble_sort.py
+++ DS and Algorithms/Sorting/bubble_sort.py
@@ -12,8 +12,8 @@
 # up to the location where it belongs.
 
 def bubbleSort(alist):
-    for passnum in range(len(alist) - 1, 0, -1):
-        for i in range(passnum):
+    for value in range(len(alist) - 1, 0, -1):
+        for i in range(value):
             if alist[i] > alist[i + 1]:
                 temp = alist[i]
                 alist[i] = alist[i + 1]
@@ -22,4 +22,25 @@ def bubbleSort(alist):
 
 alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 bubbleSort(alist)
+print(alist)
+
+
+# Short Bubble:
+# set a flag to False, A bubble sort can be modified to stop early if it finds that the list has become sorted.
+def shortBubbleSort(alist):
+    exchanges = True
+    passnum = len(alist) - 1
+    while passnum > 0 and exchanges:
+        exchanges = False
+        for i in range(passnum):
+            if alist[i] > alist[i + 1]:
+                exchanges = True
+                temp = alist[i]
+                alist[i] = alist[i + 1]
+                alist[i + 1] = temp
+        passnum = passnum - 1
+
+
+alist = [20, 30, 40, 90, 50, 60, 70, 80, 100, 110]
+shortBubbleSort(alist)
 print(alist)
