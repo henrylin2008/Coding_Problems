a41a175186c67dd618d40362f02550a87449b76b
hlin <henrylin2008@yahoo.com>
2021-05-18 10:00:51 -0700

    added methods to print a tree diagram

--- DS and Algorithms/Trees/tree_linked_list.py
+++ DS and Algorithms/Trees/tree_linked_list.py
@@ -33,15 +33,83 @@ class BinaryTree:
     def getRootVal(self):
         return self.key
 
+    # following methods are for the assignment from the section
+    # Write a function buildTree that returns a tree using the nodes and references implementation that looks like th
+    def printTree(self, level=0):  # outline of the tree
+        print(" " * level, self.key)
+        if self.leftChild is not None:
+            self.leftChild.printTree(level + 1)
+        else:
+            print(" " * (level + 1), "None")
+        if self.rightChild is not None:
+            self.rightChild.printTree(level + 1)
+        else:
+            print(" " * (level + 1), "None")
 
-r = BinaryTree('a')
-print(r.getRootVal())
-print(r.getLeftChild())
-r.insertLeft('b')
-print(r.getLeftChild())
-print(r.getLeftChild().getRootVal())
-r.insertRight('c')
-print(r.getRightChild())
-print(r.getRightChild().getRootVal())
-r.getRightChild().setRootVal('hello')
-print(r.getRightChild().getRootVal())
+    def depth(self, d=0):
+        """Get the depth of the tree"""
+        d1 = 0
+        d2 = 0
+        if self.leftChild:
+            d1 = max(self.leftChild.depth(d + 1), d)
+        if self.rightChild:
+            d2 = max(self.rightChild.depth(d + 1), d)
+        return max(d1, d2, d)
+
+    def _toArray(self, left, i, right, s):
+        """convert a tree to array"""
+        left += 1
+        i = i * 2 + right
+        s[left - 1][i] = self.key
+        if self.leftChild:
+            self.leftChild._toArray(left, i, 0, s)
+        if self.rightChild:
+            self.rightChild._toArray(left, i, 1, s)
+
+    def diagram(self):
+        content_size = 4
+        space = " "
+        bottom_size = (content_size + len(space))
+        depth = self.depth()
+        if depth > 4:
+            print("depth is too large")
+            return
+        width = 5 * 2**depth
+        s = list()
+        for i in range(depth + 1):
+            s.append([""] * (2 ** i))
+        self._toArray(0, 0, 0, s)
+        for l in range(depth + 1):
+            for i in range(len(s[l])):
+                print(str(s[l][i]).center(width // 2 ** l), end="")
+            print()
+            if l < depth:
+                for i in range(len(s[l])):
+                    print("/   \\".center(width // 2 ** l), end="")
+                print()
+
+
+def main():
+    r = BinaryTree('a')
+    r.insertLeft('b')
+    r.getLeftChild().insertRight('d')
+    r.insertRight('f')
+    r.insertRight('c')
+    r.getRightChild().insertLeft('e')
+    r.diagram()
+
+
+if __name__ == "__main__":
+    main()
+
+# r = BinaryTree('a')
+# print(r.getRootVal())
+# print(r.getLeftChild())
+# r.insertLeft('b')
+# print(r.getLeftChild())
+# print(r.getLeftChild().getRootVal())
+# r.insertRight('c')
+# print(r.getRightChild())
+# print(r.getRightChild().getRootVal())
+# r.getRightChild().setRootVal('hello')
+# print(r.getRightChild().getRootVal())
