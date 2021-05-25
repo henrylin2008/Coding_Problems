6cde3bc27046c388efbc16b2bf96ad313c873451
hlin <henrylin2008@yahoo.com>
2021-05-24 22:42:50 -0700

    Binary Heap constructor and insert

--- DS and Algorithms/Trees/Priority_Queues_Binary_Heaps.py
+++ DS and Algorithms/Trees/Priority_Queues_Binary_Heaps.py
@@ -33,3 +33,48 @@ print(bh.delMin())  # 5
 print(bh.delMin())  # 7
 
 print(bh.delMin())  # 11
+
+
+# A complete binary tree is a tree in which each level has all of its nodes. The exception to this is the bottom level
+# of the tree, which we fill in from left to right.
+# -Left child of node p is 2p
+# -right child of node p is 2p+1
+# -The parent of child node c is c/2
+# index: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 |
+# value: 0 | 5 | 9 | 11| 14| 18| 19| 21| 33| 17| 27 |
+#                  (1)5
+#               /         \
+#            (2)9          (3)11
+#          /     \         /    \
+#      (4)14    (5)18    (6)19 (7)21
+#      /  \      /
+#   (8)33 (9)17 (10)27
+
+class BinHeap:
+    def __init__(self):
+        self.heapList = [0]  # first index set to 0
+        self.currentSize = 0
+
+
+# Heap Order Property
+# In a heap, for every node x with parent p, the key in p is smaller than or equal to the key in x. (or for every parent
+# there are no children that are smaller than them
+
+
+# Insert
+# i's left child: 2 * i
+# i's right child: 2 * i + 1
+# i's parent: i // 2
+def percUp(self, i):
+    while i // 2 > 0:   # while has a parent
+        if self.heapList[i] < self.heapList[i // 2]:  # if out of order
+            tmp = self.heapList[i // 2]
+            self.heapList[i // 2] = self.heapList[i]
+            self.heapList[i] = tmp
+        i = i // 2  # lop with new position
+
+
+def insert(self, k):
+    self.heapList.append(k)  # insert at end of the list
+    self.currentSize = self.currentSize + 1  # update size
+    self.percUp(self.currentSize)  # adjust to be in heap order
