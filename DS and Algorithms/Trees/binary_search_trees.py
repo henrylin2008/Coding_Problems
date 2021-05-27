ee3adc306352585217bcf2e077c95810ce0fbd52
hlin <henrylin2008@yahoo.com>
2021-05-26 22:46:38 -0700

    added remove method

--- DS and Algorithms/Trees/binary_search_trees.py
+++ DS and Algorithms/Trees/binary_search_trees.py
@@ -100,6 +100,44 @@ class BinarySearchTree:
     def __delitem__(self, key):
         self.delete(key)
 
+    def remove(self, currentNode):
+        if currentNode.isLeaf():  # leaf
+            if currentNode == currentNode.parent.leftChild:
+                currentNode.parent.leftChild = None
+            else:
+                currentNode.parent.rightChild = None
+        elif currentNode.hasBothChildren():  # interior
+            successor = currentNode.findSuccessor()
+            successor.spliceOut()
+            currentNode.key = successor.key
+            currentNode.payload = successor.payload
+
+        else:  # this node has one child
+            if currentNode.hasLeftChild():
+                if currentNode.isLeftChild():
+                    currentNode.leftChild.parent = currentNode.parent
+                    currentNode.parent.leftChild = currentNode.leftChild
+                elif currentNode.isRightChild():
+                    currentNode.leftChild.parent = currentNode.parent
+                    currentNode.parent.rightChild = currentNode.leftChild
+                else:
+                    currentNode.replaceNodeData(currentNode.leftChild.key,
+                                                currentNode.leftChild.payload,
+                                                currentNode.leftChild.leftChild,
+                                                currentNode.leftChild.rightChild)
+            else:
+                if currentNode.isLeftChild():
+                    currentNode.rightChild.parent = currentNode.parent
+                    currentNode.parent.leftChild = currentNode.rightChild
+                elif currentNode.isRightChild():
+                    currentNode.rightChild.parent = currentNode.parent
+                    currentNode.parent.rightChild = currentNode.rightChild
+                else:
+                    currentNode.replaceNodeData(currentNode.rightChild.key,
+                                                currentNode.rightChild.payload,
+                                                currentNode.rightChild.leftChild,
+                                                currentNode.rightChild.rightChild)
+
 
 class TreeNode:
     def __init__(self, key, val, left=None, right=None, parent=None):
