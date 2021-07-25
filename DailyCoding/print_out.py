7608f648c450a8b0cadec4685e3c057274f154c8
hlin <henrylin2008@yahoo.com>
2021-07-25 09:19:45 -0700

    updating file name and its format

--- DailyCoding/print_out.py
+++ DailyCoding/print_out.py
@@ -6,17 +6,19 @@
 
 functions = []
 for i in range(10):
-    functions.append(lambda : i)
+    functions.append(lambda: i)
 
 for f in functions:
     print(f())
-    
+
 # Solution
 # At a first glance, it seems like the snippet should print out from 0 to 9. But actually, it prints 9 10 times.
 #
-# The problem is that the functions have closure and have access to the non-local variable i. We instead want the value of i when the functions are declared.
+# The problem is that the functions have closure and have access to the non-local variable i. We instead want the
+# value of i when the functions are declared.
 #
-# In order to solve this issue, we should capture the value i when the funcionts are declared. This would make i a local variable inside the anonymous functions.
+# In order to solve this issue, we should capture the value i when the funcionts are declared. This would make i a
+# local variable inside the anonymous functions.
 
 functions = []
 for i in range(10):
