1721ef0b28681f7495b5483688ce7d01c70821ac
hlin <henrylin2008@yahoo.com>
2021-07-28 07:45:52 -0700

    updating file name and the format

--- DailyCoding/linked_list_sum.py
+++ DailyCoding/linked_list_sum.py
@@ -2,7 +2,8 @@
 # Problem
 # This problem was asked by Microsoft.
 #
-# Let's represent an integer in a linked list format by having each node represent a digit in the number. The nodes make up the number in reversed order.
+# Let's represent an integer in a linked list format by having each node represent a digit in the number. The nodes
+# make up the number in reversed order.
 #
 # For example, the following linked list:
 #
@@ -18,15 +19,23 @@
 # return 124 (99 + 25) as:
 #
 # 4 -> 2 -> 1
+
 # Solution
-# We can add two numbers using the same process as elementary grade school addition — add the least significant digits with a carry.
+
+# We can add two numbers using the same process as elementary grade school addition — add the least significant
+# digits with a carry.
 #
-# We'll start at the head of the two nodes, and compute sum % 10. We write this down, move the two nodes up and add a carry if the sum was greater than 10. A tricky part here is to finding the terminating condition. We can see that this happens when there is no more carry and the two linked lists have reached the end. Since this is tricky to represent in code, we instead extend the nodes one more until they are both None and carry is 0 and then simply return None.
+# We'll start at the head of the two nodes, and compute sum % 10. We write this down, move the two nodes up and add a
+# carry if the sum was greater than 10. A tricky part here is to finding the terminating condition. We can see that
+# this happens when there is no more carry and the two linked lists have reached the end. Since this is tricky to
+# represent in code, we instead extend the nodes one more until they are both None and carry is 0 and then simply
+# return None.
 
 class Node:
     def __init__(self, val, next=None):
         self.val = val
         self.next = next
+
 
 def add(node0, node1, carry=0):
     if not node0 and not node1 and not carry:
@@ -39,7 +48,6 @@ def add(node0, node1, carry=0):
     node0_next = node0.next if node0 else None
     node1_next = node1.next if node1 else None
     carry_next = 1 if total >= 10 else 0
-
 
     return Node(total % 10, add(node0_next, node1_next, carry_next))
 # This will run in O(max(m, n)) time, where m and n are the lengths of the linked lists.
--- DailyCoding/max_profit.py
+++ DailyCoding/max_profit.py
@@ -2,18 +2,24 @@
 # Problem
 # This problem was asked by Facebook.
 #
-# Given an array of numbers representing the stock prices of a company in chronological order and an integer k, return the maximum profit you can make from k buys and sells. You must buy the stock before you can sell it, and you must sell the stock before you can buy it again.
+# Given an array of numbers representing the stock prices of a company in chronological order and an integer k,
+# return the maximum profit you can make from k buys and sells. You must buy the stock before you can sell it,
+# and you must sell the stock before you can buy it again.
 #
 # For example, given k = 2 and the array [5, 2, 4, 0, 1], you should return 3.
 #
 # Solution
-# This problem is quite a bit harder than Daily Coding Problem #47, where we only needed to buy and sell a stock once to get the max profit.
+
+# This problem is quite a bit harder than Daily Coding Problem #47, where we only needed to buy and sell a stock once
+# to get the max profit.
 #
-# Let's consider the last price in the array. Either we used that price in a transaction or we didn't. Thus, the max profit for our input should be the max of either:
+# Let's consider the last price in the array. Either we used that price in a transaction or we didn't. Thus,
+# the max profit for our input should be the max of either:
 #
 # The max profit of prices[:-1] using k transactions (we didn't use the last price)
 # The best you can get by selling the stock on the last day (we use the last price)
-# We can use dynamic programming to represent the maximum profit we can get. Let dp[i][j] represent the maximum profit using at most i transactions up to day j (inclusive).
+# We can use dynamic programming to represent the maximum profit we can get. Let dp[i][j] represent the maximum
+# profit using at most i transactions up to day j (inclusive).
 #
 # So the recurrence is:
 #
@@ -22,11 +28,13 @@
 #
 # dp[i][j] = max(
 #     dp[i][j - 1], # We don't use the last price
-#     max(price[j] - price[m] + dp[i - 1][m] for m in range(j)) # Best we can do by completing last transaction on jth day
-# )
+#     max(price[j] - price[m] + dp[i - 1][m] for m in range(j)) # Best we can do by completing last transaction on jth
+#     day )
+
 # So let's build our matrix bottom-up and fill it according to the recurrence:
 
 from math import inf
+
 
 def max_profit(k, prices):
     n = len(prices)
@@ -46,11 +54,14 @@ def max_profit(k, prices):
             dp[i][j] = max(best_so_far, dp[i][j - 1])
 
     return dp[k][n - 1]
+
+
 # This will run in O(kn2) time and take O(kn) space.
 #
-# We can improve the time complexity by noticing that we're redoing some of the same calculations in the innermost for loop. In the term prices[j] - prices[m] + dp[i - 1][m], prices[j] stays constant and the rest of the term can be factored into the outer loop.
+# We can improve the time complexity by noticing that we're redoing some of the same calculations in the innermost
+# for loop. In the term prices[j] - prices[m] + dp[i - 1][m], prices[j] stays constant and the rest of the term can
+# be factored into the outer loop.
 
-from math import inf
 
 def max_profit(k, prices):
     n = len(prices)
