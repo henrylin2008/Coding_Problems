890565e02135e6e6f9f13ead9c64c84493501402
hlin <henrylin2008@yahoo.com>
2021-10-27 11:03:20 -0700

    valid palindrome

---
+++ LeetCode/Blind 75/String/125_Valid_Palindrome.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/valid-palindrome/
+# Problem 125 - Valid Palindrome
+# Category: Easy
+#
+# Given a string s, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+#
+#
+# Example 1:
+#
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+#
+# Example 2:
+#
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+#
+#
+# Constraints:
+# -1 <= s.length <= 2 * 105
+# -s consists only of printable ASCII characters.
+
+# Solution: create an empty string to store every character from the given s, then compare it with the reverse string
+#           this solution uses the extra memory (an empty string), and uses the built-in functions
+# Time: O(n)
+# Space: O(n); n is number of characters in s
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_str = ""
+
+        for c in s:
+            if c.isalnum():
+                new_str += c.lower()
+        return new_str == new_str[::-1]
