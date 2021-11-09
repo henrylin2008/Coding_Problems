7b0dd0a834677b4d909ff026ef3a78418b4f2aec
hlin <henrylin2008@yahoo.com>
2021-11-09 00:06:10 -0800

    group anagrams

--- LeetCode/Blind 75/String/49_Group_Anagrams.py
+++ LeetCode/Blind 75/String/49_Group_Anagrams.py
@@ -24,3 +24,21 @@
 # 0 <= strs[i].length <= 100
 # strs[i] consists of lowercase English letters.
 
+# Solution:
+# use Hashmap to count how many time of each letter appears; ex: 'eat': a:1, e:1, t:1
+from collections import defaultdict
+
+# time: O(m*n*26) -> O(mn); m: number of input strings; n: length (chars) of the string
+class Solution:
+    def groupAnagrams(self, strs):
+        res = defaultdict(list)  # mapping charCount to list of anagrams; defaultdict to deal with non-existing char
+
+        for str in strs:
+            count = [0] * 26  # [0....0] = [a....z]
+
+            for char in str:
+                count[ord(char) - ord("a")] += 1    # count of each character
+
+            res[tuple(count)].append(str)   # group anagrams together; tuple(count): b/c count is a list and it can't be
+            # the key.
+        return res.values()  # return list/group of anagrams
