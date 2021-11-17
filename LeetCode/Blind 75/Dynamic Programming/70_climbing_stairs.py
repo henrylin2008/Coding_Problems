242952e9ac90d8ed311aa6e250e7816f83c31218
hlin <henrylin2008@yahoo.com>
2021-11-17 13:39:02 -0800

    climbing stairs

---
+++ LeetCode/Blind 75/Dynamic Programming/70_climbing_stairs.py
@@ -0,0 +1,43 @@
+# link: https://leetcode.com/problems/climbing-stairs/
+# Climbing stairs: Easy
+# You are climbing a staircase. It takes n steps to reach the top.
+#
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+#
+# Example 1:
+#
+# Input: n = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+# Example 2:
+#
+# Input: n = 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+#
+#
+# Constraints:
+#
+# 1 <= n <= 45
+
+# Solution idea: dynamic programming - Bottom up:
+# ex: n = 5:
+# stairs:      0      1       2      3        4       5
+# DP:        5+3=8   3+2=5   2+1=3  1+1=2     1       1
+# from:      1,2     2,3     3,4    4,5     (base cases)
+#                                             one    two
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one, two = 1, 1     # base cases
+
+        for i in range(n - 1):
+            temp = one      # temp variable for var one
+            one = one + two  # new one (sum of 2 previous variables)
+            two = temp      # move two to old one (temp)
+
+        return one
