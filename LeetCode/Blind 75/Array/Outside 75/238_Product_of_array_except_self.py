c0a0bfa4faf502b4d90b9a6de32e425c384d0675
hlin <henrylin2008@yahoo.com>
2021-11-29 23:55:04 -0800

    product of array except self

--- LeetCode/Blind 75/Array/Outside 75/238_Product_of_array_except_self.py
+++ LeetCode/Blind 75/Array/Outside 75/238_Product_of_array_except_self.py
@@ -30,3 +30,27 @@
 #
 # Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra
 # space for space complexity analysis.)
+
+# Solution: 2 passes: one for prefix (in-order), one for postfix (reverse order)
+# ex:       1     2     3     4
+#             \      \     \
+# prefix    1     1     2     6  (prefix starts at 1, then multiply it by the new prefix number: 1, 1*1, 1*2, 2*3)
+#           |     |     |     |  (postfix starts at 1)
+# postfix   24    12    8     6  (from right to left; 1*6=6, 4(1*4) *2(prefix)=8, 12 (4*3)*1 = 12, 24 (12*2) * 1 = 24)
+
+# Time: O(n)
+# Space: O(1)
+class Solution:
+    def productExceptSelf(self, nums):  # List[int]) -> List[int]
+        result = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):  # loop through to get the prefix result: 1, 1, 2, 6
+            result[i] = prefix
+            prefix *= nums[i]  # previous prefix * new number from nums
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):   # reverse order, starts from the end (to the left)
+            result[i] *= postfix
+            postfix *= nums[i]
+        return result
+
