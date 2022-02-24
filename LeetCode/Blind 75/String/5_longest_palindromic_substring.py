e762f4faf4f67dc8edf9705dd01fe20f70040fc6
henrylin2008 <henrylin2008@yahoo.com>
2022-02-24 10:22:37 -0800

    Longest Palindromic Substring

--- LeetCode/Blind 75/String/5_longest_palindromic_substring.py
+++ LeetCode/Blind 75/String/5_longest_palindromic_substring.py
@@ -12,7 +12,34 @@
 # Input: s = "cbbd"
 # Output: "bb"
 #
-#
 # Constraints:
 # 1 <= s.length <= 1000
 # s consist of only digits and English letters.
+
+# Solution: foreach char in str, consider it were the middle, consider if pali was odd or even
+# Time: O(n^2); loop through each character: O(n), and expand it out takes is another O(n), thus O(n^2)
+# Space: O(n); worst case: need to store every character in s.
+def longestPalindrome(self, s: str) -> str:
+    result = ""
+    result_len = 0
+
+    for i in range(len(s)):
+        # loop for odd numbers (ex:'babad' --> 'bab')
+        left, right = i, i
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            if right - left + 1 > result_len:
+                result = s[left:right+1]
+                result_len = right - left + 1
+            left -= 1
+            right += 1
+
+        # loop for even numbers (ex: 'cbbd' --> 'bb')
+        left, right = i, i + 1
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            if right - left + 1 > result_len:
+                result = s[left:right+1]
+                result_len = right - left + 1
+            left -= 1
+            right += 1
+
+    return result
