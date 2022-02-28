5665e00baac60f08cbacfaa5382c7a7b0661b9fa
henrylin2008 <henrylin2008@yahoo.com>
2022-02-28 11:49:12 -0800

    Palindromic Substrings

--- LeetCode/Blind 75/String/647_palindromic_substrings.py
+++ LeetCode/Blind 75/String/647_palindromic_substrings.py
@@ -43,3 +43,21 @@ class Solution:
                 l -= 1
                 r += 1
         return res
+
+
+# put the duplicated code in a function
+# class Solution:
+#     def countSubstrings(self, s: str) -> int:
+#         res = 0
+#         for i in range(len(s)):
+#             res += self.count_Pali(s, i, i)
+#             res += self.count_Pali(s, i, i + 1)
+#         return res
+#
+#     def count_Pali(self, s, l, r):
+#         res = 0
+#         while l >= 0 and r < len(s) and s[l] == s[r]:
+#             res += 1
+#             l -= 1
+#             r += 1
+#         return res
