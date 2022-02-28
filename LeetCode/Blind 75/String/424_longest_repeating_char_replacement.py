8ae9b160865fb88dc5c2fb60982bad653846b55a
henrylin2008 <henrylin2008@yahoo.com>
2022-02-28 10:16:40 -0800

    Longest Repeating Character Replacement

--- LeetCode/Blind 75/String/424_longest_repeating_char_replacement.py
+++ LeetCode/Blind 75/String/424_longest_repeating_char_replacement.py
@@ -26,21 +26,24 @@
 # s consists of only uppercase English letters.
 # 0 <= k <= s.length
 
-# Time: O(n)
-#
+# PAY ATTENTION: limited to chars A-Z; for each capital char, check if it could create the longest repeating substr,
+# use sliding window to optimize; check if windowlen=1 works, if yes, increment len, if not, shift window right;
+
+# Time: O(26n) -> O(n)
+# Space: O(n)
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count = {}  # count of each character
-        res = 0     # longest substring with k replacement
+        count = {}  # count of each character; key=the char, value=count of each char
+        res = 0  # longest substring with k replacement
+        left = 0  # left pointer
+        max_freq = 0  # most frequency of a letter
+        for right in range(len(s)):
+            count[s[right]] = 1 + count.get(s[right], 0)  # increment of the count of s[r]
+            max_freq = max(max_freq, count[s[right]])  # max frequency of the hashmap (most freq letter in s)
 
-        l = 0       # left pointer
-        for r in range(len(s)):
-            count[s[r]] = 1 + count.get(s[r], 0)    # increment of the count of s[r]
-            max_freq = max(max_freq, count[s[r]])
+            while (right - left + 1) - max_freq > k:  # # of replacement > # of replacement allowed
+                count[s[left]] -= 1  # decrement of count for letter at the left pointer
+                left += 1  # shift left pointer
 
-            while (r - l + 1) - max_freq > k:  # # of replacement > # of replacement allowed
-                count[s[l]] -= 1        # decrement of count for left pointer
-                l += 1                  # shift left pointer
-
-            res = max(res, r - l + 1)
+            res = max(res, right - left + 1)  # update the result
         return res
