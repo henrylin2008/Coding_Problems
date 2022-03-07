261cdd397a0ac129c6914dbae644ba590e0a0a02
henrylin2008 <henrylin2008@yahoo.com>
2022-03-07 10:01:19 -0800

    Search in Rotated Sorted Array

--- LeetCode/Blind 75/Array/33_search_in_rotated_sorted_array.py
+++ LeetCode/Blind 75/Array/33_search_in_rotated_sorted_array.py
@@ -32,14 +32,16 @@
 # *nums is an ascending array that is possibly rotated.
 # *-104 <= target <= 104
 
+# Note: at most two sorted halfs, mid will be apart of left sorted or right sorted, if target is in range of sorted
+# portion then search it, otherwise search other half
+
 # Time: O(log n); binary search
 # Space: O(1)
 from typing import List
 
-
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
+        l, r = 0, len(nums) - 1     # left, right pointer
 
         while l <= r:
             mid = (l + r) // 2
@@ -48,15 +50,15 @@ class Solution:
 
             # left sorted portion
             if nums[l] <= nums[mid]:
-                if target > nums[mid] or target < nums[l]:
+                if target > nums[mid] or target < nums[l]:  # search right portion; mid < target < nums[l]
                     l = mid + 1
-                else:
+                else:       # left < target < mid
                     r = mid - 1
 
             # right sorted portion
             else:
-                if target < nums[mid] or target > nums[r]:
+                if target < nums[mid] or target > nums[r]:  # nums[r] < target < mid
                     r = mid - 1
-                else:
+                else:   # mid < target < r
                     l = mid + 1
         return -1
