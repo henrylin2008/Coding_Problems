bf4ad179059eaa3c24b5c8e11464607b38701b7d
henrylin2008 <henrylin2008@yahoo.com>
2022-03-07 08:55:55 -0800

    Search in Rotated Sorted Array

--- LeetCode/Blind 75/Array/33_search_in_rotated_sorted_array.py
+++ LeetCode/Blind 75/Array/33_search_in_rotated_sorted_array.py
@@ -32,4 +32,31 @@
 # *nums is an ascending array that is possibly rotated.
 # *-104 <= target <= 104
 
+# Time: O(log n); binary search
+# Space: O(1)
+from typing import List
 
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+
+            # left sorted portion
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+            # right sorted portion
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
