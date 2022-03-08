e3fd17efed9012a748d609684c5387d22f9b221b
henrylin2008 <henrylin2008@yahoo.com>
2022-03-08 00:36:00 -0800

    Maximum Depth of Binary Tree - BFS

--- LeetCode/Blind 75/Tree/104_max_depth_of_binary_tree.py
+++ LeetCode/Blind 75/Tree/104_max_depth_of_binary_tree.py
@@ -43,3 +43,24 @@ class Solution:
             return 0
 
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+# BFS solution: use queue to store node/s from each level (top down) until it reaches to an empty node;
+#               ex: 1(l1) + 2(l2) + 3(l2) + 4(l3) + 5 (l4) + empty
+# Time: O(n)
+# Space: O(n)
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        level = 0
+        q = deque([root])
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
