c39a3ed6cf5884ae63ee8387ebfbb1bcc514f142
henrylin2008 <henrylin2008@yahoo.com>
2022-03-08 11:37:13 -0800

    Same tree

--- LeetCode/Blind 75/Tree/100_same_tree.py
+++ LeetCode/Blind 75/Tree/100_same_tree.py
@@ -46,6 +46,6 @@ class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if not p and not q:     # if both trees are empty
             return True
-        if not p or not q or p.val != q.val:    # if one tree is not empty, or values of node are not equal
+        if not p or not q or p.val != q.val:    # if one of the tree is not empty, or values of the node are not equal
             return False
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)    # recursive comparing nodes
