c6ba327879c9f28727e5c45f4cc4727d583e31f3
henrylin2008 <henrylin2008@yahoo.com>
2022-03-10 00:42:31 -0800

    Subtree of Another Tree

--- LeetCode/Blind 75/Tree/572_subtree_of_another_tree.py
+++ LeetCode/Blind 75/Tree/572_subtree_of_another_tree.py
@@ -23,3 +23,32 @@
 # The number of nodes in the subRoot tree is in the range [1, 1000].
 # -104 <= root.val <= 104
 # -104 <= subRoot.val <= 104
+
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot:     # if subRoot is empty -> subtree of root
+            return True
+        if not root:        # if root is not emtpy and subRoot is empty
+            return False
+        if self.sameTree(root, subRoot):    # if both trees are the same
+            return True
+        # if subRoot is a subtree of either left or right of the root tree
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def sameTree(self, s, t):
+        if not s and not t:  # if both trees empty
+            return True
+        if s and t and s.val == t.val:  # if both trees are not empty and values are the same
+            return self.sameTree(s.left, t.left) and self.sameTree(s.right, t.right)
+        return False  # at least one tree is empty and another is not empty
