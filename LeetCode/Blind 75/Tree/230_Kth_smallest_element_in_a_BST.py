8eb5e7974a4f6af10578d33bb30995f9a5ba7b15
henrylin2008 <henrylin2008@yahoo.com>
2022-03-11 22:33:11 -0800

    Kth Smallest Element in a BST

--- LeetCode/Blind 75/Tree/230_Kth_smallest_element_in_a_BST.py
+++ LeetCode/Blind 75/Tree/230_Kth_smallest_element_in_a_BST.py
@@ -22,3 +22,31 @@
 #
 # Follow up: If the BST is modified often (i.e., we can do insert and delete operations) and you need to find the kth
 # smallest frequently, how would you optimize?
+
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        n = 0
+        stack = []
+        cur = root
+
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+
+            cur = stack.pop()  # pop recent added item
+            n += 1
+            if n == k:
+                return cur.val
+            cur = cur.right
