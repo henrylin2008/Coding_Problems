9f5af934ffac381a9ce8c6c4926d3ceb97853da8
henrylin2008 <henrylin2008@yahoo.com>
2022-03-12 23:53:05 -0800

    Binary Tree Inorder Traversal

--- LeetCode/Blind 75/Tree/Outside 75/94_Binary_Tree_Inorder_Traversal.py
+++ LeetCode/Blind 75/Tree/Outside 75/94_Binary_Tree_Inorder_Traversal.py
@@ -27,3 +27,29 @@
 #
 #
 # Follow up: Recursive solution is trivial, could you do it iteratively?
+
+# Definition for a binary tree node.
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    # recursive solution
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        def inorder(root):
+            if not root:
+                return
+            inorder(root.left)
+            res.append(root.val)
+            inorder(root.right)
+
+        inorder(root)
+        return res
