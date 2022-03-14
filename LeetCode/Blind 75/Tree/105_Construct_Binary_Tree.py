5bfc59fdf2ed5eb70d1c16a45c2d0949a69614b5
henrylin2008 <henrylin2008@yahoo.com>
2022-03-14 11:18:46 -0700

    Construct Binary Tree from preorder and inorder

--- LeetCode/Blind 75/Tree/105_Construct_Binary_Tree.py
+++ LeetCode/Blind 75/Tree/105_Construct_Binary_Tree.py
@@ -35,12 +35,20 @@ class TreeNode:
 
 
 class Solution:
+    # ex 1: preorder = [3,  9,     20,15,7      ],        inorder = [9,          3,      15,20,7]
+    #                  root, left, right subtree                 left (size:1) root,   right subtree (size:3)
+    # Logic: recursive calls; first number in preorder is always the root, use this number to find the index of this
+    #       value in the inorder, that helps to find the size of sub-arrays (anything to the left of the root is left
+    #       sub-array, and anything to the right is the right subarray), the size helps for partition in preorder list
+    #       (ex1: left: 1, right: 3), then recursive calls on the sub-arrays
+    # Time: O(n^2); due to recursive stack
+    # Space: O(N)
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if not preorder or not inorder:     # base case: if given list is empty
             return None
 
-        root = TreeNode(preorder[0])
-        mid = inorder.index(preorder[0])
+        root = TreeNode(preorder[0])        # root is always the preorder[0]
+        mid = inorder.index(preorder[0])    # index of the root in the inorder list
         root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
         root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
         return root
