2923503826020fc135d87b871bc2ee26fc7fd96e
henrylin2008 <henrylin2008@yahoo.com>
2022-03-13 23:47:38 -0700

    Construct Binary Tree from Preorder and Inorder Traversal

--- LeetCode/Blind 75/Tree/105_Construct_Binary_Tree.py
+++ LeetCode/Blind 75/Tree/105_Construct_Binary_Tree.py
@@ -22,3 +22,25 @@
 # Each value of inorder also appears in preorder.
 # preorder is guaranteed to be the preorder traversal of the tree.
 # inorder is guaranteed to be the inorder traversal of the tree.
+
+# Definition for a binary tree node.
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:     # base case: if given list is empty
+            return None
+
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
+        return root
