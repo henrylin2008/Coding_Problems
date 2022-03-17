e1a86778b9f61787c4c85b0bab3e6119f5c97929
henrylin2008 <henrylin2008@yahoo.com>
2022-03-17 11:46:57 -0700

    Implement Trie (Prefix Tree)

--- LeetCode/Blind 75/Tree/208_Implement_Trie.py
+++ LeetCode/Blind 75/Tree/208_Implement_Trie.py
@@ -39,3 +39,44 @@
 # 1 <= word.length, prefix.length <= 2000
 # word and prefix consist only of lowercase English letters.
 # At most 3 * 104 calls in total will be made to insert, search, and startsWith.
+
+# Note: node has children characters, and bool if its an ending character, node DOESN’T have or need char,
+#       since root node doesn’t have a char, only children;
+
+# Time: O(m); m: key length
+# Space: O(1)
+class TrieNode:
+    def __init__(self):
+        self.children = {}      # dict to store child node/s
+        self.end_word = False   # mark for the node as the end of the word
+
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.end_word = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.end_word            # True if curr.end_word == True
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+
+        for c in prefix:
+            if c not in curr.children:  # if character not in the tree, return False
+                return False
+            curr = curr.children[c]     # move the pointer to the child
+        return True                     # no need to check end of word, since we only look for prefix
