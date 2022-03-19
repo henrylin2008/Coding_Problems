7f6d65156ea9ecfc845c11fdbf9a522dca244762
henrylin2008 <henrylin2008@yahoo.com>
2022-03-18 23:48:22 -0700

    Top K Frequent Elements using a heap

--- LeetCode/Blind 75/Heap/347_top_k_frequent_elements.py
+++ LeetCode/Blind 75/Heap/347_top_k_frequent_elements.py
@@ -25,3 +25,17 @@
 #
 # Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
 
+# Bucket Sort:
+#   -index: i(count), count/appearance of each value
+#   -values: the value that associates with the count
+# Ex: [1,1,1,2,2,3]; k = 2
+#
+# i (count): |  0  |  1  |  2  |  3  |  4  |  5  |  6  |     <-- size of given array; frequency of a value
+# values:    |     | [3] | [2] | [1] |     |     |     |     <-- value that associates to the count (above ^)
+#
+# Top k (top 2 values):
+#   - i (count): 2, 3; 3: most frequent element
+#   - values:   [2, 1] --> [1, 2]
+
+# Time: O(n); linear time; O(n) for input array, O(n) for values row, O(n) + O(n) -> O(n)
+# Space: O(n); hashmap to count the input array
