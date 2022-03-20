20fc8ec95ae35ffdc342d518498d9abd308676b8
henrylin2008 <henrylin2008@yahoo.com>
2022-03-20 01:37:42 -0700

    Merge k Sorted Lists

--- LeetCode/Blind 75/Heap/23_Merge_k_Sorted_Lists.py
+++ LeetCode/Blind 75/Heap/23_Merge_k_Sorted_Lists.py
@@ -39,3 +39,45 @@
 # lists[i] is sorted in ascending order.
 # The sum of lists[i].length will not exceed 104.
 
+
+from typing import Optional, List
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists or len(lists) == 0:
+            return None
+        while len(lists) > 1:   # pairs 2 lists together each time, until only one list left
+            merged_lists = []
+
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None     # if i+1 inbound, else None
+                merged_lists.append(self.mergeList(l1, l2))     # merge 2 lists
+            lists = merged_lists    # update the lists, merged_lists is a temp variable
+        return lists[0]
+
+    # merge 2 lists
+    def mergeList(self, l1, l2):
+        dummy = tail = ListNode()
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+        return dummy.next
