2458a7d2a35f036a84d4d94754b139bc9603b53a
henrylin2008 <henrylin2008@yahoo.com>
2022-03-22 11:42:29 -0700

    reorder list

--- LeetCode/Blind 75/Linked List/143_Reorder_List.py
+++ LeetCode/Blind 75/Linked List/143_Reorder_List.py
@@ -13,13 +13,10 @@
 #
 #
 # Example 1:
-#
-#
 # Input: head = [1,2,3,4]
 # Output: [1,4,2,3]
+#
 # Example 2:
-#
-#
 # Input: head = [1,2,3,4,5]
 # Output: [1,5,2,4,3]
 #
@@ -29,3 +26,41 @@
 # The number of nodes in the list is in the range [1, 5 * 104].
 # 1 <= Node.val <= 1000
 
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # find middle point
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reverse second half of list
+        second = slow.next      # beginning of the second half
+        slow.next = None        # end of the first half
+        prev = None             # the pointer for first node of second half
+        while second:           # while second half not null
+            temp = second.next  # next node
+            second.next = prev  # reverse link of beginning node
+            prev = second       # move the pointer to the beginning of the second half
+            second = temp       # move the pointer of second node of the second half
+
+        # merge two halves
+        first, second = head, prev          # prev: beginning of second half in reverse order (last node)
+        while second:
+            temp1, temp2 = first.next, second.next  # temp1: 2nd node of 1st half; temp2: 2nd node of 2nd half
+            first.next = second             # first -> second
+            second.next = temp1             # second -> first.next
+            first, second = temp1, temp2    # move the pointers to the next nodes
