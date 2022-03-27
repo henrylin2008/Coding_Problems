e35ae3d1e0243f2974ee22df3809ab6325652f0b
henrylin2008 <henrylin2008@yahoo.com>
2022-03-27 02:23:54 -0700

    Course Schedule

--- LeetCode/Blind 75/Graph/207_course_schedule.py
+++ LeetCode/Blind 75/Graph/207_course_schedule.py
@@ -34,3 +34,38 @@
 # prerequisites[i].length == 2
 # 0 <= ai, bi < numCourses
 # All the pairs prerequisites[i] are unique.
+
+# note: build adjacentcy_list with edges, run dfs on each V, if while dfs on V we see V again, then loop exists,
+# otherwise V isnt in a loop, 3 states= not visited, visited, still visiting
+
+from typing import List
+
+
+# Time: O(n + pre); n: number of nodes; preq: prerequisites
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # map each course to prereq list
+        pre_map = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            pre_map[crs].append(pre)
+
+        visit_set = set()  # all courses along the current DFS path
+
+        def dfs(crs):
+            if crs in visit_set:  # if the course already been visited; loop
+                return False
+            if pre_map[crs] == []:  # this course has no prereq
+                return True
+
+            visit_set.add(crs)  # add this crs to visit_set
+            for pre in pre_map[crs]:  # loop through prereq for this course
+                if not dfs(pre):  # run dfs on pre, if one course failed, then False
+                    return False
+            visit_set.remove(crs)  # remove visited crs from visit_set
+            pre_map[crs] = []  # set no prereq
+            return True
+
+        for crs in range(numCourses):  # run dfs on every course
+            if not dfs(crs):  # False if any return false
+                return False
+        return True
