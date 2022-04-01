c23ebe3324b261a4216a66148260431865cc1c84
henrylin2008 <henrylin2008@yahoo.com>
2022-04-01 10:10:45 -0700

    reverse string

--- LeetCode/Blind 75/String/Outside 75/344_reverse_string.py
+++ LeetCode/Blind 75/String/Outside 75/344_reverse_string.py
@@ -37,3 +37,31 @@ class Solution:
             s[l], s[r] = s[r], s[l]
             l += 1
             r -= 1
+
+    # Method 2: using a stack, add strings into a stack, then pop the top item and replace it with s[i]
+    # Time: O(n)
+    # Space: O(n)
+    # def reverseString(self, s: List[str]) -> None:
+    #     """
+    #     Do not return anything, modify s in-place instead.
+    #     """
+    #     stack = []
+    #     for c in s:
+    #         stack.append(c)
+    #     i = 0
+    #     while stack:
+    #         s[i] = stack.pop()
+    #         i += 1
+
+    # Method 3: recursive
+    # Time: O(n)
+    # Space: O(n)
+    #     def reverseString(self, s: List[str]) -> None:
+    #         """
+    #         Do not return anything, modify s in-place instead.
+    #         """
+    #         def reverse(l, r):
+    #             if l < r:
+    #                 s[l], s[r] = s[r], s[l]
+    #                 reverse(l + 1, r - 1)
+    #         reverse(0, len(s) - 1)
