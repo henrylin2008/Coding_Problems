7f2036cbcc097f7af2ab4acebf360526d028ebda
henrylin2008 <henrylin2008@yahoo.com>
2022-04-06 23:41:41 -0700

    Climbing stairs

--- LeetCode/Blind 75/Dynamic Programming/70_climbing_stairs.py
+++ LeetCode/Blind 75/Dynamic Programming/70_climbing_stairs.py
@@ -31,7 +31,10 @@
 # DP:        5+3=8   3+2=5   2+1=3  1+1=2     1       1
 # from:      1,2     2,3     3,4    4,5     (base cases)
 #                                             one    two
-
+#                                 one+two    two
+#                           one+two  two
+#                 one+two   two
+#          one+two   two
 # Time: O(n); looping through the ints
 # Space: O(1); only using 2 variables 
 class Solution:
