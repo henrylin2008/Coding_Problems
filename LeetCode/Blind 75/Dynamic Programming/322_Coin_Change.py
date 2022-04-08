0685e32f6c2e3559a98171d27c798fe161e3fb33
henrylin2008 <henrylin2008@yahoo.com>
2022-04-08 00:17:03 -0700

    coin change

--- LeetCode/Blind 75/Dynamic Programming/322_Coin_Change.py
+++ LeetCode/Blind 75/Dynamic Programming/322_Coin_Change.py
@@ -29,17 +29,21 @@
 # 1 <= coins.length <= 12
 # 1 <= coins[i] <= 231 - 1
 # 0 <= amount <= 104
+
+# Logic: Dynamic Programming - Button up; base case is dp[0] = 0 (zero coin), and an array with max value (amount + 1),
+# go through every amount and every given coin, find the min coins to get to the amount (min(dp[a], 1 + dp[a - c]));
+# return dp[amount] if it's not the default value else return -1
 from typing import List
 
 
-# Time: O(amount * len(coins))
+# Time: O(amount * len(coins)); given amount, given number of coins
 # Space: O(amount)
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] * (amount + 1)        # [max val]; 0...amount
+        dp = [amount + 1] * (amount + 1)        # [max val]; [0, max_val, max_val, .... amount]
         dp[0] = 0   # base case
-        for a in range(1, amount + 1):  # reverse order, going from 1 to amount
-            for c in coins:
-                if a - c >= 0:  # if amount - current_coin >= 0
-                    dp[a] = min(dp[a], 1 + dp[a - c])   # min coins; min(current_coin, current_coin(1) + dp[a-c] )
+        for a in range(1, amount + 1):  # reverse order, going from 1 to amount; for every amount
+            for c in coins:     # for every coin
+                if a - c >= 0:  # if remainder of amount - current coin is not negative, continue searching
+                    dp[a] = min(dp[a], 1 + dp[a - c])   # min coins; min(itself, 1(current coin) + dp[a-c](amt - c))
         return dp[amount] if dp[amount] != amount + 1 else -1   # return if the value is not the default value else -1
