2581532df97876720aeb01307ae58c246f1b6152
henrylin2008 <henrylin2008@yahoo.com>
2022-04-09 23:55:36 -0700

    House Robber

--- LeetCode/Blind 75/Dynamic Programming/198_House_Robber.py
+++ LeetCode/Blind 75/Dynamic Programming/198_House_Robber.py
@@ -29,3 +29,14 @@
 #
 # 1 <= nums.length <= 100
 # 0 <= nums[i] <= 400
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+        for n in nums:
+            temp = max(rob1 + n, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
