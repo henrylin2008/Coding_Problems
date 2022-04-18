21b354172fe9ce9768b08933a5e990387f041416
henrylin2008 <henrylin2008@yahoo.com>
2022-04-18 11:18:35 -0700

    Longest Common Subsequence

--- LeetCode/Blind 75/Dynamic Programming/1143_longest_common_subsequence.py
+++ LeetCode/Blind 75/Dynamic Programming/1143_longest_common_subsequence.py
@@ -34,3 +34,18 @@
 #
 # 1 <= text1.length, text2.length <= 1000
 # text1 and text2 consist of only lowercase English characters.
+
+class Solution:
+    # Dynamic problem - button up
+    # Time: O(n * m); n: len(text1), m: len(text2)
+    # Space: O(n * m); 2d dynamic grids (dp in the code), one extra column and one extra row with 0s
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+        # loop through 2D table in reverse order
+        for i in range(len(text1) - 1, -1, -1):     # reverse in rows
+            for j in range(len(text2) - 1, -1, -1): # reverse in columns
+                if text1[i] == text2[j]:            # if 2 characters match
+                    dp[i][j] = 1 + dp[i+1][j+1]     # +1 in diagonal
+                else:               # if 2 characters do not match
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])      # max of right grid or below grid
+        return dp[0][0]             # return the first grid in the 2D table
