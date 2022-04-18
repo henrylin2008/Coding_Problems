816fa645096ce1118da2f01f033ce855a6a2df70
henrylin2008 <henrylin2008@yahoo.com>
2022-04-18 01:05:26 -0700

    Longest Increasing Subsequence

--- LeetCode/Blind 75/Dynamic Programming/300_Longest_Increasing_Subsequence.py
+++ LeetCode/Blind 75/Dynamic Programming/300_Longest_Increasing_Subsequence.py
@@ -28,16 +28,25 @@
 #
 #
 # Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?
+
+# note: recursive: foreach num, get subseq with num and without num, only include num if prev was less, cache solution
+# of each; dp=subseq length which must end with each num, curr num must be after a prev dp or by itself;
+
 from typing import List
 
 
 # Dynamic programming
+# Idea: loop in the reverse order (back to the front), create a list with all 1s for caching; compare current number and
+# the subsequence num/s to find if the nums are in increasing order, if so, store the max(list[i], 1 +list[j]), and
+# return max value in the list
+# Time: O(n^2); it needs to compare every subsequence value/s
+# space: O(n); list to store every nums in the worse case
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         lis = [1] * len(nums)       # cache to store repeated work; a list with all 1s
 
         for i in range(len(nums) - 1, -1, -1):  # loop through in the reverse order
-            for j in range(i + 1, len(nums)):   # every subsequence after i
+            for j in range(i + 1, len(nums)):   # every subsequence after i; ex: (2, 4), (2, 3)
                 if nums[i] < nums[j]:           # if current num < next num:
                     lis[i] = max(lis[i], 1 + lis[j])    # max of list[i] or 1 + list[i]
         return max(lis)     # return max in the lis
