6145cc40ce039141b0407af540e334eaaf303ec6
henrylin2008 <henrylin2008@yahoo.com>
2022-04-28 01:05:36 -0700

    3sum

--- LeetCode/Blind 75/Two Pointers/15_3sum.py
+++ LeetCode/Blind 75/Two Pointers/15_3sum.py
@@ -25,3 +25,31 @@
 #
 # 0 <= nums.length <= 3000
 # -105 <= nums[i] <= 105
+from typing import List
+
+
+class Solution:
+    # Time: O(nlogn) + O(n^2) => O(n^2); O(nlogn): sorting; O(n^2): 2 nested loops
+    # Space: O(1) or O(n); O(1) for only using the pointers; O(n): sorting, for using certain libraries
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        for idx, val in enumerate(nums):
+            if idx > 0 and val == nums[idx - 1]:    # not the first value, and current value is same as last value
+                continue                            # skip
+
+            l, r = idx + 1, len(nums) - 1
+            while l < r:
+                three_sum = val + nums[l] + nums[r]
+                if three_sum > 0:
+                    r -= 1
+                elif three_sum < 0:
+                    l += 1
+                else:
+                    res.append([val, nums[l], nums[r]])
+                    l += 1      # update only the left pointer; conditions above would update the other pointer
+                    # ex: [-2, -2, 0, 0, 2, 2]
+                    while nums[l] == nums[l - 1] and l < r:     # current value == last value and left < right
+                        l += 1
+        return res
