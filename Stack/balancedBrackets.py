776ccd7a9652d4039d8ed29b5cb8c8ff796b8e62
henrylin2008 <henrylin2008@yahoo.com>
2022-05-02 10:56:50 -0700

    balanced brackets

--- Stack/balancedBrackets.py
+++ Stack/balancedBrackets.py
@@ -2,13 +2,13 @@
 # https://www.algoexpert.io/questions/Balanced%20Brackets
 # Difficulty: Medium
 #
-# Write a function that takes in a string made up brackets ((,[,{,),],and})) and other oeprational characters. The function should return
-# a boolean representing wheather the string is balanced with regards to brackets.
+# Write a function that takes in a string made up brackets ((,[,{,),],and})) and other operational characters. The
+# function should return a boolean representing whether the string is balanced with regards to bracket.
 #
-# A string is said to be balanced if it has as many opening brackets or a certain type as it has closing brackets of that
-# type and if no bracket is unmatched. Note that an opening bracket can't match a corresponding closing bracket that comes
-# before it, and similarly, a closing bracket can't match a corresponding opening bracket that comes after it. Also, brackets
-# can't overlap each other as in [(])
+# A string is said to be balanced if it has as many opening brackets or a certain type as it has closing brackets of
+# that type and if no bracket is unmatched. Note that an opening bracket can't match a corresponding closing bracket
+# that comes before it, and similarly, a closing bracket can't match a corresponding opening bracket that comes after
+# it. Also, brackets can't overlap each other as in [(])
 
 # Sample Input:
 # string = "([])(){}(())()()"
@@ -25,23 +25,23 @@
 def balancedBrackets(string):
     openingBrackets = "{[("  # string/array of all opening brackets
     closingBrackets = "}])"  # string/array of all closing brackets
-    matchingBrackets = {")":"(", "]":"[", "}":"{"} # use a dictionary to map closing bracket with corresponding opening bracket
-    stack = [] # empty array
+    matchingBrackets = {")": "(", "]": "[", "}": "{"}  # use a dictionary to map closing bracket with corresponding
+    # opening bracket
+    stack = []  # empty array
     for char in string:
-        if char in openingBrackets: # if current char is in openingBrackets, then store it to the stack
+        if char in openingBrackets:  # if current char is in openingBrackets, then store it to the stack
             stack.append(char)
-        elif char in closingBrackets: # if current char in closingBrackets:
+        elif char in closingBrackets:  # if current char in closingBrackets:
             if len(stack) == 0:  # if stack is empty: return False
                 print("False")
                 return False
-            elif stack[-1] == matchingBrackets[char]: # if top item in stack matches corresponding closing bracket
-                stack.pop() # pop top item from the stack
+            elif stack[-1] == matchingBrackets[char]:  # if top item in stack matches corresponding closing bracket
+                stack.pop()  # pop top item from the stack
             else:  # otherwise, it's a unbalanced stack, then return False
                 # print("False")
                 return False
     # print("true")
-    return len(stack) == 0 # return True if the stack is empty; otherwise there's unmatched bracket/s
-
+    return len(stack) == 0  # return True if the stack is empty; otherwise there's unmatched bracket/s
 
 # balancedBrackets("(()[[{{}}]})")
 # balancedBrackets("()[]{}{")
