6863dd0b680c313de361571097391a6d1b92e6ea
henrylin2008 <henrylin2008@yahoo.com>
2022-05-16 22:32:13 -0700

    Koko Eating Bananas

--- LeetCode/Blind 75/Binary Search/875_Koko_eating_bananas.py
+++ LeetCode/Blind 75/Binary Search/875_Koko_eating_bananas.py
@@ -37,22 +37,29 @@ import math
 
 
 class Solution:
-    # Time: O(log(max(p))*p)
+    # Time: O(log(max(p))*p); p: piles; log(max(p)): binary search
     # Space: O(1)
+    # Solution: binary search, each loop, find mid-point (k), can we eat banana with k and less than current h
+    #           -Ex: [3,6,7,11], H=8; k=(1+11)//2=6; l,r=1,11; hrs=[3/6,6/6,7/6,11/6]=1+1+2+2=6(<8); res: 6
+    #            next loop: k=3; l,r=1,5; hrs=[3/3,6/3,7/3,11/3]=1+2+3+4=10(>8); took too long, search right portion
+    #            next loop: k=4, l,r=4,5; hrs=[3/4,6/4,7/4,11/4]=1+2+2+3=8; res: 4 (4<6, update res)
+    #           -if hrs (6) <= h (8); shift right pointer to (m-1); 6 is valid, is it minimum? keep trying left portion,
+    #            update result if a minimum value is found, and shift right pointer (k - 1)
+    #           -if hrs > h, k is too small, not enough time to eat all bananas, search right portion (increase k)
     def minEatingSpeed(self, piles, h: int) -> int:
-        l, r = 1, max(piles)
-        res = r
+        l, r = 1, max(piles)  # left, right pointer
+        res = r  # r: at least max(piles) would work, and we are looking for the minimum, thus can't use 0 initially
 
         while l <= r:
-            k = (l + r) // 2
-            hours = 0
-            for p in piles:
-                hours += math.ceil(p / k)
+            k = (l + r) // 2  # mid point
+            hours = 0  # how many hours to eat all the bananas
+            for p in piles:  # for every pile
+                hours += math.ceil(p / k)  # total hours; p/k: how many hours take to eat bananas in a pile; round up
 
-            if hours <= h:
-                res = min(res, k)
-                r = k - 1
+            if hours <= h:  # if hours < given input h
+                res = min(res, k)  # get the min of res and k (mid-pointer)
+                r = k - 1  # search left portion
             else:
-                l = k + 1
+                l = k + 1  # search right portion
 
-        return res
+        return res  # return min in the res
