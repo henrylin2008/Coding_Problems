cbdb22108305070f7ebc68563eb5155fc0f068e2
henrylin2008 <henrylin2008@yahoo.com>
2022-05-19 23:22:59 -0700

    Median of Two Sorted Arrays

--- LeetCode/Blind 75/Binary Search/4_median_of_two_sorted_arrays.py
+++ LeetCode/Blind 75/Binary Search/4_median_of_two_sorted_arrays.py
@@ -27,3 +27,34 @@
 # 0 <= n <= 1000
 # 1 <= m + n <= 2000
 # -106 <= nums1[i], nums2[i] <= 106
+from typing import List
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        half = total // 2
+
+        if len(B) < len(A):
+            A, B = B, A
+
+        l, r = 0, len(A) - 1
+        while True:
+            i = (l + r) // 2    # A
+            j = half - i - 2    # B; -2 because index are starting 0 (i and j)
+
+            A_left = A[i] if i >= 0 else float("-infinity")
+            A_right = A[i + 1] if (i + 1) < len(A) else float("infinity")
+            B_left = B[j] if j >= 0 else float("-infinity")
+            B_right = B[j + 1] if (j + 1) < len(B) else float("infinity")
+
+            # Partition is correct
+            if A_left <= B_right and B_left <= A_right:
+                if total % 2:   # odd
+                    return min(A_right, B_right)
+                return (max(A_left, B_left) + min(A_right, B_right)) / 2    # even
+            elif A_left > B_right:
+                r = i - 1
+            else:
+                l = i + 1
