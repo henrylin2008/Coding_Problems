8880ca71b13b3fbf622fdca033baa606a28f10e7
henrylin2008 <henrylin2008@yahoo.com>
2022-05-22 01:41:46 -0700

    Median of Two Sorted Arrays

--- LeetCode/Blind 75/Binary Search/4_median_of_two_sorted_arrays.py
+++ LeetCode/Blind 75/Binary Search/4_median_of_two_sorted_arrays.py
@@ -31,6 +31,12 @@ from typing import List
 
 
 class Solution:
+    # Time: Log(min(n, m)); running binary search on the smaller array
+    # Space: O(1); no data structure is used
+    # Logic: get half of total length of 2 lists; run binary search on the shorter length(A), get the middle point of A
+    #        and B, and left, right value of A and B; if the partition is correction (A_left <= B_right and B_left <=
+    #        A_right): if it's an odd num, return min of either of A_right or B_right; else if it's even, get the min
+    #        of (A_right, B_right) and max of (A_left, B_left), then divide 2.
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         A, B = nums1, nums2
         total = len(nums1) + len(nums2)     # total length of 2 nums
@@ -38,23 +44,25 @@ class Solution:
 
         if len(B) < len(A):  # running binary search on A, ensure A is a smaller array
             A, B = B, A     # swap A, B; since we wanted to run binary search on A
-
-        l, r = 0, len(A) - 1    # left, right pointers for the binary search on A
+        # run binary search
+        l, r = 0, len(A) - 1    # left, right pointer of the binary search on A
         while True:
             i = (l + r) // 2    # middle point; A
-            j = half - i - 2    # B; -2 because index are starting 0 (i and j)
-
-            A_left = A[i] if i >= 0 else float("-infinity")  # if i is inbound set value at a[i] else default -inf
-            A_right = A[i + 1] if (i + 1) < len(A) else float("infinity")   # if i+1 inbound set A[i+1] else default inf
-            B_left = B[j] if j >= 0 else float("-infinity")  # if j in inbound set value at a[j] else default -inf
+            j = half - i - 2    # rest of the half from B; -2 because index are starting 0 (i and j)
+            # get values to find mediums
+            #   set value if inbound else default value
+            A_left = A[i] if i >= 0 else float("-infinity")  # if i is inbound set the value at i else default -inf
+            A_right = A[i + 1] if (i + 1) < len(A) else float("infinity")  # if i+1 inbound set A[i+1] else default inf
+            B_left = B[j] if j >= 0 else float("-infinity")  # if j is inbound set value at j else default -inf
             B_right = B[j + 1] if (j + 1) < len(B) else float("infinity")   # if j+1 inbound set A[j+1] else default inf
 
-            # Partition is correct
-            if A_left <= B_right and B_left <= A_right:  # check if
-                if total % 2:   # odd, total % 2 == 1
-                    return min(A_right, B_right)
-                return (max(A_left, B_left) + min(A_right, B_right)) / 2  # even
-            elif A_left > B_right:
-                r = i - 1   # reduce size of left partition of A
+            if A_left <= B_right and B_left <= A_right:  # Partition is correct
+                # odd
+                if total % 2:   # if total % 2 == 1
+                    return min(A_right, B_right)    # if 4, infinity, return 4
+                # even
+                return (max(A_left, B_left) + min(A_right, B_right)) / 2  # get the medium from 2 middle values
+            elif A_left > B_right:  # if A_left has too many elements
+                r = i - 1   # reduce size of left partition from A
             else:
-                l = i + 1   # increase size of left partition of A
+                l = i + 1   # increase size of left partition from A
