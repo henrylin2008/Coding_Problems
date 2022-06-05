f64ed19688cb9ba4dece2828282fd5222f9269bc
henrylin2008 <henrylin2008@yahoo.com>
2022-06-04 23:44:43 -0700

    Balanced Binary Tree

--- LeetCode/Blind 75/Graphs/DFS/110_Balanced_Binary_Tree.py
+++ LeetCode/Blind 75/Graphs/DFS/110_Balanced_Binary_Tree.py
@@ -26,3 +26,27 @@
 #
 # The number of nodes in the tree is in the range [0, 5000].
 # -104 <= Node.val <= 104
+
+
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            if not root:    # empty tree
+                return [True, 0]    # [True/False, height]
+
+            left, right = dfs(root.left), dfs(root.right)
+            balanced = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)
+
+            return [balanced, 1 + max(left[1], right[1])]
+        return dfs(root)[0]
