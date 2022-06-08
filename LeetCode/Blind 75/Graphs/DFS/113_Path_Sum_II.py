545b4bb2eea9411d671a04fb42ccc15639d8ca87
henrylin2008 <henrylin2008@yahoo.com>
2022-06-07 23:45:55 -0700

    Path sum II

--- LeetCode/Blind 75/Graphs/DFS/113_Path_Sum_II.py
+++ LeetCode/Blind 75/Graphs/DFS/113_Path_Sum_II.py
@@ -40,3 +40,29 @@
 # The number of nodes in the tree is in the range [0, 5000].
 # -1000 <= Node.val <= 1000
 # -1000 <= targetSum <= 1000
+# Definition for a binary tree node.
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        res = []
+
+        def dfs(node, path, cur_sum):
+            if not node:
+                return False
+
+            cur_sum += node.val
+            if not node.left and not node.right and cur_sum == targetSum:
+                res.append(path + [node.val])
+            return dfs(node.left, path + [node.val], cur_sum) or dfs(node.right, path + [node.val], cur_sum)
+
+        dfs(root, [], 0)
+        return res
