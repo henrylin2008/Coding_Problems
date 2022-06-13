ed7471c0b8d59913c47466c83f0c4dfb1d8fa8df
henrylin2008 <henrylin2008@yahoo.com>
2022-06-12 23:45:30 -0700

    Lowest Common Ancestor of a binary tree

--- LeetCode/Blind 75/Graphs/DFS/236_Lowest_Common_Ancestor_of_a_binary_tree.py
+++ LeetCode/Blind 75/Graphs/DFS/236_Lowest_Common_Ancestor_of_a_binary_tree.py
@@ -32,3 +32,34 @@
 # All Node.val are unique.
 # p != q
 # p and q will exist in the tree.
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def lca(root, node1, node2):
+    if not root:    # Empty node
+        return
+
+    # case 2 in above figure
+    if root == node1 or root == node2:
+        return root
+
+    left = lca(root.left, node1, node2)
+    right = lca(root.right, node1, node2)
+
+    # case 1
+    if left and right:
+        return root
+
+    # at this point, left and right can't be both non-null since we checked above
+    # case 4 and 5, report target node or LCA back to parent
+    if left:
+        return left
+    if right:
+        return right
+
+    # case 4, not found return null
+    return None
