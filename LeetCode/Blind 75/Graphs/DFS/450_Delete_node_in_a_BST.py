6dc101265b56bb0d1a24d8fa97ef38fa178b94a0
henrylin2008 <henrylin2008@yahoo.com>
2022-06-17 23:43:13 -0700

    delete node in a BST

--- LeetCode/Blind 75/Graphs/DFS/450_Delete_node_in_a_BST.py
+++ LeetCode/Blind 75/Graphs/DFS/450_Delete_node_in_a_BST.py
@@ -50,3 +50,41 @@
 # Each node has a unique value.
 # root is a valid binary search tree.
 # -105 <= key <= 105
+
+
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        if root.val == key:
+            # 4 possibilities
+            if not root.left and not root.right:
+                return None
+            if not root.left and root.right:
+                return root.right
+            if not root.right and root.left:
+                return root.left
+            pointer = root.right
+            while pointer.left:
+                pointer = pointer.left
+            root.val = pointer.val
+            root.right = self.deleteNode(root.right, root.val)
+
+        elif root.val > key:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            root.right = self.deleteNode(root.right, key)
+
+        return root
