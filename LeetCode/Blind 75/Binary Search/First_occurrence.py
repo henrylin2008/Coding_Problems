0b57a839f323f547c216adf6bb5fc3cd68a4544b
henrylin2008 <henrylin2008@yahoo.com>
2022-07-01 10:42:37 -0700

    Find Element in Sorted Array with Duplicates

--- LeetCode/Blind 75/Binary Search/First_occurrence.py
+++ LeetCode/Blind 75/Binary Search/First_occurrence.py
@@ -19,3 +19,29 @@
 # Explanation: 6 does not exists in the array.
 
 
+from typing import List
+
+
+# Time Complexity: O(log(n))
+# The problem is equivalent to finding the boundary of elements < 3 and elements >= 3. Now the problem is reduced to
+# finding the first true element in a boolean array.
+def find_first_occurrence(arr: List[int], target: int) -> int:
+    l, r = 0, len(arr) - 1
+    ans = -1
+    while l <= r:
+        mid = (l + r) // 2
+        if arr[mid] == target:
+            ans = mid       # update the answer if the current value == target, and shift the right pointer
+            r = mid - 1
+        elif arr[mid] < target:
+            l = mid + 1
+        else:       # need to shift the right pointer if current value (arr[mid]) > target
+            r = mid - 1
+    return ans
+
+
+if __name__ == '__main__':
+    arr = [int(x) for x in input().split()]
+    target = int(input())
+    res = find_first_occurrence(arr, target)
+    print(res)
