96812efbce38b04724c429c619f65094fd1de9f2
henrylin2008 <henrylin2008@yahoo.com>
2022-06-30 22:38:56 -0700

    Finding the boundary with Binary Search

---
+++ LeetCode/Blind 75/Binary Search/Finding_the_boundary.py
@@ -0,0 +1,13 @@
+# Finding the Boundary with Binary Search
+# https://algo.monster/problems/binary_search_boundary
+
+# An array of boolean values is divided into two sections; the left section consists of all false and the right
+# section consists of all true. Find the boundary of the right section, i.e. the index of the first true element. If
+# there is no true element, return -1.
+#
+# Input: arr = [false, false, true, true, true]
+#
+# Output: 2
+#
+# Explanation: first true's index is 2.
+
