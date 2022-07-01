99186ec32bbbe489f3e18a2f73c4a7f34703f2ef
henrylin2008 <henrylin2008@yahoo.com>
2022-06-30 22:45:45 -0700

    Finding the boundary with Binary Search

--- LeetCode/Blind 75/Binary Search/Finding_the_boundary.py
+++ LeetCode/Blind 75/Binary Search/Finding_the_boundary.py
@@ -11,3 +11,32 @@
 #
 # Explanation: first true's index is 2.
 
+from typing import List
+
+
+def find_boundary(arr: List[bool]) -> int:
+    l, r = 0, len(arr) - 1
+    boundary = -1
+    while l <= r:
+        mid = (l + r) // 2
+        if arr[mid]:
+            boundary = mid
+            r = mid - 1
+        else:
+            l = mid + 1
+
+    return boundary
+
+
+if __name__ == '__main__':
+    arr = [x == "true" for x in input().split()]
+    res = find_boundary(arr)
+    print(res)
+
+
+# Input:
+# test #1: false false true true true
+# test #2: true
+# test #3: false false false
+# test #4: true true true true true
+# test #5: false true
