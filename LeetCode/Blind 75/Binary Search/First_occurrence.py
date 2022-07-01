07971d8356cdb092c4df41611a70cbf71b21dc27
henrylin2008 <henrylin2008@yahoo.com>
2022-07-01 10:38:25 -0700

    Find Element in Sorted Array with Duplicates

---
+++ LeetCode/Blind 75/Binary Search/First_occurrence.py
@@ -0,0 +1,21 @@
+# Find Element in Sorted Array with Duplicates
+# https://algo.monster/problems/binary_search_duplicates
+
+# Given a sorted array of integers and a target integer, find the first occurrence of the target and return its
+# index. Return -1 if the target is not in the array.
+#
+# Input:
+# arr = [1, 3, 3, 3, 3, 6, 10, 10, 10, 100]
+# target = 3
+#
+# Output: 1
+# Explanation: First occurrence of 3 is at index 1.
+#
+# Input:
+# arr = [2, 3, 5, 7, 11, 13, 17, 19]
+# target = 6
+#
+# Output: -1
+# Explanation: 6 does not exists in the array.
+
+
