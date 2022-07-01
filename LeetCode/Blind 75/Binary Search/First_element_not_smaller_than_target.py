d835bf0c56841b02d5ad8d4b209d789e71e9ec5e
henrylin2008 <henrylin2008@yahoo.com>
2022-06-30 23:48:19 -0700

    First Element Not Smaller Than Target

---
+++ LeetCode/Blind 75/Binary Search/First_element_not_smaller_than_target.py
@@ -0,0 +1,18 @@
+# First Element Not Smaller Than Target
+# Link: https://algo.monster/problems/binary_search_first_element_not_smaller_than_target
+
+# Given an array of integers sorted in increasing order and a target, find the index of the first element in the
+# array that is larger than or equal to the target. Assume that it is guaranteed to find a satisfying number.
+#
+# Input:
+#   arr = [1, 3, 3, 5, 8, 8, 10]
+#   target = 2
+# Output: 1
+# Explanation: the first element larger than 2 is 3 which has index 1.
+#
+# Input:
+#   arr = [2, 3, 5, 7, 11, 13, 17, 19]
+#   target = 6
+# Output: 3
+# Explanation: the first element larger than 6 is 7 which has index 3.
+
