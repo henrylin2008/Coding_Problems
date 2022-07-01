6415badcae4ce47451b4d7cfc489aa30e5e74889
henrylin2008 <henrylin2008@yahoo.com>
2022-07-01 10:46:12 -0700

    square root

---
+++ LeetCode/Blind 75/Binary Search/Sorted Array/square_root.py
@@ -0,0 +1,14 @@
+# Square Root
+# https://algo.monster/problems/sqrt
+
+# Given an integer, find its square root without using the built-in square root function. Only return the integer
+# part (truncate the decimals).
+#
+# Input: 16
+# Output: 4
+#
+# Input: 8
+# Output: 2
+#
+# Explanation: square root of 8 is 2.83..., return integer part 2
+
