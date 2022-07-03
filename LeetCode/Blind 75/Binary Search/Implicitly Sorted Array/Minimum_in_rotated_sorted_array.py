b98d33c1bdf23182dbb29b4600eb0550c9b22ba2
henrylin2008 <henrylin2008@yahoo.com>
2022-07-02 23:54:05 -0700

    Find Minimum in Rotated Sorted Array

---
+++ LeetCode/Blind 75/Binary Search/Implicitly Sorted Array/Minimum_in_rotated_sorted_array.py
@@ -0,0 +1,14 @@
+# Find Minimum in Rotated Sorted Array
+# link: https://algo.monster/problems/min_in_rotated_sorted_array
+
+# A sorted array of unique integers was rotated at an unknown pivot. For example, [10, 20, 30, 40, 50] becomes [30,
+# 40, 50, 10, 20]. Find the index of the minimum element in this array. All the numbers are unique.
+#
+# Input: [30, 40, 50, 10, 20]
+# Output: 3
+# Explanation: the smallest element is 10 and its index is 3.
+#
+# Input: [3, 5, 7, 11, 13, 17, 19, 2]
+# Output: 7
+# Explanation: the smallest element is 2 and its index is 7.
+
