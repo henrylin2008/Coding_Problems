a7dfd5587810def023a04d2c197813879c7b5251
henrylin2008 <henrylin2008@yahoo.com>
2022-07-04 00:10:18 -0700

    The Peak of a Mountain Array

--- LeetCode/Blind 75/Binary Search/Implicitly Sorted Array/Peak_of_Mountain_Array.py
+++ LeetCode/Blind 75/Binary Search/Implicitly Sorted Array/Peak_of_Mountain_Array.py
@@ -6,8 +6,9 @@
 #  -has an element with the largest value called "peak", with index k. The array elements monotonically increase from
 #  the first element to A[k], and then monotonically decreases from A[k + 1] to the last element of the array. Thus
 #  creating a "mountain" of numbers.
-#  -That is, given A[0]<...<A[k-1]<A[k]>A[k+1]>...>A[n-1], we need to find the index k. Note that the peak element is
-#  neither the first nor the last element of the array.
+#
+# That is, given A[0]<...<A[k-1]<A[k]>A[k+1]>...>A[n-1], we need to find the index k. Note that the peak element is
+# neither the first nor the last element of the array.
 #
 # Find the index of the peak element. Assume there is only one peak element.
 #
@@ -16,3 +17,26 @@
 # Output: 3
 #
 # Explanation: the largest element is 3 and its index is 3.
+
+
+from typing import List
+
+
+def peak_of_mountain_array(arr: List[int]) -> int:
+    left, right = 0, len(arr) - 1
+    boundary = -1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] > arr[mid + 1]:
+            boundary = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+    return boundary
+
+
+if __name__ == '__main__':
+    arr = [int(x) for x in input().split()]
+    res = peak_of_mountain_array(arr)
+    print(res)
