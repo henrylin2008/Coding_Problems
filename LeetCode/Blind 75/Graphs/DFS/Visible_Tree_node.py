0793441462f0c37f6dfa8a5bc6ef7edc3a0001b8
henrylin2008 <henrylin2008@yahoo.com>
2022-07-11 23:49:37 -0700

    Visible Tree node

--- LeetCode/Blind 75/Graphs/DFS/Visible_Tree_node.py
+++ LeetCode/Blind 75/Graphs/DFS/Visible_Tree_node.py
@@ -48,8 +48,8 @@ def visible_tree_node(root: Node) -> int:
         if root.val >= max_sofar:
             total += 1
 
-        total += dfs(root.left, max(max_sofar, root.val))
-        # max_sofar for child node is the larger of previous max and current node val
+        total += dfs(root.left, max(max_sofar, root.val))  # max_sofar for child node is the larger of previous max
+        # and current node val
         total += dfs(root.right, max(max_sofar, root.val))
 
         return total
