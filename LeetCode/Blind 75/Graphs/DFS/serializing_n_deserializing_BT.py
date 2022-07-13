ca7af653ead326811e283b3d9d044f19fab0eb12
henrylin2008 <henrylin2008@yahoo.com>
2022-07-12 23:43:17 -0700

    Serializing and Deserializing Binary Tree

---
+++ LeetCode/Blind 75/Graphs/DFS/serializing_n_deserializing_BT.py
@@ -0,0 +1,6 @@
+# Serializing and Deserializing Binary Tree
+# https://algo.monster/problems/serializing_tree
+
+# Given a binary tree, write a serialize function that converts the tree into a string and a deserialize function
+# that converts a string to a binary tree. You may serialize the tree into any string representation you want as long
+# as it can be deseralized.
