99d379fa3cfd639a414cb671af6552dda2f0b884
henrylin2008 <henrylin2008@yahoo.com>
2022-07-12 23:47:40 -0700

    Balanced Binary Tree

--- LeetCode/Blind 75/Graphs/DFS/110_Balanced_Binary_Tree.py
+++ LeetCode/Blind 75/Graphs/DFS/110_Balanced_Binary_Tree.py
@@ -46,12 +46,55 @@ class Solution:
     #       it reaches the top of the tree
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         def dfs(root):
-            if not root:    # empty tree
-                return [True, 0]    # [True/False, height]
+            if not root:  # empty tree
+                return [True, 0]  # [True/False, height]
 
-            left, right = dfs(root.left), dfs(root.right)   # dfs on the left, right subtrees
+            left, right = dfs(root.left), dfs(root.right)  # dfs on the left, right subtrees
             # tree is balanced if left/right subtrees returns True, and root subtree is balanced (abs(l[1] - r[1] <= 1)
             balanced = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)
 
-            return [balanced, 1 + max(left[1], right[1])]   # [is tree balanced from the root, height of the tree]
-        return dfs(root)[0]    # [True/False], idx: 0
+            return [balanced, 1 + max(left[1], right[1])]  # [is tree balanced from the root, height of the tree]
+
+        return dfs(root)[0]  # [True/False], idx: 0
+
+
+# Solution #2
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# Returns -1 if is not a binary tree. The height if it is.
+def tree_height(tree):
+    if tree is None:
+        return 0
+    left_height = tree_height(tree.left)
+    right_height = tree_height(tree.right)
+    if left_height is -1 or right_height is -1:
+        return -1
+    if abs(left_height - right_height) > 1:
+        return -1
+    return max(left_height, right_height) + 1
+
+
+def is_balanced(tree: Node) -> bool:
+    return tree_height(tree) != -1
+
+
+# this function build a tree from input
+# learn more about how trees are encoded in https://algo.monster/problems/serializing_tree
+def build_tree(nodes, f):
+    val = next(nodes)
+    if val == 'x': return None
+    left = build_tree(nodes, f)
+    right = build_tree(nodes, f)
+    return Node(f(val), left, right)
+
+
+if __name__ == '__main__':
+    tree = build_tree(iter(input().split()), int)
+    res = is_balanced(tree)
+    print('true' if res else 'false')
