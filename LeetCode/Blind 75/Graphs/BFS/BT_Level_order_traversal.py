78b7350c1d22d4148be98fe39f6741229bd593d2
henrylin2008 <henrylin2008@yahoo.com>
2022-07-17 00:55:19 -0700

    BT level order traversal

--- LeetCode/Blind 75/Graphs/BFS/BT_Level_order_traversal.py
+++ LeetCode/Blind 75/Graphs/BFS/BT_Level_order_traversal.py
@@ -13,3 +13,47 @@
 #          \
 #           7              -------- Level 3           [7] ]
 
+# Output: [[1], [2,3], [4,5,6], [7]]
+
+from collections import deque
+from typing import List
+
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def level_order_traversal(root: Node) -> List[List[int]]:
+    res = []
+    queue = deque([root])  # at least one element in the queue to kick start bfs
+    while len(queue) > 0:  # as long as there is element in the queue
+        n = len(queue)  # number of nodes in current level
+        new_level = []  # store child node/s
+        for _ in range(n):  # dequeue each node in the current level
+            node = queue.popleft()
+            new_level.append(node.val)
+            for child in [node.left, node.right]:  # enqueue non-null children
+                if child is not None:
+                    queue.append(child)
+        res.append(new_level)
+    return res
+
+
+# this function build a tree from input
+# learn more about how trees are encoded in https://algo.monster/problems/serializing_tree
+def build_tree(nodes, f):
+    val = next(nodes)
+    if val == 'x': return None
+    left = build_tree(nodes, f)
+    right = build_tree(nodes, f)
+    return Node(f(val), left, right)
+
+
+if __name__ == '__main__':
+    root = build_tree(iter(input().split()), int)
+    res = level_order_traversal(root)
+    for row in res:
+        print(' '.join(map(str, row)))
