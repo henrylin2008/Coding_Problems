5d927975a53ab1e1587a14916113ff13bd103318
henrylin2008 <henrylin2008@yahoo.com>
2022-07-16 23:30:42 -0700

    Binary Tree Level Order Traversal

---
+++ LeetCode/Blind 75/Graphs/BFS/BT_Level_order_traversal.py
@@ -0,0 +1,15 @@
+# Binary Tree Level Order Traversal
+# https://algo.monster/problems/binary_tree_level_order_traversal
+
+# Given a binary tree, return its level order traversal. The input is the root node of the tree. The output should be
+# a list of lists of integers, with the ith list containing the values of nodes on level i, from left to right.
+#
+# For example:
+#               1          -------- Level 0         [ [1],
+#             /   \
+#            2     3       -------- Level 1           [2, 3],
+#          /  \      \
+#         4     5     6    -------- Level 2           [4, 5, 6],
+#          \
+#           7              -------- Level 3           [7] ]
+
