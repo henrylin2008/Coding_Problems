72c2360a8e843d12dd786408e5745011ac1ddb4e
henrylin2008 <henrylin2008@yahoo.com>
2022-07-19 00:52:29 -0700

    Binary Tree Right Side View

---
+++ LeetCode/Blind 75/Graphs/BFS/199_Binary_Tree_right_side_view.py
@@ -0,0 +1,30 @@
+# 199. Binary Tree Right Side View
+# https://leetcode.com/problems/binary-tree-right-side-view/
+# https://algo.monster/problems/binary_tree_right_side_view
+# Medium
+
+# Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes
+# you can see ordered from top to bottom.
+
+# Example 1:
+#               1
+#            /     \
+#          2         3
+#           \          \
+#             5          4
+# Input: root = [1,2,3,null,5,null,4]
+# Output: [1,3,4]
+#
+# Example 2:
+# Input: root = [1,null,3]
+# Output: [1,3]
+#
+# Example 3:
+# Input: root = []
+# Output: []
+
+# Constraints:
+#
+# The number of nodes in the tree is in the range [0, 100].
+# -100 <= Node.val <= 100
+
