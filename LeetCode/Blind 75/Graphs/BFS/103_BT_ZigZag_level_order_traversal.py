cb5f56f2ca93874e4156011699490ad26ee8607a
henrylin2008 <henrylin2008@yahoo.com>
2022-07-18 23:55:56 -0700

    Binary Tree Zigzag Level Order Traversal

--- LeetCode/Blind 75/Graphs/BFS/103_BT_ZigZag_level_order_traversal.py
+++ LeetCode/Blind 75/Graphs/BFS/103_BT_ZigZag_level_order_traversal.py
@@ -23,3 +23,50 @@
 # Input: root = []
 # Output: []
 
+from collections import deque
+from typing import List
+
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def zig_zag_traversal(root: Node) -> List[List[int]]:
+    res = []
+    queue = deque([root])  # at least one element in the queue to kick start bfs
+    left_to_right = True
+    while len(queue) > 0:  # as long as there is element in the queue
+        n = len(queue)  # number of nodes in current level, see explanation above
+        new_level = []
+        for _ in range(n):  # dequeue each node in the current level
+            node = queue.popleft()
+            if node:
+                new_level.append(node.val)
+                queue.append(node.left)
+                queue.append(node.right)
+        if not left_to_right:
+            new_level.reverse()  # reverse current level
+        if new_level:
+            res.append(new_level)
+        left_to_right = not left_to_right  # flip flag
+    return res
+
+
+# this function build a tree from input
+# learn more about how trees are encoded in https://algo.monster/problems/serializing_tree
+def build_tree(nodes, f):
+    val = next(nodes)
+    if val == 'x': return None
+    left = build_tree(nodes, f)
+    right = build_tree(nodes, f)
+    return Node(f(val), left, right)
+
+
+if __name__ == '__main__':
+    root = build_tree(iter(input().split()), int)
+    res = zig_zag_traversal(root)
+    for row in res:
+        print(' '.join(map(str, row)))
