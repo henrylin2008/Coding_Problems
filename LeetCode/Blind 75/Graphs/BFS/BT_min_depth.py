aaf57e9b80f298d0804a438a87f626c513ea45be
henrylin2008 <henrylin2008@yahoo.com>
2022-07-20 00:28:58 -0700

    Binary Tree Min Depth

---
+++ LeetCode/Blind 75/Graphs/BFS/BT_min_depth.py
@@ -0,0 +1,14 @@
+# Binary Tree Min Depth
+# https://algo.monster/problems/binary_tree_min_depth
+
+# Given a binary tree, find the depth of the shallowest leaf node.
+#                  1                --- Level 0
+#               /     \
+#             2         3           --- Level 1
+#           /   \         \
+#         4      5          6       --- Level 2
+#           \      \
+#            7      8               --- Level 3
+
+# 6 is the shallowest leaf, with depth of 2
+
