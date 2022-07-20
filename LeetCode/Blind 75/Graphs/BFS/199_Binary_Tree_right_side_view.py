31e5de81d175528be6d9ee37a5639de0e7a90c06
henrylin2008 <henrylin2008@yahoo.com>
2022-07-19 23:36:02 -0700

    Binary Tree Right Side View

--- LeetCode/Blind 75/Graphs/BFS/199_Binary_Tree_right_side_view.py
+++ LeetCode/Blind 75/Graphs/BFS/199_Binary_Tree_right_side_view.py
@@ -28,3 +28,41 @@
 # The number of nodes in the tree is in the range [0, 100].
 # -100 <= Node.val <= 100
 
+from collections import deque
+
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def binary_tree_right_side_view(root):
+    res = []
+    queue = deque([root])  # at least one element in the queue to kick start bfs
+    while len(queue) > 0:  # as long as there is element in the queue
+        n = len(queue)  # number of nodes in current level
+        res.append(queue[0].val)  # only append the first node we encounter since it's the rightmost
+        for _ in range(n):  # dequeue each node in the current level
+            node = queue.popleft()
+            for child in [node.right, node.left]:  # we add right children first so it'll pop out of the queue first
+                if child is not None:
+                    queue.append(child)
+    return res
+
+
+# this function build a tree from input
+# learn more about how trees are encoded in https://algo.monster/problems/serializing_tree
+def build_tree(nodes, f):
+    val = next(nodes)
+    if val == 'x': return None
+    left = build_tree(nodes, f)
+    right = build_tree(nodes, f)
+    return Node(f(val), left, right)
+
+
+if __name__ == '__main__':
+    root = build_tree(iter(input().split()), int)
+    res = binary_tree_right_side_view(root)
+    print(' '.join(map(str, res)))
