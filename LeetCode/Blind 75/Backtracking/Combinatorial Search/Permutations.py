07764c71567a1a4c87561fc2ec2b75f2d5cc1226
henrylin2008 <henrylin2008@yahoo.com>
2022-07-22 00:45:44 -0700

    Permutations

---
+++ LeetCode/Blind 75/Backtracking/Combinatorial Search/Permutations.py
@@ -0,0 +1,13 @@
+# Permutations
+# Given a string of unique letters, find all of its distinct permutations.
+#
+# Input
+# letters: a string of unique letters
+# Output
+# all of its distinct permutations
+#
+# Example 1:
+# Input:
+# letters = `abc`
+# Output:
+# ```` abc acb bac bca cab cba
