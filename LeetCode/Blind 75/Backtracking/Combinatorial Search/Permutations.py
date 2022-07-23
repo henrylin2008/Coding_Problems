f45aeaf3dd8394ad615f7dd81bb9a096f49e7bda
henrylin2008 <henrylin2008@yahoo.com>
2022-07-22 23:44:06 -0700

    Permutations

--- LeetCode/Blind 75/Backtracking/Combinatorial Search/Permutations.py
+++ LeetCode/Blind 75/Backtracking/Combinatorial Search/Permutations.py
@@ -11,3 +11,36 @@
 # letters = `abc`
 # Output:
 # ```` abc acb bac bca cab cba
+
+
+from typing import List
+
+
+def permutations(letters):
+    def dfs(path, used, res):
+        if len(path) == len(letters):
+            res.append(''.join(path))
+            return
+
+        for i, letter in enumerate(letters):
+            # skip used letters
+            if used[i]:
+                continue
+            # add letter to permutation, mark letter as used
+            path.append(letter)
+            used[i] = True
+            dfs(path, used, res)
+            # remove letter from permutation, mark letter as unused
+            path.pop()
+            used[i] = False
+
+    res = []
+    dfs([], [False] * len(letters), res)
+    return res
+
+
+if __name__ == '__main__':
+    letters = input()
+    res = permutations(letters)
+    for line in res:
+        print(line)
