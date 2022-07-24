c4cf2e19dc10615b072a124e2a8a6295bacad430
henrylin2008 <henrylin2008@yahoo.com>
2022-07-24 00:00:12 -0700

    Letter Combinations of A Phone Number

---
+++ LeetCode/Blind 75/Backtracking/Combinatorial Search/Letter Combinations of A Phone Number.py
@@ -0,0 +1,17 @@
+# Letter Combinations of A Phone Number
+# Link: https://algo.monster/problems/letter_combinations_of_phone_number
+
+# Given a phone number that contains digits 2-9, find all possible letter combinations the phone number could translate
+# to.
+#       1       2       3
+#             A B C   D E F
+#       4       5       6
+#     G H I   J K L   M N O
+#       7       8       9
+#    P Q R S  T U V   W X Y Z
+# Input:
+# "56"
+#
+# Output:
+# ["jm","jn","jo","km","kn","ko","lm","ln","lo"]
+
