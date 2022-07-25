763dd8b9f40c99a5fc9095366be26590f66bfabf
henrylin2008 <henrylin2008@yahoo.com>
2022-07-25 00:41:03 -0700

    Ternary Tree Paths

---
+++ LeetCode/Blind 75/Backtracking/Combinatorial Search/Ternary Tree Paths.py
@@ -0,0 +1,57 @@
+# Ternary Tree Paths
+# https://algo.monster/problems/dfs_with_states
+
+# Given a ternary tree (each node of the tree has at most three children), find all root-to-leaf paths.
+
+
+#               1
+#           /   |   \
+#         2     4     6
+#        /
+#       3
+
+# [ 1 -> 2 -> 3,
+#   1 -> 4,
+#   1 -> 6 ]
+
+
+from typing import List
+
+
+class Node:
+    def __init__(self, val, children=None):
+        if children is None:
+            children = []
+        self.val = val
+        self.children = children
+
+
+def ternary_tree_paths(root: Node) -> List[str]:
+    def dfs(root, path, res):
+        if all(c is None for c in root.children):
+            res.append('->'.join(path) + '->' + str(root.val))
+            return
+
+        for child in root.children:
+            if child:
+                dfs(child, path + [str(root.val)], res)
+
+    res = []
+    if root: dfs(root, [], res)
+    return res
+
+
+# this function build a tree from input
+# learn more about how trees are encoded in https://algo.monster/problems/serializing_tree
+def build_tree(nodes, f):
+    val = next(nodes)
+    num = int(next(nodes))
+    children = [build_tree(nodes, f) for _ in range(num)]
+    return Node(f(val), children)
+
+
+if __name__ == '__main__':
+    root = build_tree(iter(input().split()), int)
+    res = ternary_tree_paths(root)
+    for line in res:
+        print(line)
