a7b6579e636d2d9070350c3374c8ce20f57198c3
henrylin2008 <henrylin2008@yahoo.com>
2022-07-28 23:47:53 -0700

    Word Break

---
+++ LeetCode/Blind 75/Backtracking/Memoization/Word_Break.py
@@ -0,0 +1,15 @@
+# Word Break
+# https://algo.monster/problems/word_break
+
+# Given a string and a list of words, determine if the string can be constructed from concatenating words from the
+# list of words. A word can be used multiple times.
+#
+# Input:
+# s = "algomonster"
+# words = ["algo", "monster"]
+# Output: true
+#
+# Input:
+# s = "aab"
+# words = ["a", "c"]
+# Output: false
