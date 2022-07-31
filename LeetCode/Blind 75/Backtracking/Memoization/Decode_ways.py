aa414b5cf37dbc501e925ca049d6b06283004368
henrylin2008 <henrylin2008@yahoo.com>
2022-07-30 23:26:35 -0700

    Decode ways

---
+++ LeetCode/Blind 75/Backtracking/Memoization/Decode_ways.py
@@ -0,0 +1,27 @@
+# Decode Ways
+# Link: https://algo.monster/problems/decode_ways
+
+# We have a message to decode. Letters are encoded to digits by their positions in the alphabet
+#
+# 1 A -> 1
+# 2 B -> 2
+# 3 C -> 3
+# 4 ...
+# 5 Y -> 25
+# 6 Z -> 26
+#
+# Given a non-empty string of digits, how many ways are there to decode it?
+#
+# Input: "18"
+#
+# Output: 2
+#
+# Explanation: "18" can be decoded as "AH" or "R"
+#
+# Input: "123"
+#
+# Output: 3
+#
+# Explanation: "123" can be decoded as "ABC", "LC", "AW"
+
+
