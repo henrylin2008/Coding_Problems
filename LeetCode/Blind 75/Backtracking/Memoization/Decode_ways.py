0a3e1f49cf3e14da2d1528409f9466c322bbc375
henrylin2008 <henrylin2008@yahoo.com>
2022-07-30 23:28:36 -0700

    Decode ways

--- LeetCode/Blind 75/Backtracking/Memoization/Decode_ways.py
+++ LeetCode/Blind 75/Backtracking/Memoization/Decode_ways.py
@@ -25,3 +25,40 @@
 # Explanation: "123" can be decoded as "ABC", "LC", "AW"
 
 
+# 1. Identify states
+# What state do we need to know whether we have decoded a string?
+#   1. We can keep track of the number of digits we have already matched in index i. When i == length of digits, we have
+#   finished.
+# What state do we need to decide which child nodes of the state-space tree should be visited next?
+# Since there's no constraint on which letters can be used for decoding, we don't need any state here.
+#
+# 2. Draw the space-state tree
+#
+# 3. DFS
+# Using the backtracking template as a basis, we add the state we identified in step 1:
+#   1. i for the number of digits we have already matched.
+#
+# DFS returns the number of ways we can decode digits[i:].
+#
+# Time Complexity: O(2^n)
+#
+# n is the length of the string. Essentially at every digit we either make a new number or add it to the old one. We
+# can make this into linear time through dp but currently we have a exponential time solution.
+
+def decode_ways(digits):
+    # use numbers 1 to 26 to represent all alphabet letters
+    prefixes = [str(i) for i in range(1, 27)]
+
+    def dfs(start_index):
+        if start_index == len(digits):
+            return 1
+
+        ways = 0
+        remaining = digits[start_index:]
+        for prefix in prefixes:
+            if remaining.startswith(prefix):
+                ways += dfs(start_index + len(prefix)) # add number of ways returned from child node
+
+        return ways
+
+    return dfs(0)
