79abf28cfbf5048c7ea67dde2ee2534928fc695f
henrylin2008 <henrylin2008@yahoo.com>
2022-07-31 23:20:17 -0700

    Palindrome Partitioning

--- LeetCode/Blind 75/Backtracking/Pruning/Palindrome Partitioning.py
+++ LeetCode/Blind 75/Backtracking/Pruning/Palindrome Partitioning.py
@@ -42,3 +42,35 @@
 #
 # This is because once we determine a palindrome we work backwards to consider the other palindromes and enumerate them
 # accordingly.
+
+
+from typing import List
+
+
+def partition(s: str) -> List[List[str]]:
+    ans = []
+
+    n = len(s)
+
+    def is_palindrome(word):
+        return word == word[::-1]
+
+    def dfs(start, cur_path):
+        if start == n:
+            ans.append(cur_path[:])
+            return
+
+        for i in range(start + 1, n + 1):
+            prefix = s[start: i]
+            if is_palindrome(prefix):
+                dfs(i, cur_path + [prefix])
+
+    dfs(0, [])
+    return ans
+
+
+if __name__ == '__main__':
+    s = input()
+    res = partition(s)
+    for row in res:
+        print(' '.join(row))
