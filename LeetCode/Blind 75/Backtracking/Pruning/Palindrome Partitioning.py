b09762dc1cf13121b428d99991c26dcdaf228c3a
henrylin2008 <henrylin2008@yahoo.com>
2022-07-31 23:14:50 -0700

    Palindrome Partitioning

---
+++ LeetCode/Blind 75/Backtracking/Pruning/Palindrome Partitioning.py
@@ -0,0 +1,17 @@
+# Palindrome Partitioning
+# https://algo.monster/problems/palindrome_partitioning
+
+# Given a string s, partition s such that every substring of the partition is a palindrome.
+#
+# Return all possible palindrome partitioning of s.
+#
+# Example 1:
+# Input: aab
+#
+# Output:
+#   [
+#   ["aa","b"],
+#   ["a","a","b"]
+#   ]
+
+
