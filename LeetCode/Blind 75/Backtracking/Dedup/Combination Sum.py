a9a755eb405b7bb028a153de780113290026560a
henrylin2008 <henrylin2008@yahoo.com>
2022-08-01 23:42:57 -0700

    Combination Sum

--- LeetCode/Blind 75/Backtracking/Dedup/Combination Sum.py
+++ LeetCode/Blind 75/Backtracking/Dedup/Combination Sum.py
@@ -94,3 +94,30 @@
 # of the tree is at most target divided by the smallest number in candidates.
 #
 # Every number can be used or not used therefore leading to exponential time complexity.
+
+
+from typing import List
+
+
+def combination_sum(candidates: List[int], target: int) -> List[List[int]]:
+    def dfs(nums, start_index, remaining, path):
+        if remaining == 0:
+            res.append(path[:])
+            return
+        for i in range(start_index, len(nums)):
+            num = nums[i]
+            if remaining - num < 0:
+                continue
+            dfs(nums, i, remaining - num, path + [num])
+
+    res = []
+    dfs(candidates, 0, target, [])
+    return res
+
+
+if __name__ == '__main__':
+    candidates = [int(x) for x in input().split()]
+    target = int(input())
+    res = combination_sum(candidates, target)
+    for row in res:
+        print(' '.join(map(str, row)))
