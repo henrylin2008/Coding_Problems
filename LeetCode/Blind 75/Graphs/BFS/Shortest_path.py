bfdc6c0c3a893170d7a9f2172c69e46931b41bd3
henrylin2008 <henrylin2008@yahoo.com>
2022-08-06 23:34:41 -0700

    shortest path

--- LeetCode/Blind 75/Graphs/BFS/Shortest_path.py
+++ LeetCode/Blind 75/Graphs/BFS/Shortest_path.py
@@ -24,6 +24,15 @@
 #        B 3
 
 
+# Explanation
+# BFS is best for finding the distance between two nodes since it traverses level by level. Apply the template from
+# BFS template. Since the graph is already built for us, get_neighbors function retrieves the adjacency list.
+#
+# Time Complexity: O(n+m)
+#
+# Again we adopt the convention that n denote the number of nodes in the graph and m the number of edges. The time
+# spent is equal to the number of nodes and edges in the worst case. Consider for example a linear graph 0->1->2->3
+# and so on where we want to get from end to end, we would traverse every node and edge exactly once.
 
 from collections import deque
 from typing import List
@@ -60,3 +69,6 @@ if __name__ == '__main__':
     b = int(input())
     res = shortest_path(graph, a, b)
     print(res)
+
+# BFS can find the shortest path for unweighted graphs. For weighted graphs, we need algorithms like Dijkstra's
+# algorithm. Dijkstra's algorithm rarely comes up in coding interviews so we won't get into details here.
