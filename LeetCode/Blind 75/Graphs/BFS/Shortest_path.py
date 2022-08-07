b7d17086a4c0df082137224b50117987c28e72b7
henrylin2008 <henrylin2008@yahoo.com>
2022-08-06 23:32:53 -0700

    shortest path

---
+++ LeetCode/Blind 75/Graphs/BFS/Shortest_path.py
@@ -0,0 +1,25 @@
+# Shortest Path Between A and B
+# https://algo.monster/problems/shortest_path_unweight
+
+# Given an (unweighted) graph, return the length of the shortest path between two nodes A and B, in terms of the number
+# of edges.
+#
+# Input:
+# graph: {
+#   0: [1, 2],
+#   1: [0, 2, 3],
+#   2: [0, 1],
+#   3: [1]
+# }
+# A: 0
+# B: 3
+#
+# Output: 2
+
+#                   A
+#                   0
+#               /       \
+#             1    ---    2
+#           /
+#        B 3
+
