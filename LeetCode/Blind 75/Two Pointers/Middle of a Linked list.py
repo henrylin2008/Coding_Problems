4570e50d5abd620a79c65e1d0944002421564769
henrylin2008 <henrylin2008@yahoo.com>
2022-08-13 23:42:57 -0700

    Middle of a Linked List

---
+++ LeetCode/Blind 75/Two Pointers/Middle of a Linked list.py
@@ -0,0 +1,14 @@
+# Middle of a Linked List
+# https://algo.monster/problems/middle_of_linked_list
+
+# Find the middle node of a linked list.
+#
+# Input: 0 1 2 3 4
+#
+# Output: 2
+#
+# If the number of nodes is even, then return the second middle node.
+#
+# Input: 0 1 2 3 4 5
+#
+# Output: 3
