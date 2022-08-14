a519658e7e92a6f972d88804ac5872068d0b6702
henrylin2008 <henrylin2008@yahoo.com>
2022-08-13 23:43:58 -0700

    Middle of a Linked List

--- LeetCode/Blind 75/Two Pointers/Middle of a Linked list.py
+++ LeetCode/Blind 75/Two Pointers/Middle of a Linked list.py
@@ -30,3 +30,29 @@
 # Time Complexity: O(n)
 #
 # Technically O(n/2) but again constants are cut out from the time complexity and so we are left with just O(n).
+
+class Node:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+
+def middle_of_linked_list(head: Node) -> int:
+    slow = fast = head
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+    return slow.val
+
+
+def build_list(nodes, f):
+    val = next(nodes, None)
+    if val is None: return None
+    nxt = build_list(nodes, f)
+    return Node(f(val), nxt)
+
+
+if __name__ == '__main__':
+    head = build_list(iter(input().split()), int)
+    res = middle_of_linked_list(head)
+    print(res)
