4c28f0eacd4d27eef3c9e00861d413afc0c07d02
henrylin2008 <henrylin2008@yahoo.com>
2022-08-13 23:43:40 -0700

    Middle of a Linked List

--- LeetCode/Blind 75/Two Pointers/Middle of a Linked list.py
+++ LeetCode/Blind 75/Two Pointers/Middle of a Linked list.py
@@ -12,3 +12,21 @@
 # Input: 0 1 2 3 4 5
 #
 # Output: 3
+
+# Intuition
+
+# If it was an array, then we can get its length and middle element trivially. For a linked list, we have to traverse
+# it to find its length l. We can find l by traversing the list once and then find the middle element by traversing
+# it again and stop on the l/2th element.
+#
+# Is there any way to traverse only once? We can use two pointers
+#
+#   -a fast pointer that moves 2 nodes at a time and
+#   -a slow pointer that moves 1 node at a time
+#
+# Since the speed of the fast pointer is 2x of the slow pointer, by the time the fast pointer reaches the end the
+# slow pointer should be at the eexact middle of the list.
+#
+# Time Complexity: O(n)
+#
+# Technically O(n/2) but again constants are cut out from the time complexity and so we are left with just O(n).
