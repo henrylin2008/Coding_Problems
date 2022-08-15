9700c8f45878e5a34bc4e00afd140c8c7d6dd96f
henrylin2008 <henrylin2008@yahoo.com>
2022-08-14 23:36:47 -0700

    Longest Substring without Repeating Characters

---
+++ LeetCode/Blind 75/Two Pointers/Longest Substring w/Longest substring without repeating characters.py
@@ -0,0 +1,15 @@
+# Longest Substring without Repeating Characters
+# https://algo.monster/problems/longest_substring_without_repeating_characters
+
+# Find the length of the longest substring of a given string without repeating characters.
+#
+# Input: abccabcabcc
+# Output: 3
+#
+# Explanation: longest substrings are abc, cab, both of length 3
+#
+# Input: aaaabaaa
+# Output: 2
+#
+# Explanation: ab is the longest substring, length 2
+
