b614438f03afb825594b099899dea3d08b5c3d2a
henrylin2008 <henrylin2008@yahoo.com>
2022-08-14 23:37:29 -0700

    Longest Substring without Repeating Characters

--- LeetCode/Blind 75/Two Pointers/Longest Substring w/Longest substring without repeating characters.py
+++ LeetCode/Blind 75/Two Pointers/Longest Substring w/Longest substring without repeating characters.py
@@ -13,3 +13,25 @@
 #
 # Explanation: ab is the longest substring, length 2
 
+# Explanation
+# Intuition
+
+# The brute force way is to check every single substring and count the ones with non-repeating characters. A
+# substring is defined by a start index and an end index.
+
+
+def longest_substring_without_repeating_characters(s: str) -> int:
+    n = len(s)
+    longest = 0
+    for start in range(n):
+        for end in range(n):
+            sub = s[start: end + 1]
+            if len(set(sub)) == len(sub):
+                longest = max(longest, end + 1 - start)
+    return longest
+
+
+if __name__ == '__main__':
+    s = input()
+    res = longest_substring_without_repeating_characters(s)
+    print(res)
