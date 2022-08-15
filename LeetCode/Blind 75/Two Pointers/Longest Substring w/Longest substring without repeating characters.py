395ba1e3a3f09301db97bcf9cd9d23f4dfd47889
henrylin2008 <henrylin2008@yahoo.com>
2022-08-14 23:38:16 -0700

    Longest Substring without Repeating Characters

--- LeetCode/Blind 75/Two Pointers/Longest Substring w/Longest substring without repeating characters.py
+++ LeetCode/Blind 75/Two Pointers/Longest Substring w/Longest substring without repeating characters.py
@@ -19,7 +19,6 @@
 # The brute force way is to check every single substring and count the ones with non-repeating characters. A
 # substring is defined by a start index and an end index.
 
-
 def longest_substring_without_repeating_characters(s: str) -> int:
     n = len(s)
     longest = 0
@@ -28,6 +27,40 @@ def longest_substring_without_repeating_characters(s: str) -> int:
             sub = s[start: end + 1]
             if len(set(sub)) == len(sub):
                 longest = max(longest, end + 1 - start)
+    return longest
+
+
+if __name__ == '__main__':
+    s = input()
+    res = longest_substring_without_repeating_characters(s)
+    print(res)
+
+
+# To improve on brute force, we have to skip unnecessary operations. For a substring starting with start that already
+# contains one duplicate character, we want to stop checking more substrings with the start index. When this happens
+# we want to increment start and look at the next set of substrings.
+#
+# This makes it a classic sliding window problem. A sliding window is defined by two pointers moving in the same
+# direction. We move the window (incrementing pointers) whilst maintaining a certain invariant. For this particular
+# problem, the invariant is the characters inside the window being unique. We use a set to record what's in the
+# window. And when we encounter a character that's already in the window, we want to move the left pointer until it
+# goes past the last occurrence of that character.
+#
+# Time Complexity: O(n)
+
+def longest_substring_without_repeating_characters(s: str) -> int:
+    n = len(s)
+    longest = 0
+    l = r = 0
+    window = set()
+    while r < n:
+        if s[r] not in window:
+            window.add(s[r])
+            r += 1
+        else:
+            window.remove(s[l])
+            l += 1
+        longest = max(longest, r - l)
     return longest
 
 
