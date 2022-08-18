ea64d52aed06d512ea105a104cbe8e94ea39e3ad
henrylin2008 <henrylin2008@yahoo.com>
2022-08-17 23:32:51 -0700

    Minimum Window Substring

--- LeetCode/Blind 75/Two Pointers/Minimum Window Substring.py
+++ LeetCode/Blind 75/Two Pointers/Minimum Window Substring.py
@@ -21,3 +21,18 @@
 #   * 1 <= len(check), len(original) <= 10^5
 #   * original and check both contains only upper case and lower case characters in English. The characters are case
 #   sensitive.
+
+# Solution
+# The solution is similar to Find All Anagrams in a String, except instead of matching exactly, we are to find a
+# window that contains all characters in check.
+#
+# In this case, the comparison for checking valid window is changed to compare that for every character in check,
+# see if the window contains more of that character.
+#
+# In addition, the moving conditions of the window changes as well. Instead of two pointers moving at once,
+# maintaining the size of the window, each pointer moves independently. When the window does not contain check,
+# we move the end pointer until it does (or it reaches the end), then we move the start pointer until the window no
+# longer contains check. In this case, just before moving the window was the local minimal substring. Then it's a
+# simple matter of comparing local minimal substrings and find the minimum one.
+#
+# Time Complexity: O(n)
