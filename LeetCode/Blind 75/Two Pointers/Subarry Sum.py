22867fcd392a19ad1d5fb2c6f2beb94f7a45e07e
henrylin2008 <henrylin2008@yahoo.com>
2022-08-18 23:39:51 -0700

    Subarray Sum

---
+++ LeetCode/Blind 75/Two Pointers/Subarry Sum.py
@@ -0,0 +1,10 @@
+# Subarray Sum
+# https://algo.monster/problems/subarray_sum
+
+# Given an array of integers and an integer target, find a subarray that sums to target and return the start and end indices of the subarray.
+#
+# Input: arr: 1 -20 -3 30 5 4 target: 7
+#
+# Output: 1 4
+#
+# Explanation: -20 - 3 + 30 = 7. The indices for subarray [-20,-3,30] is 1 and 4 (right exclusive).
