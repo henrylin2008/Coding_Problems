217bdce865a5228bb2992a3c3085ad5346846948
henrylin2008 <henrylin2008@yahoo.com>
2022-08-18 23:56:38 -0700

    Subarray Sum

--- LeetCode/Blind 75/Two Pointers/Subarry Sum.py
+++ LeetCode/Blind 75/Two Pointers/Subarry Sum.py
@@ -27,3 +27,30 @@
 # the dictionary, we have found our subarray.
 #
 # Time Complexity: O(n)
+
+from typing import List
+
+
+def subarray_sum(arr: List[int], target: int) -> List[int]:
+    # prefix_sum 0 happens when we have an empty array
+    prefix_sums = {0: 0}
+    cur_sum = 0
+    for i in range(len(arr)):
+        cur_sum += arr[i]
+        complement = cur_sum - target
+        if complement in prefix_sums:
+            return [prefix_sums[complement], i + 1]
+        prefix_sums[cur_sum] = i + 1
+
+
+if __name__ == '__main__':
+    arr = [int(x) for x in input().split()]
+    target = int(input())
+    res = subarray_sum(arr, target)
+    print(' '.join(map(str, res)))
+
+# Note that in the implementation, typically we use prefix_sum[i] to denote the sum of elements in 0...i - 1 (rightmost
+# element i is not included in the sum). One good thing about this is prefix_sum[0] then means sum of array up to but
+# not including the first element, i.e. empty array. The definition of empty array sum is useful when there exists a
+# subarray starting from 0 that sums up to the target. Without the definition of empty array sum we would miss it
+# because its complement 0 does not exist in the dictionary
