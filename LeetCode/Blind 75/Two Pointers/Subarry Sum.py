efa9cf6523b10134f45eb1b2ee5395de23f0fddf
henrylin2008 <henrylin2008@yahoo.com>
2022-08-19 00:06:05 -0700

    Subarray Sum

--- LeetCode/Blind 75/Two Pointers/Subarry Sum.py
+++ LeetCode/Blind 75/Two Pointers/Subarry Sum.py
@@ -54,3 +54,26 @@ if __name__ == '__main__':
 # not including the first element, i.e. empty array. The definition of empty array sum is useful when there exists a
 # subarray starting from 0 that sums up to the target. Without the definition of empty array sum we would miss it
 # because its complement 0 does not exist in the dictionary
+
+from typing import Counter, List
+
+
+def subarray_sum_total(arr: List[int], target: int) -> int:
+    prefix_sums = Counter
+    prefix_sums[0] = 1  # since empty array's sum is 0
+    cur_sum = 0
+    count = 0
+    for i in range(len(arr)):
+        cur_sum += arr[i]
+        complement = cur_sum - target
+        if complement in prefix_sums:
+            count += prefix_sums[complement]
+        prefix_sums[cur_sum] += 1
+    return count
+
+
+if __name__ == '__main__':
+    arr = [int(x) for x in input().split()]
+    target = int(input())
+    res = subarray_sum_total(arr, target)
+    print(res)
