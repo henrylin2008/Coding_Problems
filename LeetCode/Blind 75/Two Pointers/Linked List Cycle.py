8ecbbfcb7ac91b316879ea93bb294339bb75dda6
henrylin2008 <henrylin2008@yahoo.com>
2022-08-19 23:46:39 -0700

    Linked List Cycle

---
+++ LeetCode/Blind 75/Two Pointers/Linked List Cycle.py
@@ -0,0 +1,32 @@
+# Linked List Cycle
+# https://algo.monster/problems/linked_list_cycle
+
+# Given a linked list with potentially a loop, determine whether the linked list from the first node contains a cycle
+# in it. For bonus points, do this with constant space.
+#
+# Parameters
+# nodes: The first node of a linked list with potentially a loop.
+# Result
+# Whether there is a loop contained in the linked list.
+# Examples
+# Example 1
+# Input:
+#
+#   0 --> 1 --> 2 --> 3 --> 4
+#         ^                 |
+#         |                 |
+#         ------------------
+#
+# Output:
+# true
+#
+# Example 2
+# Input:
+#
+#   0 --> 1 --> 2 --> 3 --> 4
+#
+# Output:
+# false
+#
+# Constraints
+# 1 <= len(nodes) <= 10^5
