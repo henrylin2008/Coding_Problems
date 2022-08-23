2ebd05ffce1cdc6d319104827726d3a196fbf0db
henrylin2008 <henrylin2008@yahoo.com>
2022-08-22 23:19:18 -0700

    K Closest Points

---
+++ LeetCode/Blind 75/Priority Queue-Heap/K Closest Points.py
@@ -0,0 +1,8 @@
+# K Closest Points
+# https://algo.monster/problems/k_closest_points
+
+# Given a list of points on a 2D plane. Find k closest points to origin (0, 0).
+#
+# Input: [(1, 1), (2, 2), (3, 3)], 1
+#
+# Output: [(1, 1)]
