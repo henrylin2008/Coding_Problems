a916e892c1f9c69c00de46ad4cd281ef86610249
henrylin2008 <henrylin2008@yahoo.com>
2022-08-25 23:10:15 -0700

    Kth Smallest Element in a Sorted Matrix

--- LeetCode/Blind 75/Priority Queue-Heap/Kth Smallest Element in a Sorted Matrix.py
+++ LeetCode/Blind 75/Priority Queue-Heap/Kth Smallest Element in a Sorted Matrix.py
@@ -43,3 +43,36 @@
 #
 # For this specific implementation below, the time complexity is O(N + K log(N)) since it takes O(N) to process the
 # first row and each of the k iterations take O(log(N)) to process due to the use of the min heap.
+
+from heapq import heappop, heappush
+from typing import List
+
+
+def kth_smallest(matrix: List[List[int]], k: int) -> int:
+    n = len(matrix)
+    # Keeps track of items in the heap, and their row and column numbers
+    heap = [(matrix[0][0], 0, 0)]
+    # Keeps track of the top of each row that is not processed
+    column_top = [0] * n
+    # Keeps track of the first number each row not processed
+    row_first = [0] * n
+    # Repeat the process k - 1 times.
+    while k > 1:
+        k -= 1
+        min_val, row, column = heappop(heap)
+        row_first[row] = column + 1
+        # Add the item on the right to the heap if everything above it is processed
+        if column + 1 < n and column_top[column + 1] == row:
+            heappush(heap, (matrix[row][column + 1], row, column + 1))
+        column_top[column] = row + 1
+        # Add the item below it to the heap if everything before it is processed
+        if row + 1 < n and row_first[row + 1] == column:
+            heappush(heap, (matrix[row + 1][column], row + 1, column))
+    return heap[0][0]
+
+
+if __name__ == '__main__':
+    matrix = [[int(x) for x in input().split()] for _ in range(int(input()))]
+    k = int(input())
+    res = kth_smallest(matrix, k)
+    print(res)
