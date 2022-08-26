bf3e976c4d0a4d5770ce27d75404c534179498d4
henrylin2008 <henrylin2008@yahoo.com>
2022-08-25 23:08:38 -0700

    Kth Smallest Element in a Sorted Matrix

---
+++ LeetCode/Blind 75/Priority Queue-Heap/Kth Smallest Element in a Sorted Matrix.py
@@ -0,0 +1,19 @@
+# Kth Smallest Element in a Sorted Matrix
+# https://algo.monster/problems/kth_smallest_element_in_a_sorted_matrix
+
+# Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element
+# in the matrix.
+#
+# Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+#
+# Example:
+# Input:
+# matrix = [
+#   [ 1,  5,  9],
+#   [10, 11, 13],
+#   [12, 13, 15]
+# ],
+# k = 8,
+# Output: 13
+# Note:
+# You may assume k is always valid, 1 ≤ k ≤ n^2. You may also assume that 1 <= n <= 1000.
