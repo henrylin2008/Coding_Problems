2327305dc2f1359383cbec4b6c77519ad3d56001
henrylin2008 <henrylin2008@yahoo.com>
2022-08-26 22:51:19 -0700

    Reorganize String

--- LeetCode/Blind 75/Priority Queue-Heap/Moving Best/Reorganize String.py
+++ LeetCode/Blind 75/Priority Queue-Heap/Moving Best/Reorganize String.py
@@ -25,3 +25,51 @@
 #
 # For the implementation, we use a heap to guarantee the character that is repeated the most is processed first,
 # and because the character repeated the most changes as we process more characters.
+
+from heapq import heapify, heappop
+from typing import Counter
+
+
+def reorganize_string(s: str) -> str:
+    n = len(s)
+    # Count up the characters that appear in the string
+    str_count = Counter[s]
+    # store negative value since we want a max heap
+    pq = [(-value, key) for key, value in str_count.items()]
+    heapify(pq)
+    # Return empty string if there are too many of one character
+    if - pq[0][0] > (n + 1) // 2:
+        return ""
+    # Stores the resulting char array to be converted to string
+    res = [None] * n
+    # Pointer to the next item to be inserted
+    # Increment by 2 until it reaches the end to fill out even positions,
+    # then it is reset to 1 to fill out odd positions
+    pointer = 0
+    # Insert characters into the char array by their multiplicity
+    while len(pq) > 0:
+        (count, char) = heappop(pq)
+        count = - count
+        for i in range(count):
+            res[pointer] = char
+            pointer += 2
+            if pointer >= n:
+                pointer = 1
+    return "".join(res)
+
+
+if __name__ == '__main__':
+    s = input()
+    res = reorganize_string(s)
+    if not res:
+        print("Impossible")
+        exit()
+    input_counter, output_counter = Counter[s], Counter[res]
+    if input_counter != output_counter:
+        print("Not rearrangement")
+        exit()
+    for i in range(len(res) - 1):
+        if res[i] == res[i + 1]:
+            print(f"Same character at index {i} and {i + 1}")
+            exit()
+    print("Valid")
