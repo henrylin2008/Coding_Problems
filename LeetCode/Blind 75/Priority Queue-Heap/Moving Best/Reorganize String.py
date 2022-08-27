c18782874818a54268bdb1b3ac14ab3ffbda25ff
henrylin2008 <henrylin2008@yahoo.com>
2022-08-26 22:50:13 -0700

    Reorganize String

---
+++ LeetCode/Blind 75/Priority Queue-Heap/Moving Best/Reorganize String.py
@@ -0,0 +1,16 @@
+# Reorganize String
+# https://algo.monster/problems/reorganize_string
+
+# Given a string s, check if the letters can be rearranged so that two characters that are adjacent to each other are
+# not the same.
+#
+# If possible, output any possible result. If not possible, return the empty string.
+#
+# Example 1:
+# Input:s = "aab"
+# Output: aba
+# Example 2:
+# Input:s = "aaab"
+# Output: ``
+# Note:
+# s will consist of lowercase letters and have length in range [1, 500].
