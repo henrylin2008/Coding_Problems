1ebe05170d9ce24fa247a9f556faa7023e8da5d3
henrylin2008 <henrylin2008@yahoo.com>
2022-08-26 22:50:46 -0700

    Reorganize String

--- LeetCode/Blind 75/Priority Queue-Heap/Moving Best/Reorganize String.py
+++ LeetCode/Blind 75/Priority Queue-Heap/Moving Best/Reorganize String.py
@@ -14,3 +14,14 @@
 # Output: ``
 # Note:
 # s will consist of lowercase letters and have length in range [1, 500].
+
+# Solution
+# We divide the indices of the string into two: odds and evens. In that case, elements with odd indices can only be
+# adjacent to elements with even indices, and vice versa. Note that because the index starts at zero, there will
+# always be more or equal evens than odds. If there are more of one character than the number of evens,
+# it is impossible to rearrange the string so no adjacent characters are the same. Otherwise, we can start from the
+# character that occurs the most and fill out spots with even indices. Once we run out of spots to fill, we fill out
+# the spots with odd indices. In this case, the result will guarantee to have no same adjacent characters.
+#
+# For the implementation, we use a heap to guarantee the character that is repeated the most is processed first,
+# and because the character repeated the most changes as we process more characters.
