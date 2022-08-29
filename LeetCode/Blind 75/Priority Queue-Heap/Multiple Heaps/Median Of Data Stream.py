f2269da7d0d80d9c20e99305987138a2343ce05b
henrylin2008 <henrylin2008@yahoo.com>
2022-08-28 23:29:03 -0700

    Median of Data Stream

---
+++ LeetCode/Blind 75/Priority Queue-Heap/Multiple Heaps/Median Of Data Stream.py
@@ -0,0 +1,14 @@
+# Median of Data Stream
+# https://algo.monster/problems/median_of_data_stream
+
+# Given a stream of numbers, find the median number at any given time (accurate to 1 decimal place). Do this in O(1)
+# time complexity.
+#
+# Example:
+#
+# add_number(1)
+# add_number(2)
+# add_number(3)
+# get_median() == 2.0
+# add_number(4)
+# get_median() == 2.5
