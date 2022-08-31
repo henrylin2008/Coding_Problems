368b3857cb418e72e84835178c92a628153171d4
henrylin2008 <henrylin2008@yahoo.com>
2022-08-30 23:13:43 -0700

    Knapsack, Weight-Only

--- LeetCode/Blind 75/Dynamic Programming/Knapsack/Knapsack Weight-only.py
+++ LeetCode/Blind 75/Dynamic Programming/Knapsack/Knapsack Weight-only.py
@@ -30,3 +30,33 @@
 # Constraints
 # 1 <= len(weights) <= 100
 # 1 <= weights[i] <= 100
+
+# Solution
+# Brute Force
+# A brute force method enumerates all possibilities. We start with a total sum of 0 and process every item by either
+# choosing to include it into our sum or not into our sum. Once no more items are left to process, we can include the
+# final sum in a list of sums. Additionally, we will store these sums in a set since there can be repeating sums.
+#
+# By going through every possibility, we're generating all possible subsets, so we guarantee that we are also
+# generating all possible sums.
+#
+# Since there are n items, two possibilities each, and it takes O(1) to compute each possibility, the final runtime
+# is O(2^n).
+#
+# The following is the space-state tree for this idea using input [1, 3, 3, 5]. Each level i of the tree represents a
+# binary decision to include or not include the ith number. For example, we have two branches in level i = 1,
+# the left branch means not picking the ith item 3, and the right branch means picking it.
+
+def generate_sums(weights, sums, sum, n):
+    if n == 0:
+        sums.add(sum)
+        return
+    generate_sums(weights, sums, sum, n - 1)
+    generate_sums(weights, sums, sum + weights[n - 1], n - 1)
+
+
+def knapsack_weight_only(weights):
+    sums = set()
+    n = len(weights)
+    generate_sums(weights, sums, 0, n)
+    return list(sums)
