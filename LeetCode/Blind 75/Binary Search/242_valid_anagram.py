69826dc3d27e9145ff17a27f19568c926867d3a0
henrylin2008 <henrylin2008@yahoo.com>
2022-08-31 11:59:41 -0700

    Valid Anagram

--- LeetCode/Blind 75/Binary Search/242_valid_anagram.py
+++ LeetCode/Blind 75/Binary Search/242_valid_anagram.py
@@ -24,3 +24,19 @@
 #
 # Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
 
+
+# Time: O(n); O(s+t)
+# Space: O(n); store every char and its count
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count_s, count_t = {}, {}
+
+        for i in range(len(s)):
+            count_s[s[i]] = 1 + count_s.get(s[i], 0)
+            count_t[t[i]] = 1 + count_t.get(t[i], 0)
+        return count_s == count_t
+
+
