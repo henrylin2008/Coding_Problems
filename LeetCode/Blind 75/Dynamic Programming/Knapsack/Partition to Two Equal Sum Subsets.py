1428eefad1732ec583d0dac3aed256553a8b8c1d
henrylin2008 <henrylin2008@yahoo.com>
2022-09-01 23:44:48 -0700

    Partition to Two Equal Sum Subsets

---
+++ LeetCode/Blind 75/Dynamic Programming/Knapsack/Partition to Two Equal Sum Subsets.py
@@ -0,0 +1,24 @@
+# Partition to Two Equal Sum Subsets
+# https://algo.monster/problems/partition_equal_subset_sum
+#
+# Input
+#   nums: the array
+# Output
+# if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal
+#
+# Examples
+# Example 1:
+# Input:
+#   nums = [3, 4, 7]
+# Output: true
+#
+# Explanation:
+# The array can be partitioned as [3,4] and [7].
+#
+# Example 2:
+# Input:
+# nums = [1, 5, 11, 5]
+# Output: true
+#
+# Explanation:
+# The array can be partitioned as [1, 5, 5] and [11].
