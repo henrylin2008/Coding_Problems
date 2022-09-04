d60a3fa7f7e7bf0cb6b86da9c8cd78044218eb7c
henrylin2008 <henrylin2008@yahoo.com>
2022-09-03 23:52:41 -0700

    Coin Change

--- LeetCode/Blind 75/Dynamic Programming/Coin Change/Coin Change.py
+++ LeetCode/Blind 75/Dynamic Programming/Coin Change/Coin Change.py
@@ -14,3 +14,39 @@
 # Example 2:
 # Input: coins = [3], amount = 1
 # Output: -1
+
+
+# Solution
+# Brute Force
+# This is a classical problem very similar to the unbounded knapsack problem.
+#
+# A brute force method enumerates all possibilities with the use of backtracking. We start with a total sum of 0 and
+# try every denomination. Since we can use each denomination more than once, after we use it we try every
+# denomination again until we reach the target.
+
+# The run time is O(n^{amount / T}) where T is the minimum denomination. The space complexity is amount / T since the
+# recursive tree will be at most amount / T deep at any given time.
+
+from math import inf
+
+
+def min_coins(coins, amount, sum):
+    if sum == amount:
+        return 0
+
+    if sum > amount:
+        return inf
+
+    ans = inf
+    for coin in coins:
+        result = min_coins(coins, amount, sum + coin)
+        if result == inf:
+            continue
+        ans = min(ans, result + 1)
+
+    return ans
+
+
+def coin_change(coins: List[int], amount: int) -> int:
+    result = min_coins(coins, amount, 0)
+    return result if result != inf else -1
