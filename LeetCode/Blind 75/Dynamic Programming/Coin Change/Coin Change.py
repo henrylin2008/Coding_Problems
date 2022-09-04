e472a8e42f53d985e82a5f9c6962deb80cb0f506
henrylin2008 <henrylin2008@yahoo.com>
2022-09-03 23:58:56 -0700

    Coin Change

--- LeetCode/Blind 75/Dynamic Programming/Coin Change/Coin Change.py
+++ LeetCode/Blind 75/Dynamic Programming/Coin Change/Coin Change.py
@@ -87,3 +87,59 @@ def coin_change(coins: List[int], amount: int) -> int:
     memo = [-1] * (amount + 1)
     result = min_coins(coins, amount, 0, memo)
     return result if result != inf else -1
+
+
+# The runtime is O(n * amount) since there are O(amount) states, each state takes O(n) to compute. The space
+# complexity is O(amount) due to the 1D dp array.
+
+# Bottom-up DP
+# This can be done iteratively as well.
+#
+# First, similar to the top-down solution, let dp[i] be the minimum number of denominations required to get a sum of i.
+#
+# The idea is that we begin with dp[0] = 0 since it takes 0 coins to get a sum of 0. Next, we build up:
+#   dp[1] = dp[0] + 1 = 1: We can build a sum of 1 using a coin of denomination 1 on top of the minimum number of coins
+#   to get a sum of 0
+#   dp[2] = dp[0] + 1 = 1: Similar to above, but we use a coin of denomination 2 on top of the answer for a sum of 0
+#   dp[3] = dp[2] + 1 = 2: coin of denomination 1 on top of the answer for a sum of 2 or a 2 on top of a sum of 1.
+#   Either case, the minimum is 2 coins.
+#   dp[4] = dp[2] + 1 = 2: coin of denomination 2 on top of the answer for a sum of 2
+#   dp[5] = min(dp[0], dp[3], dp[4]) + 1 = 1: either a coin of 5 on top of 0, a coin of 2 on top of 3, or a coin of 1 on
+#   top of 4. The minimum is a 5 on top of 0.
+#   dp[6] = min(dp[1], dp[4], dp[5]) + 1 = 2: either a coin of 5 on top of 1, a coin of 2 on top of 4, or a coin of 1 on
+#   top of 5. The minimum is a 1 on top of 5.
+#   ...
+#   dp[n] = min(dp[n - 1], dp[n - 2], dp[n - 5]) + 1: We use a coin of denomination of either 1, 2, or 5 on top of the
+#   minimum answer between the sums of n - 1, n - 2, or n - 5.
+
+
+# Implementation
+from math import inf
+from typing import List
+
+
+def coin_change(coins: List[int], amount: int) -> int:
+    if amount == 0: return 0
+    dp = [inf] * (amount + 1)
+    dp[0] = 0
+
+    for i in range(1, amount + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                dp[i] = min(dp[i - coin] + 1, dp[i])
+
+    return dp[-1] if dp[-1] < inf else -1
+
+
+if __name__ == '__main__':
+    coins = [int(x) for x in input().split()]
+    amount = int(input())
+    res = coin_change(coins, amount)
+    print(res)
+
+# Notice the small details in the implementation.
+#
+#      -We start with all values being maximum number possible to mean that the amount cannot be made up and to ensure
+#      those values are not taken when we do min.
+#      -If the target value is smaller than current coin value then the coin cannot be used since it would yield a
+#      negative new target.
