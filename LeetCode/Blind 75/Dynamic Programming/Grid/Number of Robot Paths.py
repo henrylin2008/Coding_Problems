772fee0e5619d769d676b66b8c6c25d47dbe577f
henrylin2008 <henrylin2008@yahoo.com>
2022-09-05 23:27:02 -0700

    Number of Robot Paths

---
+++ LeetCode/Blind 75/Dynamic Programming/Grid/Number of Robot Paths.py
@@ -0,0 +1,24 @@
+# Number of Unique Paths to Go from Top Left to Bottom Right
+# https://algo.monster/problems/robot_unique_path
+
+# A robot is located at the top-left corner of a m x n grid.
+#
+# The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right
+# corner of the grid (marked 'Finish' in the diagram below).
+#
+# How many possible unique paths are there?
+#
+# Example 1:
+# Input: m = 2, n = 3
+# Output: 3
+#
+# Explanation:
+# From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+#
+#   1. Right -> Right -> Down
+#   2. Right -> Down -> Right
+#   3. Down -> Right -> Right
+#
+# Example 2:
+# Input: m = 5, n = 3
+# Output: 15
