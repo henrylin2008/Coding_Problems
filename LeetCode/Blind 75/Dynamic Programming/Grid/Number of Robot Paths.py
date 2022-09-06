699ce1b3a154c4e1fe3c5ee2d2401da0f1fb5632
henrylin2008 <henrylin2008@yahoo.com>
2022-09-05 23:28:03 -0700

    Number of Robot Paths

--- LeetCode/Blind 75/Dynamic Programming/Grid/Number of Robot Paths.py
+++ LeetCode/Blind 75/Dynamic Programming/Grid/Number of Robot Paths.py
@@ -44,3 +44,26 @@
 # Note that since for the top row, there's only one way to reach each cell - from the left. And for the leftmost
 # column, there's only one way to reach each cell - from the top. We pre-populate them before moving to the internal
 # cells.
+
+def unique_paths(m: int, n: int) -> int:
+    dp = [[0 for _ in range(n)] for _ in range(m)]
+
+    for c in range(n):
+        dp[0][c] = 1
+
+    for r in range(m):
+        dp[r][0] = 1
+
+    # dp[r][c] = number of unique paths to reach (r, c)
+    for r in range(1, m):
+        for c in range(1, n):
+            dp[r][c] = dp[r - 1][c] + dp[r][c - 1]
+
+    return dp[-1][-1]
+
+
+if __name__ == '__main__':
+    m = int(input())
+    n = int(input())
+    res = unique_paths(m, n)
+    print(res)
