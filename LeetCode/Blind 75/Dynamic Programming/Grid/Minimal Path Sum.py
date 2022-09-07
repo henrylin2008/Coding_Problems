60626b095701101a45e16981474b4298ab9465e1
henrylin2008 <henrylin2008@yahoo.com>
2022-09-06 23:33:42 -0700

    Minimal Path Sum

--- LeetCode/Blind 75/Dynamic Programming/Grid/Minimal Path Sum.py
+++ LeetCode/Blind 75/Dynamic Programming/Grid/Minimal Path Sum.py
@@ -37,3 +37,34 @@
 # We fill the dp matrix row by row from left to right.
 #
 # Time Complexity: O(m*n)
+
+from typing import List
+
+
+def min_path_sum(grid: List[List[int]]) -> int:
+    m = len(grid)
+    n = len(grid[0])
+    dp = [[0 for _ in range(n)] for _ in range(m)]
+
+    cur_sum = 0
+    for c in range(n):
+        cur_sum = cur_sum + grid[0][c]
+        dp[0][c] = cur_sum
+
+    cur_sum = 0
+    for r in range(m):
+        cur_sum = cur_sum + grid[r][0]
+        dp[r][0] = cur_sum
+
+    # dp[r][c] = min path sum to reach (r, c)
+    for r in range(1, m):
+        for c in range(1, n):
+            dp[r][c] = min(dp[r - 1][c], dp[r][c - 1]) + grid[r][c]
+
+    return dp[-1][-1]
+
+
+if __name__ == '__main__':
+    grid = [[int(x) for x in input().split()] for _ in range(int(input()))]
+    res = min_path_sum(grid)
+    print(res)
