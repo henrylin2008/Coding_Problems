6bc5a6973b8264241250b08c914ca2a42586d837
henrylin2008 <henrylin2008@yahoo.com>
2022-09-06 23:33:25 -0700

    Minimal Path Sum

--- LeetCode/Blind 75/Dynamic Programming/Grid/Minimal Path Sum.py
+++ LeetCode/Blind 75/Dynamic Programming/Grid/Minimal Path Sum.py
@@ -17,3 +17,23 @@
 # Explanation:
 # Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
 
+
+# Solution
+# This problem is similar to Robot Unique Paths. We can only move right or down from a cell. The difference is the
+# cells now have weights and we want to choose the path with the minimal weight. The keyword "minimal" and the grid
+# is a good sign to use dynamic programming.
+#
+# The equivalent of "only able to move right or down" is "can only reach a cell from top or left". Since top and left
+# are the only two ways to reach a cell, the minimal path sum to reach a cell is the minimum sum of top and left +
+# value of the current cell.
+#
+# Let dp[r][c] represent the minimal path sum to reach cell (r, c).
+#
+# dp[r][c] = min(dp[r - 1][c], dp[r][c - 1]) + grid[r][c]
+#
+# where dp[r - 1][c] is the minimal path sum to reach the cell at the top of the current cell and dp[r][c - 1] is the
+# minimal path sum to reach the cell to the left of the current cell
+#
+# We fill the dp matrix row by row from left to right.
+#
+# Time Complexity: O(m*n)
