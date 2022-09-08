e393816166e99a3a06f625f670d83cfe34bbd118
henrylin2008 <henrylin2008@yahoo.com>
2022-09-07 23:17:58 -0700

    Longest Increasing Subsequence

---
+++ LeetCode/Blind 75/Dynamic Programming/Dynamic Number of Subproblems/Longest Increasing Subsequence.py
@@ -0,0 +1,24 @@
+# Longest Increasing Subsequence
+# Link: https://algo.monster/problems/longest_increasing_subsequence
+
+# Input
+#   nums: the integer sequence
+# Output
+#   the length of longest subsequence
+#
+# Examples
+# Example 1:
+# Input:
+#   nums = [50, 3, 10, 7, 40, 80]
+# Output: 4
+#
+# Explanation:
+# The longest subsequence is [3, 7, 40, 80] which has length 4.
+#
+# Example 2:
+# Input:
+# nums = [1, 2, 4, 3]
+# Output: 3
+#
+# Explanation:
+# Both [1, 2, 4] and [1, 2, 3] are longest subsequences which have length 3.
