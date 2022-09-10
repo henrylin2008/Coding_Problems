461049e5119b218557693045e27371e97d390179
henrylin2008 <henrylin2008@yahoo.com>
2022-09-09 23:12:04 -0700

    Perfect Squares

---
+++ LeetCode/Blind 75/Dynamic Programming/Dynamic Number of Subproblems/Perfect Squares.py
@@ -0,0 +1,18 @@
+# Perfect Squares
+# https://algo.monster/problems/perfect_squares
+
+# Given a number that is less than 10^5 determine the smallest amount of perfect squares needed to sum to a
+# particular number? The same number can be used multiple times.
+#
+# Examples
+# Example 1:
+# Input: 12
+# Output: 3
+# Explanation:
+# 12 = 4 + 4 + 4
+#
+# Example 2:
+# Input: 13
+# Output: 2
+# Explanation:
+# 13 = 4 + 9
