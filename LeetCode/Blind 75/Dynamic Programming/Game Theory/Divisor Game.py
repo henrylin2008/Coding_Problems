d1503f3e5de8e345fb2581df8b30fae2637dde54
henrylin2008 <henrylin2008@yahoo.com>
2022-09-10 22:57:12 -0700

    Divisor Game

--- LeetCode/Blind 75/Dynamic Programming/Game Theory/Divisor Game.py
+++ LeetCode/Blind 75/Dynamic Programming/Game Theory/Divisor Game.py
@@ -23,3 +23,25 @@
 # Explanation:
 # James chooses 1, and Oliver chooses 1, and James has no more moves.
 
+# Solution
+# Winning and losing states
+# A winning state is a state where the player will win the game if they play optimally, and a losing state is a state
+# where the player will lose the game if the opponent plays optimally. We can classify all states of a game so that
+# each state is either a winning state or a losing state.
+#
+# State 1 is clearly a losing state, because the player cannot make any moves. States 2 is winning state, because we
+# can remove one stick and force the opponent into the losing state of 1 and win the game. State 3, in turn,
+# is a losing state, because any move leads to a state that is a winning state for the opponent.
+#
+# More generally, if there is a move that leads from the current state to a losing state, the current state is a
+# winning state, and otherwise the current state is a losing state. Using this observation, we can classify all
+# states of a game starting with losing states where there are no possible moves.
+#
+# State 1 is a losing state. For each states from 2..N, we try all the possible x we can choose and see if it results
+# in a losing state for the opponent. The state transitions for the first 8 numbers are illustrated here:
+
+# Dynamic programming
+# Now we fill the table where each entry dp[i] represents whether the current state is a winning state.
+#
+# Time Complexity: O(n^2)
+
