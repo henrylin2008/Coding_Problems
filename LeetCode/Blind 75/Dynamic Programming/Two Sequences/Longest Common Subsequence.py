aeb5b67c4776e60287feb6a5ae2d08b8536ed9d0
henrylin2008 <henrylin2008@yahoo.com>
2022-09-13 23:50:17 -0700

    Longest Common Subsequence

--- LeetCode/Blind 75/Dynamic Programming/Two Sequences/Longest Common Subsequence.py
+++ LeetCode/Blind 75/Dynamic Programming/Two Sequences/Longest Common Subsequence.py
@@ -33,3 +33,34 @@
 # Output: 0
 # Explanation:
 # There is no such common subsequence, so the result is 0.
+
+# Solution
+# Brute Force
+# The brute force solution is to generate every possible subsequence, which is essentially generating subsets of the
+# string, in O(2^n) time, where n is the length of the string, using a combinatorial search. Once we have every
+# subsequence of both strings of length n and length m, we compare every pair of subsequences in O(n * m) time,
+# keeping track of which matching pair has the longest length. The final time complexity would be O(2^n + 2^m + n *
+# m) which simplifies to O(2^n + 2^m).
+
+def generate_subsequences(s, n, ans, subsequences):
+    if n == 0:
+        subsequences.append(ans)
+        return
+    generate_subsequences(s, n - 1, s[n - 1] + ans, subsequences)
+    generate_subsequences(s, n - 1, ans, subsequences)
+
+
+def longest_common_subsequence(word1, word2):
+    word1_subsequences = []
+    word2_subsequences = []
+    generate_subsequences(word1, len(word1), "", word1_subsequences)
+    generate_subsequences(word2, len(word2), "", word2_subsequences)
+
+    longest_match_length = 0
+    for subsequence1 in word1_subsequences:
+        for subsequence2 in word2_subsequences:
+            if subsequence1 == subsequence2:
+                length = len(subsequence1)
+                longest_match_length = max(longest_match_length, length)
+
+    return longest_match_length
