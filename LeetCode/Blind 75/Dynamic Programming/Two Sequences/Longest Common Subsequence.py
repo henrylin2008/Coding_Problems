441cca651572f382d57c6255858eef0b503af3af
henrylin2008 <henrylin2008@yahoo.com>
2022-09-13 23:49:41 -0700

    Longest Common Subsequence

---
+++ LeetCode/Blind 75/Dynamic Programming/Two Sequences/Longest Common Subsequence.py
@@ -0,0 +1,35 @@
+# Longest Common Subsequence
+# https://algo.monster/problems/longest_common_subsequence
+
+# Given two strings word1 and word2, return the length of their longest common subsequence.
+#
+# A subsequence of a string is a new string generated from the original string with some characters(can be none)
+# deleted without changing the relative order of the remaining characters. (eg, "ace" is a subsequence of "abcde"
+# while "aec" is not). A common subsequence of two strings is a subsequence that is common to both strings.
+#
+# If there is no common subsequence, return 0.
+#
+# Example 1:
+# Input:
+#   word1 = "abcde"
+#   word2 = "ace"
+#
+# Output: 3
+# Explanation:
+# The longest common subsequence is ace and its length is 3.
+#
+# Example 2:
+# Input:
+#   word1 = "almost"
+#   word2 = "algomonster"
+# Output: 6
+# Explanation:
+# The longest common subsequence is almost and its length is 6.
+#
+# Example 3:
+# Input:
+# word1 = "abc"
+# word2 = "def"
+# Output: 0
+# Explanation:
+# There is no such common subsequence, so the result is 0.
