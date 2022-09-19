ef2fe3d4b77f1ba00b0985c08ce93379f3cbb9c8
henrylin2008 <henrylin2008@yahoo.com>
2022-09-18 23:46:02 -0700

    Minimum Cost to Visit Every Node in a Graph

---
+++ LeetCode/Blind 75/Dynamic Programming/Bitmask/Min Cost to visit every node.py
@@ -0,0 +1,8 @@
+# Minimum Cost to Visit Every Node in a Graph
+# https://algo.monster/problems/min_cost_to_visit_every_node
+
+# Output the minimum cost to traverse every node in a directed weighted graph. The graph will be in the form of a 2D
+# list where element [i,j] in the array denotes the weight of the directed edge from i to j. If the value is 0 then
+# the edge doesn't exist. You do not have to end at the starting node. All edges are guaranteed to be in the range [
+# 0,1000], there will not exceed 15 nodes in the graph. The starting node will always be at node 0. If a solution
+# does not exist return -1.
