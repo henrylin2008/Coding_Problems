ea8b6e5930db19b8cb77ecc7e40171a83b12c0c9
henrylin2008 <henrylin2008@yahoo.com>
2022-09-19 23:49:01 -0700

    Shortest Path Between A and B

--- LeetCode/Blind 75/Graphs/Vanilla BFS/shortest path.py
+++ LeetCode/Blind 75/Graphs/Vanilla BFS/shortest path.py
@@ -15,3 +15,13 @@
 # B: 3
 #
 # Output: 2
+
+# Explanation
+# BFS is best for finding the distance between two nodes since it traverses level by level. Apply the template from
+# BFS template. Since the graph is already built for us, get_neighbors function retrieves the adjacency list.
+#
+# Time Complexity: O(n+m)
+#
+# Again we adopt the convention that n denote the number of nodes in the graph and m the number of edges. The time
+# spent is equal to the number of nodes and edges in the worst case. Consider for example a linear graph 0->1->2->3
+# and so on where we want to get from end to end, we would traverse every node and edge exactly once.
