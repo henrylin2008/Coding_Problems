1853c9797347700f3774d8fc6504229c305ee4bc
henrylin2008 <henrylin2008@yahoo.com>
2022-09-19 23:49:41 -0700

    Shortest Path Between A and B

--- LeetCode/Blind 75/Graphs/Vanilla BFS/shortest path.py
+++ LeetCode/Blind 75/Graphs/Vanilla BFS/shortest path.py
@@ -25,3 +25,46 @@
 # Again we adopt the convention that n denote the number of nodes in the graph and m the number of edges. The time
 # spent is equal to the number of nodes and edges in the worst case. Consider for example a linear graph 0->1->2->3
 # and so on where we want to get from end to end, we would traverse every node and edge exactly once.
+
+# Solution
+from collections import deque
+from typing import List
+
+
+def shortest_path(graph: List[List[int]], a: int, b: int) -> int:
+    def get_neighbors(node: int):
+        return graph[node]
+
+    # BFS template
+    def bfs(root: int, target: int):
+        queue = deque([root])
+        visited = set([root])
+        level = 0
+        while len(queue) > 0:
+            n = len(queue)
+            for n in range(n):
+                node = queue.popleft()
+                if node == target:
+                    return level
+                for neighbor in get_neighbors(node):
+                    if neighbor in visited:
+                        continue
+                    queue.append(neighbor)
+                    visited.add(neighbor)
+            level += 1
+
+    return bfs(a, b)
+
+
+if __name__ == '__main__':
+    graph = [[int(x) for x in input().split()] for _ in range(int(input()))]
+    a = int(input())
+    b = int(input())
+    res = shortest_path(graph, a, b)
+    print(res)
+
+# A quick note on "unweighted". Sometimes, graph's edges can have weight. For example, a graph that represents city
+# connections and edges' weights represent the distance between cities.
+#
+# BFS can find the shortest path for unweighted graphs. For weighted graphs, we need algorithms like Dijkstra's
+# algorithm. Dijkstra's algorithm rarely comes up in coding interviews so we won't get into details here.
