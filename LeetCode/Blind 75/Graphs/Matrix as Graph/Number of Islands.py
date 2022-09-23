1c0858536986cdc1c35a6b0ae57ee1876d5af37c
henrylin2008 <henrylin2008@yahoo.com>
2022-09-22 23:51:11 -0700

    Find the Number of Islands

---
+++ LeetCode/Blind 75/Graphs/Matrix as Graph/Number of Islands.py
@@ -0,0 +1,6 @@
+# Find the Number of Islands
+# https://algo.monster/problems/number_of_islands
+
+# Given a 2-dimensional matrix of values with 0 and 1, count the number of islands of 1. An island consists of all
+# value 1 cells and is surrounded by either an edge or all 0 cells. A cell can only be adjacent to each other
+# horizontally or vertically, not diagonally.
