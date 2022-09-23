cb8dc5ed2637f520e451a5bf246d9becb80e2101
henrylin2008 <henrylin2008@yahoo.com>
2022-09-22 23:54:50 -0700

    Find the Number of Islands

--- LeetCode/Blind 75/Graphs/Matrix as Graph/Number of Islands.py
+++ LeetCode/Blind 75/Graphs/Matrix as Graph/Number of Islands.py
@@ -72,3 +72,52 @@ if __name__ == '__main__':
     grid = [[int(x) for x in input().split()] for _ in range(int(input()))]
     res = count_number_of_islands(grid)
     print(res)
+
+# Using a matrix to store the visited nodes
+
+# You may have noticed the matrix itself is a good place to store the information of whether a cell has been visited.
+# We can update the cell that has already been visited to another number like 2 to denote it's been visited. We can
+# even simply update it to 0 since the island has been recorded and we can consider the visited cells simply as water.
+from typing import List
+from collections import deque
+
+
+def count_number_of_islands(grid: List[List[int]]) -> int:
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+
+    def get_neighbors(coord):
+        res = []
+        row, col = coord
+        delta_row = [-1, 0, 1, 0]
+        delta_col = [0, 1, 0, -1]
+        for i in range(len(delta_row)):
+            r = row + delta_row[i]
+            c = col + delta_col[i]
+            if 0 <= r < num_rows and 0 <= c < num_cols:
+                res.append((r, c))
+        return res
+
+    def bfs(start):
+        queue = deque([start])
+        r, c = start
+        grid[r][c] = 0
+        while len(queue) > 0:
+            node = queue.popleft()
+            for neighbor in get_neighbors(node):
+                r, c = neighbor
+                if grid[r][c] == 0:
+                    continue
+                queue.append(neighbor)
+                grid[r][c] = 0
+    count = 0
+    for r in range(num_rows):
+        for c in range(num_cols):
+            if grid[r][c] == 0:
+                continue
+            bfs((r, c))
+            count += 1
+    return count
+
+# Note that although this is a common solution in coding problems, in the real world you most likely do NOT want to
+# mutate the function arguments.
