3125ea23e5e5882771ad64732fed5be708b9ed2f
henrylin2008 <henrylin2008@yahoo.com>
2022-09-23 23:42:02 -0700

    Knight Minimum Moves

--- LeetCode/Blind 75/Graphs/Matrix as Graph/Knight Minimum Moves.py
+++ LeetCode/Blind 75/Graphs/Matrix as Graph/Knight Minimum Moves.py
@@ -31,3 +31,49 @@ def get_neighbors(coord):
         c = col + delta_col[i]
         res.append((r, c))
     return res
+
+
+# The rest is pretty much the same as previous matrix graph problems. Applying the templates:
+
+# Solution
+from collections import deque
+
+
+def get_knight_shortest_path(x: int, y: int) -> int:
+    def get_neighbors(coord):
+        res = []
+        row, col = coord
+        delta_row = [-2, -2, -1, 1, 2, 2, 1, -1]
+        delta_col = [-1, 1, 2, 2, 1, -1, -2, -2]
+        for i in range(len(delta_row)):
+            r = row + delta_row[i]
+            c = col + delta_col[i]
+            res.append((r, c))
+        return res
+
+    def bfs(start):
+        visited = set()
+        steps = 0
+        queue = deque([start])
+        while len(queue) > 0:
+            n = len(queue)
+            for _ in range(n):
+                node = queue.popleft()
+                if node[0] == y and node[1] == x:
+                    return steps
+                for neighbor in get_neighbors(node):
+                    r, c = neighbor
+                    if neighbor in visited:
+                        continue
+                    queue.append(neighbor)
+                    visited.add(neighbor)
+            steps += 1
+
+    return bfs((0, 0))
+
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    res = get_knight_shortest_path(x, y)
+    print(res)
