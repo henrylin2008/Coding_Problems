104c67340768054222b18b6524ab527a9f8b308f
henrylin2008 <henrylin2008@yahoo.com>
2022-09-23 23:26:44 -0700

    Knight Minimum Moves

---
+++ LeetCode/Blind 75/Graphs/Matrix as Graph/Knight Minimum Moves.py
@@ -0,0 +1,8 @@
+# Knight Minimum Moves
+# https://algo.monster/problems/knight_shortest_path
+
+# On an infinitely large chessboard, a knight is located on [0, 0].
+#
+# A knight can move in eight directions.
+
+# Given a destination coordinate [x, y], determine the minimum number of moves from [0, 0] to [x, y].
