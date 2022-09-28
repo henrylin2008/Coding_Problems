d2a0ce64e51c7f58ad93de26a63233ef997b77b3
henrylin2008 <henrylin2008@yahoo.com>
2022-09-27 21:32:58 -0700

    Word Ladder

--- LeetCode/Blind 75/Graphs/Implicit Graph/Word Ladder.py
+++ LeetCode/Blind 75/Graphs/Implicit Graph/Word Ladder.py
@@ -19,3 +19,32 @@
 #
 # Explanation: We can go from COLD to WARM by going through COLD → CORD → CARD → WARD → WARM
 
+# Intuition
+# We start from a word. In each step we can only go from one word in the list to another, and we want to end up on
+# some word. This suggests that we can represent each word with a node in a graph. In order to do this simply replace
+# every character in the string with a new character and see if we have generated this word before. We can use a set
+# to keep track of which words we have already made as nodes. Each step that transforms a word to another then
+# becomes an edge between the corresponding nodes:
+#
+#
+# Now the problem becomes "find the minimum distance from one node in a graph to the other", which is a problem that
+# BFS handles very easily.
+#
+# Conceptually, we now have an algorithm that is a 2-step process:
+#   1. build a graph that represents the words and transformations
+#   2. use BFS on the graph to find the minimum distance
+#
+# In the actual algorithm, however, we do not need to build up the graph before using BFS: we can build the graph as
+# we go: instead of storing the edges, we calculate the set of neighbors for the current node only when we need to
+# visit them.
+#
+# Our get_neighbors function looks like this:
+#
+# from string import ascii_letters
+def get_neighbors(word):
+    for i in range(len(word)):
+        # replace word[i] with every ascii letter
+        for c in ascii_letters:
+            next_word = word[:i] + c + word[i + 1:]
+            if in_wordlist(next_word):
+                process(next_word)
