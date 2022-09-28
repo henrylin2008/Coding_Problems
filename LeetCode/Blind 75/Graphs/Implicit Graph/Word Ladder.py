4086c38c96d84edbe2624d77365facf1ae2199ef
henrylin2008 <henrylin2008@yahoo.com>
2022-09-27 21:33:32 -0700

    Word Ladder

--- LeetCode/Blind 75/Graphs/Implicit Graph/Word Ladder.py
+++ LeetCode/Blind 75/Graphs/Implicit Graph/Word Ladder.py
@@ -48,3 +48,44 @@ def get_neighbors(word):
             next_word = word[:i] + c + word[i + 1:]
             if in_wordlist(next_word):
                 process(next_word)
+
+
+# Solution
+# Time Complexity: O(n+m)
+#
+# The nodes in the graph are determined by the number of words in the dictionary. Edges are determined by words that
+# are 1 letter apart. Another note is that after edges are computed some performance increases might be possible by
+# hashing the string to a numerical value.
+
+from collections import deque
+from string import ascii_letters
+from typing import List
+
+
+def word_ladder(begin: str, end: str, word_list: List[str]) -> int:
+    words = set(word_list)  # make a set because existence query is O(1) vs O(N) for list
+    queue = deque([begin])
+    distance = 0
+    while len(queue) > 0:
+        n = len(queue)
+        distance += 1
+        for _ in range(n):
+            word = queue.popleft()
+            for i in range(len(word)):
+                for c in ascii_letters:
+                    next_word = word[:i] + c + word[i + 1:]
+                    if next_word not in words:
+                        continue
+                    if next_word == end:
+                        return distance
+                    queue.append(next_word)
+                    words.remove(next_word)  # removing from the set is equivalent as marking the word visited
+    return 0
+
+
+if __name__ == '__main__':
+    begin = input()
+    end = input()
+    word_list = input().split()
+    res = word_ladder(begin, end, word_list)
+    print(res)
