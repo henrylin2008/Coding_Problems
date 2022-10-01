a6b2a867581fcd3970339b1cf12d66ea3368d75c
henrylin2008 <henrylin2008@yahoo.com>
2022-09-30 23:03:36 -0700

    Maximum Sum Subarray of Size K

---
+++ LeetCode/Blind 75/Sliding Window/Outside 75/Max Sum Subarray of Size K.py
@@ -0,0 +1,17 @@
+# Maximum Sum Subarray of Size K (easy)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1627871358579_1Unit
+
+# Problem Statement
+#
+# Given an array of positive numbers and a positive number ‘k,’ find the maximum sum of any contiguous subarray of
+# size ‘k’.
+#
+# Example 1:
+# Input: [2, 1, 5, 1, 3, 2], k=3
+# Output: 9
+# Explanation: Subarray with maximum sum is [5, 1, 3].
+#
+# Example 2:
+# Input: [2, 3, 4, 1, 5], k=2
+# Output: 7
+# Explanation: Subarray with maximum sum is [3, 4].
