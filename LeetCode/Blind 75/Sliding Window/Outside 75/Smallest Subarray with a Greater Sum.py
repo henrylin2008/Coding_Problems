8522df49c9b67ea1496177ab0a336db9552e5e90
henrylin2008 <henrylin2008@yahoo.com>
2022-10-01 23:58:53 -0700

    Smallest Subarray with a Greater Sum

--- LeetCode/Blind 75/Sliding Window/Outside 75/Smallest Subarray with a Greater Sum.py
+++ LeetCode/Blind 75/Sliding Window/Outside 75/Smallest Subarray with a Greater Sum.py
@@ -69,4 +69,10 @@ def main():
 
 main()
 
-
+# Time Complexity
+# The time complexity of the above algorithm will be O(N). The outer for loop runs for all elements,
+# and the inner while loop processes each element only once; therefore, the time complexity of the algorithm will be
+# O(N+N), which is asymptotically equivalent to O(N).
+#
+# Space Complexity
+# The algorithm runs in constant space O(1).
