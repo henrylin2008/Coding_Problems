197ce920ab11da130bbb1321d41a75686d70c5f8
henrylin2008 <henrylin2008@yahoo.com>
2022-10-02 23:45:36 -0700

    Longest Substring with K Distinct Characters

---
+++ LeetCode/Blind 75/Sliding Window/Outside 75/Longest Substring with K Distinct Characters.py
@@ -0,0 +1,21 @@
+# Longest Substring with K Distinct Characters (medium)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628541009794_1Unit
+
+# Problem Statement
+# Given a string, find the length of the longest substring in it with no more than K distinct characters.
+# You can assume that K is less than or equal to the length of the given string.
+#
+# Example 1:
+# Input: String="araaci", K=2
+# Output: 4
+# Explanation: The longest substring with no more than '2' distinct characters is "araa".
+#
+# Example 2:
+# Input: String="araaci", K=1
+# Output: 2
+# Explanation: The longest substring with no more than '1' distinct characters is "aa".
+#
+# Example 3:
+# Input: String="cbbebi", K=3
+# Output: 5
+# Explanation: The longest substrings with no more than '3' distinct characters are "cbbeb" & "bbebi".
