aff11fb8b895ecfa5f1a3f1515503e01bf5a86b6
henrylin2008 <henrylin2008@yahoo.com>
2022-10-03 23:53:23 -0700

    Fruits into Baskets

--- LeetCode/Blind 75/Sliding Window/Outside 75/Fruits into Baskets.py
+++ LeetCode/Blind 75/Sliding Window/Outside 75/Fruits into Baskets.py
@@ -62,3 +62,12 @@ def main():
 
 
 main()
+
+# Time Complexity
+# The above algorithm’s time complexity will be O(N), where ‘N’ is the number of characters in the input array. The
+# outer 'for' loop runs for all characters, and the inner 'while' loop processes each character only once; therefore,
+# the time complexity of the algorithm will be O(N+N), which is asymptotically equivalent to O(N).
+#
+# Space Complexity
+# The algorithm runs in constant space O(1) as there can be a maximum of three types of fruits stored in the
+# frequency map.
