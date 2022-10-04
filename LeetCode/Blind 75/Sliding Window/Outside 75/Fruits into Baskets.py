c2648cd83319979c2141e06ae2d3b582e859981c
henrylin2008 <henrylin2008@yahoo.com>
2022-10-03 23:49:15 -0700

    Fruits into Baskets

---
+++ LeetCode/Blind 75/Sliding Window/Outside 75/Fruits into Baskets.py
@@ -0,0 +1,14 @@
+# Fruits into Baskets (medium)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628541018393_2Unit
+
+# Problem Statement
+# You are visiting a farm to collect fruits. The farm has a single row of fruit trees. You will be given two baskets,
+# and your goal is to pick as many fruits as possible to be placed in the given baskets.
+#
+# You will be given an array of characters where each character represents a fruit tree. The farm has the following
+# restrictions:
+#   1. Each basket can have only one type of fruit. There is no limit to how many fruit a basket can hold.
+#   2. You can start with any tree, but you can’t skip a tree once you have started.
+#   3. You will pick exactly one fruit from every tree until you cannot, i.e., you will stop when you have to pick from
+#      a third fruit type.
+# Write a function to return the maximum number of fruits in both baskets.
