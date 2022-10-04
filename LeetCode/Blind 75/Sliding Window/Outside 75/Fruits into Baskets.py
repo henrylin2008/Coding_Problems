9ffe9d832727708ece697177c73377b80d7fe6d7
henrylin2008 <henrylin2008@yahoo.com>
2022-10-03 23:50:54 -0700

    Fruits into Baskets

--- LeetCode/Blind 75/Sliding Window/Outside 75/Fruits into Baskets.py
+++ LeetCode/Blind 75/Sliding Window/Outside 75/Fruits into Baskets.py
@@ -12,3 +12,14 @@
 #   3. You will pick exactly one fruit from every tree until you cannot, i.e., you will stop when you have to pick from
 #      a third fruit type.
 # Write a function to return the maximum number of fruits in both baskets.
+
+# Example 1:
+# Input: Fruit=['A', 'B', 'C', 'A', 'C']
+# Output: 3
+# Explanation: We can put 2 'C' in one basket and one 'A' in the other from the subarray ['C', 'A', 'C']
+#
+# Example 2:
+# Input: Fruit = ['A', 'B', 'C', 'B', 'B', 'C']
+# Output: 5
+# Explanation: We can put 3 'B' in one basket and two 'C' in the other basket. This can be done if we start with the
+# second letter: ['B', 'C', 'B', 'B', 'C']
