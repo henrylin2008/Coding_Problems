1519f1d55396028ed717e4ccd3b41e0dcf0445aa
henrylin2008 <henrylin2008@yahoo.com>
2022-10-04 23:35:59 -0700

    Longest Substring with Distinct Characters

---
+++ LeetCode/Blind 75/Sliding Window/Outside 75/Longest Substring with Distinct Characters.py
@@ -0,0 +1,20 @@
+# Longest Substring with Distinct Characters (hard)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628541027921_3Unit
+
+# Problem Statement
+# Given a string, find the length of the longest substring, which has all distinct characters.
+#
+# Example 1:
+# Input: String="aabccbb"
+# Output: 3
+# Explanation: The longest substring with distinct characters is "abc".
+#
+# Example 2:
+# Input: String="abbbb"
+# Output: 2
+# Explanation: The longest substring with distinct characters is "ab".
+#
+# Example 3:
+# Input: String="abccde"
+# Output: 3
+# Explanation: Longest substrings with distinct characters are "abc" & "cde".
