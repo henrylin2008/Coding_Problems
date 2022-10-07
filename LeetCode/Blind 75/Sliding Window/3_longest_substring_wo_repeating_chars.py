8724b7ea459dce70f11af5c7b2c28ce962115435
henrylin2008 <henrylin2008@yahoo.com>
2022-10-06 22:05:44 -0700

    Longest Substring Without Repeating Characters

--- LeetCode/Blind 75/Sliding Window/3_longest_substring_wo_repeating_chars.py
+++ LeetCode/Blind 75/Sliding Window/3_longest_substring_wo_repeating_chars.py
@@ -26,16 +26,17 @@
 # s consists of English letters, digits, symbols and spaces.
 
 # Solution: sliding window, if we see same char twice within curr window, shift start position (remove left, move right)
+#           use a variable (max_length) to keep track of the max length (of distinct chars) while sliding through str.
 # Time: O(n); loop through every char in the string
 # Space: O(n); set to store unique characters, potentially every character in the string
 def lengthOfLongestSubstring(self, s: str) -> int:
-    char_set = set()
-    result = 0
-    left = 0
-    for right in range(len(s)):
-        while s[right] in char_set:
-            char_set.remove(s[left])
-            left += 1
-        char_set.add(s[right])
-        result = max(result, right - left + 1)
-    return result
+    char_set = set()    # keep track of unique chars
+    max_length = 0
+    left = 0            # left pointer of the sliding window
+    for right in range(len(s)):         # sliding the right pointer
+        while s[right] in char_set:     # if right char is in the char_set
+            char_set.remove(s[left])    # remove the leftmost char
+            left += 1                   # shift left pointer
+        char_set.add(s[right])          # add right char to char_set hashmap
+        max_length = max(max_length, right - left + 1)
+    return max_length
