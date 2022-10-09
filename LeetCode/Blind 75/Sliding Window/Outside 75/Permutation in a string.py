feb32b31b950d257e938271f62643f2a43da4fb6
henrylin2008 <henrylin2008@yahoo.com>
2022-10-08 23:56:40 -0700

    Permutation in a String

--- LeetCode/Blind 75/Sliding Window/Outside 75/Permutation in a string.py
+++ LeetCode/Blind 75/Sliding Window/Outside 75/Permutation in a string.py
@@ -50,3 +50,46 @@
 #      i.e., total characters in the HashMap), we have gotten our required permutation.
 #   5. If the window size is greater than the length of the pattern, shrink the window to make it equal to the pattern’s
 #      size. At the same time, if the character going out was part of the pattern, put it back in the frequency HashMap.
+
+def find_permutation(str1, pattern):
+    window_start, matched = 0, 0
+    char_frequency = {}
+
+    for chr in pattern:
+        if chr not in char_frequency:
+            char_frequency[chr] = 0
+        char_frequency[chr] += 1
+
+    # our goal is to match all the characters from the 'char_frequency' with the current
+    # window try to extend the range [window_start, window_end]
+    for window_end in range(len(str1)):
+        right_char = str1[window_end]
+        if right_char in char_frequency:
+            # decrement the frequency of matched character
+            char_frequency[right_char] -= 1
+            if char_frequency[right_char] == 0:
+                matched += 1
+
+        if matched == len(char_frequency):
+            return True
+
+        # shrink the window by one character
+        if window_end >= len(pattern) - 1:
+            left_char = str1[window_start]
+            window_start += 1
+            if left_char in char_frequency:
+                if char_frequency[left_char] == 0:
+                    matched -= 1
+                char_frequency[left_char] += 1
+
+    return False
+
+
+def main():
+    print('Permutation exist: ' + str(find_permutation("oidbcaf", "abc")))
+    print('Permutation exist: ' + str(find_permutation("odicf", "dc")))
+    print('Permutation exist: ' + str(find_permutation("bcdxabcdy", "bcdyabcdx")))
+    print('Permutation exist: ' + str(find_permutation("aaacb", "abc")))
+
+
+main()
