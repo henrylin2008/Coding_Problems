71d357836b98613d8ce1f6b473cf444f490dfeeb
henrylin2008 <henrylin2008@yahoo.com>
2022-10-10 23:37:49 -0700

    Smallest Window containing Substrin

---
+++ LeetCode/Blind 75/Sliding Window/Outside 75/Smallest Window containing Substring.py
@@ -0,0 +1,27 @@
+# Smallest Window containing Substring (hard)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628541068682_8Unit
+
+# Problem Statement
+#
+# Given a string and a pattern, find the smallest substring in the given string which has all the character
+# occurrences of the given pattern.
+#
+# Example 1:
+# Input: String="aabdec", Pattern="abc"
+# Output: "abdec"
+# Explanation: The smallest substring having all characters of the pattern is "abdec"
+#
+# Example 2:
+# Input: String="aabdec", Pattern="abac"
+# Output: "aabdec"
+# Explanation: The smallest substring having all characters occurrences of the pattern is "aabdec"
+#
+# Example 3:
+# Input: String="abdbca", Pattern="abc"
+# Output: "bca"
+# Explanation: The smallest substring having all characters of the pattern is "bca".
+#
+# Example 4:
+# Input: String="adcad", Pattern="abc"
+# Output: ""
+# Explanation: No substring in the given string has all characters of the pattern
