dbdd572fb6430db1d5329304853f1ed1e528b68a
henrylin2008 <henrylin2008@yahoo.com>
2022-10-11 23:21:14 -0700

    Words Concatenation

--- LeetCode/Blind 75/Sliding Window/Outside 75/Word Concatenation.py
+++ LeetCode/Blind 75/Sliding Window/Outside 75/Word Concatenation.py
@@ -15,3 +15,14 @@
 # Input: String="catcatfoxfox", Words=["cat", "fox"]
 # Output: [3]
 # Explanation: The only substring containing both the words is "catfox".
+
+# Solution
+# This problem follows the Sliding Window pattern and has a lot of similarities with Maximum Sum Subarray of Size K.
+# We will keep track of all the words in a HashMap and try to match them in the given string. Here are the set of
+# steps for our algorithm:
+#   1. Keep the frequency of every word in a HashMap.
+#   2. Starting from every index in the string, try to match all the words.
+#   3. In each iteration, keep track of all the words that we have already seen in another HashMap.
+#   4. If a word is not found or has a higher frequency than required, we can move on to the next character in the
+#      string.
+#   5. Store the index if we have found all the words.
