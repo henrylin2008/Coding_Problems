174c837a7294dbb47e689f14305755813a63a7d5
henrylin2008 <henrylin2008@yahoo.com>
2022-10-11 23:23:19 -0700

    Smallest Window containing Substring

--- LeetCode/Blind 75/Sliding Window/Outside 75/Smallest Window containing Substring.py
+++ LeetCode/Blind 75/Sliding Window/Outside 75/Smallest Window containing Substring.py
@@ -88,3 +88,12 @@ def main():
 
 
 main()
+
+# Time Complexity
+# The time complexity of the above algorithm will be O(N + M) where ‘N’ and ‘M’ are the number of characters in
+# the input string and the pattern respectively.
+#
+# Space Complexity
+# The space complexity of the algorithm is O(M) since in the worst case, the whole pattern can have distinct
+# characters which will go into the HashMap. In the worst case, we also need O(N) space for the resulting
+# substring, which will happen when the input string is a permutation of the pattern.
