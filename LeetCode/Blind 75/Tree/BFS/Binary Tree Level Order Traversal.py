d302505c556de99fd66d3685e7f28f94f4a6992b
henrylin2008 <henrylin2008@yahoo.com>
2022-10-12 23:57:37 -0700

    Binary Tree Level Order Traversal

---
+++ LeetCode/Blind 75/Tree/BFS/Binary Tree Level Order Traversal.py
@@ -0,0 +1,18 @@
+# Binary Tree Level Order Traversal (easy)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743844952_44Unit
+
+# Problem Statement
+#
+# Given a binary tree, populate an array to represent its level-by-level traversal. You should populate the values of
+# all nodes of each level from left to right in separate sub-arrays.
+
+# Example 1:
+#       level order traversal: [ [1],
+#                                [2,3],
+#                                [4,5,6,7]]
+
+# Example 2:
+#       level order traversal: [ [12],
+#                                [7,1],
+#                                [9,10,5]]
+
