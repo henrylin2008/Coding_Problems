eb43684608bdf5020ea1563ac5fe8dfd6cdc015c
henrylin2008 <henrylin2008@yahoo.com>
2022-10-15 00:00:54 -0700

    Zigzag Traversal

---
+++ LeetCode/Blind 75/Tree/BFS/Zigzag Traversal.py
@@ -0,0 +1,19 @@
+# Zigzag Traversal (medium)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743859019_46Unit
+
+# Problem Statement
+#
+# Given a binary tree, populate an array to represent its zigzag level order traversal. You should populate the
+# values of all nodes of the first level from left to right, then right to left for the next level and keep
+# alternating in the same manner for the following levels.
+#
+# Example 1:
+#           [[1],
+#            [3, 2],
+#            [4, 5, 6, 7]]
+#
+# Example 2:
+#           [[12],
+#            [1, 7],
+#            [9, 10, 5],
+#            [17, 20]]
