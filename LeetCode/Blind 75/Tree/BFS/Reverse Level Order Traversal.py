df7bb017552817b7e493ccea198e39bcc5b79ea8
henrylin2008 <henrylin2008@yahoo.com>
2022-10-14 23:21:53 -0700

    Reverse Level Order Traversal

---
+++ LeetCode/Blind 75/Tree/BFS/Reverse Level Order Traversal.py
@@ -0,0 +1,18 @@
+# Reverse Level Order Traversal (easy)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743852073_45Unit
+
+# Problem Statement
+#
+# Given a binary tree, populate an array to represent its level-by-level traversal in reverse order, i.e.,
+# the lowest level comes first. You should populate the values of all nodes in each level from left to right in
+# separate sub-arrays.
+#
+# Example 1:
+#           [[4,5,6,7],
+#            [2,3],
+#            [1]]
+#
+# Example 2:
+#           [[9,10,5],
+#            [7,1],
+#            [12]]
