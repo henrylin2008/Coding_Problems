97ad16a3ec3e9621ba4268a728e07682865fdcbe
henrylin2008 <henrylin2008@yahoo.com>
2022-10-14 23:38:21 -0700

    Reverse Level Order Traversal

--- LeetCode/Blind 75/Tree/BFS/Reverse Level Order Traversal.py
+++ LeetCode/Blind 75/Tree/BFS/Reverse Level Order Traversal.py
@@ -23,3 +23,44 @@
 # difference will be that instead of appending the current level at the end, we will append the current level at the
 # beginning of the result list.
 
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+
+def traverse(root):
+    result = deque()        # different from Level Order Traversal
+    if root is None:
+        return result
+    queue = deque()
+    queue.append(root)
+    while queue:
+        level_size = len(queue)
+        current_level = []
+        for i in range(level_size):
+            current_node = queue.popleft()
+            current_level.append(current_node.val)
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+        if current_level:
+            result.appendleft(current_level)        # different from level order traversal
+    return result
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Reverse level order traversal: " + str(traverse(root)))
+
+
+main()
