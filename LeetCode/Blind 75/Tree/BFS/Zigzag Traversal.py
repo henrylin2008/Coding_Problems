b7cb4dec881ef93f34a395c3b1b5b8e43e18089b
henrylin2008 <henrylin2008@yahoo.com>
2022-10-15 23:53:39 -0700

    Zigzag Traversal

--- LeetCode/Blind 75/Tree/BFS/Zigzag Traversal.py
+++ LeetCode/Blind 75/Tree/BFS/Zigzag Traversal.py
@@ -79,3 +79,13 @@ def main():
 
 
 main()
+
+
+# Time Complexity
+# The time complexity of the above algorithm is O(N)O(N), where ‘N’ is the total number of nodes in the tree. This is
+# due to the fact that we traverse each node once.
+#
+# Space Complexity
+# The space complexity of the above algorithm will be O(N)O(N) as we need to return a list containing the level order
+# traversal. We will also need O(N)O(N) space for the queue. Since we can have a maximum of N/2N/2 nodes at any level
+# (this could happen only at the lowest level), therefore we will need O(N)O(N) space to store them in the queue.
