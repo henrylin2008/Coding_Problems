239a2999ebd2f2b788bb5410b8f427a911a5686e
henrylin2008 <henrylin2008@yahoo.com>
2022-10-16 23:48:34 -0700

    Level Averages in a Binary Tree

--- LeetCode/Blind 75/Tree/BFS/Level Averages in a Binary Tree.py
+++ LeetCode/Blind 75/Tree/BFS/Level Averages in a Binary Tree.py
@@ -33,3 +33,42 @@ class TreeNode:
     def __init__(self, val):
         self.val = val
         self.left, self.right = None, None
+
+
+def find_level_averages(root):
+    result = []
+    if root is None:
+        return result
+
+    queue = deque()
+    queue.append(root)
+    while queue:
+        level_size = len(queue)
+        level_sum = 0.0
+        for i in range(level_size):
+            current_node = queue.popleft()
+            # add the node's value to the running sum
+            level_sum += current_node.val
+            # insert the children of current node to the queue
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+        # append the current level's average to the result array
+        result.append(level_sum / level_size)
+
+    return result
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.left.right = TreeNode(2)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Level averages are: " + str(find_level_averages(root)))
+
+
+main()
