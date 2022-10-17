15699afe79bff28b38f3dfd958c3ae0be908f82a
henrylin2008 <henrylin2008@yahoo.com>
2022-10-16 22:16:07 -0700

    Level Averages in a Binary Tree

---
+++ LeetCode/Blind 75/Tree/BFS/Level Averages in a Binary Tree.py
@@ -0,0 +1,21 @@
+# Level Averages in a Binary Tree (easy)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743864804_47Unit
+
+# Problem Statement
+#
+# Given a binary tree, populate an array to represent the averages of all of its levels.
+
+# Example 1:
+#           1
+#         /   \
+#       2       3
+#     /   \    /  \
+#   4     5   6    7
+#   Level Averages: [1, 2.5, 5.5]
+#
+# Example 2:
+#           12
+#         /    \
+#       7        1
+#     /   \    /   \
+#    9     2  10    5
