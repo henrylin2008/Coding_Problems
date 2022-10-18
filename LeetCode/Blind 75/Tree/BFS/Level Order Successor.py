c19da7977bf6267fede6203a9e32c6d0054286be
henrylin2008 <henrylin2008@yahoo.com>
2022-10-18 10:17:23 -0700

    Level Order Successor

---
+++ LeetCode/Blind 75/Tree/BFS/Level Order Successor.py
@@ -0,0 +1,35 @@
+# Level Order Successor (easy)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743880609_49Unit
+
+# Problem Statement
+# Given a binary tree and a node, find the level order successor of the given node in the tree. The level order
+# successor is the node that appears right after the given node in the level order traversal.
+
+
+# Example 1:
+#           1
+#         /   \
+#       2       3
+#     /   \
+#   4     5
+# Given Node: 3
+# Level Order Successor: 4
+
+# Example 2:
+#           12
+#         /    \
+#       7        1
+#     /        /   \
+#    9       10     5
+# Given Node: 9
+# Level Order Successor: 10
+
+# Example 3:
+#           12
+#         /    \
+#       7        1
+#     /        /   \
+#    9       10     5
+# Given Node: 12
+# Level Order Successor: 7
+
