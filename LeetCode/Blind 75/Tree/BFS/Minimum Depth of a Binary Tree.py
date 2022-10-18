5d96d74acb145858a5e872e006b5e5de79b75bfb
henrylin2008 <henrylin2008@yahoo.com>
2022-10-18 00:19:06 -0700

    Minimum Depth of a Binary Tree

--- LeetCode/Blind 75/Tree/BFS/Minimum Depth of a Binary Tree.py
+++ LeetCode/Blind 75/Tree/BFS/Minimum Depth of a Binary Tree.py
@@ -83,7 +83,6 @@ def main():
 
 main()
 
-
 # Time Complexity
 # The time complexity of the above algorithm is O(N), where ‘N’ is the total number of nodes in the tree. This is
 # due to the fact that we traverse each node once.
@@ -92,3 +91,56 @@ main()
 # The space complexity of the above algorithm will be O(N) which is required for the queue. Since we can have a
 # maximum of N/2 nodes at any level (this could happen only at the lowest level), therefore we will need O(N)
 # space to store them in the queue.
+
+
+# Similar Problems
+# Problem 1: Given a binary tree, find its maximum depth (or height).
+#
+# Solution: We will follow a similar approach. Instead of returning as soon as we find a leaf node, we will keep
+# traversing for all the levels, incrementing maximumDepth each time we complete a level. Here is what the code will
+# look like:
+
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+
+def find_maximum_depth(root):
+    if root is None:
+        return 0
+
+    queue = deque()
+    queue.append(root)
+    maximumTreeDepth = 0
+    while queue:
+        maximumTreeDepth += 1
+        levelSize = len(queue)
+        for _ in range(levelSize):
+            currentNode = queue.popleft()
+
+            # insert the children of current node in the queue
+            if currentNode.left:
+                queue.append(currentNode.left)
+            if currentNode.right:
+                queue.append(currentNode.right)
+
+    return maximumTreeDepth
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Tree Maximum Depth: " + str(find_maximum_depth(root)))
+    root.left.left = TreeNode(9)
+    root.right.left.left = TreeNode(11)
+    print("Tree Maximum Depth: " + str(find_maximum_depth(root)))
+
+
+main()
