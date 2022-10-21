d63c6eb788d28963b7ea68cd8c622c968a89aed6
henrylin2008 <henrylin2008@yahoo.com>
2022-10-20 23:50:35 -0700

    Right View of a Binary Tree

---
+++ LeetCode/Blind 75/Tree/BFS/Right View of a Binary Tree.py
@@ -0,0 +1,25 @@
+# Right View of a Binary Tree (easy)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743902403_52Unit
+
+# Problem Statement
+#
+# Given a binary tree, return an array containing nodes in its right view. The right view of a binary tree is the set
+# of nodes visible when the tree is seen from the right side.
+
+# Example 1:
+#           1
+#         /   \
+#       2       3
+#     /   \    /  \
+#   4     5   6    7
+# Right view: [1, 3, 7]
+#
+# Example 2:
+#           12
+#         /    \
+#       7        1
+#     /        /   \
+#    9       10     5
+#    |
+#    3
+# Right view: [12, 1, 5, 3]
