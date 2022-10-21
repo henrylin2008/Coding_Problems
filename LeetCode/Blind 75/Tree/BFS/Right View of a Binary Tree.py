b2c5714574ff9f53bf5fa90abd24f0c5e1f93d91
henrylin2008 <henrylin2008@yahoo.com>
2022-10-21 11:13:45 -0700

    Right View of a Binary Tree

--- LeetCode/Blind 75/Tree/BFS/Right View of a Binary Tree.py
+++ LeetCode/Blind 75/Tree/BFS/Right View of a Binary Tree.py
@@ -28,3 +28,52 @@
 #
 # This problem follows the Binary Tree Level Order Traversal pattern. We can follow the same BFS approach. The only
 # additional thing we will be doing is to append the last node of each level to the result array.
+
+from __future__ import print_function
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+
+def tree_right_view(root):
+    result = []
+    if root is None:
+        return result
+
+    queue = deque()
+    queue.append(root)
+    while queue:
+        levelSize = len(queue)
+        for i in range(0, levelSize):
+            currentNode = queue.popleft()
+            # if it is the last node of this level, add it to the result
+            if i == levelSize - 1:
+                result.append(currentNode)
+            # insert the children of current node in the queue
+            if currentNode.left:
+                queue.append(currentNode.left)
+            if currentNode.right:
+                queue.append(currentNode.right)
+
+    return result
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    root.left.left.left = TreeNode(3)
+    result = tree_right_view(root)
+    print("Tree right view: ")
+    for node in result:
+        print(str(node.val) + " ", end='')
+
+
+main()
