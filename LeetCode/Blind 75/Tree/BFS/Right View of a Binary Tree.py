2d754e04ac874e6a7e124c23c95fedceda098bda
henrylin2008 <henrylin2008@yahoo.com>
2022-10-21 23:44:03 -0700

    Right View of a Binary Tree

--- LeetCode/Blind 75/Tree/BFS/Right View of a Binary Tree.py
+++ LeetCode/Blind 75/Tree/BFS/Right View of a Binary Tree.py
@@ -77,3 +77,12 @@ def main():
 
 
 main()
+
+# Time Complexity
+# The time complexity of the above algorithm is O(N), where ‘N’ is the total number of nodes in the tree. This is
+# due to the fact that we traverse each node once.
+#
+# Space Complexity
+# The space complexity of the above algorithm will be O(N) needed for the return list. We will also need O(N)
+# space for the queue. Since we can have a maximum of N/2 nodes at any level (this could happen only at the lowest
+# level), therefore we will need O(N) space to store them in the queue.
