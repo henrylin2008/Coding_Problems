c04fa6e061cd56ba4bd4d4a2b293f9f48caf1492
henrylin2008 <henrylin2008@yahoo.com>
2022-10-24 23:40:40 -0700

    Sum of Path Numbers

---
+++ LeetCode/Blind 75/Tree/DFS/Sum of Path Numbers.py
@@ -0,0 +1,25 @@
+# Sum of Path Numbers (medium)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743939828_56Unit
+
+# Problem Statement
+#
+# Given a binary tree where each node can only have a digit (0-9) value, each root-to-leaf path will represent a
+# number. Find the total sum of all the numbers represented by all paths.
+# Example 1:
+#           1
+#         /   \
+#       7       9
+#              /  \
+#             2    9
+# Output: 408
+# Explanation: The sum of all path numbers: 17 + 192 + 199
+#
+# Example 2:
+#           1
+#         /   \
+#       0       1
+#     /       /   \
+#    1       6     5
+# Output: 332
+# Explanation: The sum of all path numbers: 101 + 116 + 115
+
