964229887ca43e91abefc9d7e9fce307ff0ecacd
henrylin2008 <henrylin2008@yahoo.com>
2022-10-24 23:44:45 -0700

    Sum of Path Numbers

--- LeetCode/Blind 75/Tree/DFS/Sum of Path Numbers.py
+++ LeetCode/Blind 75/Tree/DFS/Sum of Path Numbers.py
@@ -23,3 +23,11 @@
 # Output: 332
 # Explanation: The sum of all path numbers: 101 + 116 + 115
 
+# Solution
+#
+# This problem follows the Binary Tree Path Sum pattern. We can follow the same DFS approach. The additional thing we
+# need to do is to keep track of the number representing the current path.
+#
+# How do we calculate the path number for a node? Taking the first example mentioned above, say we are at node ‘7’.
+# As we know, the path number for this node is ‘17’, which was calculated by: 1 * 10 + 7 => 17. We will follow the
+# same approach to calculate the path number of each node.
