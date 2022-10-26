dbc8d574203780ac739d36371a3f47ef1eecfc78
henrylin2008 <henrylin2008@yahoo.com>
2022-10-25 23:35:38 -0700

    Path With Given Sequence

--- LeetCode/Blind 75/Tree/DFS/Path with Given Sequence.py
+++ LeetCode/Blind 75/Tree/DFS/Path with Given Sequence.py
@@ -28,3 +28,8 @@
 # Output: true
 # Explanation: the tree does not have a path 1 -> 1 -> 6
 
+# Solution
+#
+# This problem follows the Binary Tree Path Sum pattern. We can follow the same DFS approach and additionally,
+# track the element of the given sequence that we should match with the current node. Also, we can return false as
+# soon as we find a mismatch between the sequence and the node value.
