7fd07042f733011b17835be597198aee042bbcaf
henrylin2008 <henrylin2008@yahoo.com>
2022-10-26 23:16:23 -0700

    Sum of Path Numbers

--- LeetCode/Blind 75/Tree/DFS/Sum of Path Numbers.py
+++ LeetCode/Blind 75/Tree/DFS/Sum of Path Numbers.py
@@ -31,3 +31,42 @@
 # How do we calculate the path number for a node? Taking the first example mentioned above, say we are at node ‘7’.
 # As we know, the path number for this node is ‘17’, which was calculated by: 1 * 10 + 7 => 17. We will follow the
 # same approach to calculate the path number of each node.
+
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def find_sum_of_path_numbers(root):
+    return find_root_to_leaf_path_numbers(root, 0)
+
+
+def find_root_to_leaf_path_numbers(currentNode, pathSum):
+    if currentNode is None:
+        return 0
+
+    # calculate the path number of the current node
+    pathSum = 10 * pathSum + currentNode.val
+
+    # if the current node is a leaf, return the current path sum
+    if currentNode.left is None and currentNode.right is None:
+        return pathSum
+
+    # traverse the left and the right sub-tree
+    return find_root_to_leaf_path_numbers(currentNode.left, pathSum) + \
+           find_root_to_leaf_path_numbers(currentNode.right, pathSum)
+
+
+def main():
+    root = TreeNode(1)
+    root.left = TreeNode(0)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(1)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(5)
+    print("Total Sum of Path Numbers: " + str(find_sum_of_path_numbers(root)))
+
+
+main()
