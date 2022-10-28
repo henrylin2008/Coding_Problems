658cc18dfffad8fab4cab74e1b141379cc88b368
henrylin2008 <henrylin2008@yahoo.com>
2022-10-27 23:45:43 -0700

    Count Paths for a Sum

--- LeetCode/Blind 75/Tree/DFS/Count Paths for a Sum.py
+++ LeetCode/Blind 75/Tree/DFS/Count Paths for a Sum.py
@@ -26,3 +26,19 @@
 # S: 11
 # Output: 2
 # Explanation: Here are the two paths with sum '11': 7 -> 4 and 1 -> 10
+#
+#
+# Solution
+#
+# This problem follows the Binary Tree Path Sum pattern. We can follow the same DFS approach. But there will be four
+# differences:
+#   1. We will keep track of the current path in a list which will be passed to every recursive call.
+#   2. Whenever we traverse a node we will do two things:
+#       - Add the current node to the current path.
+#       - As we added a new node to the current path, we should find the sums of all sub-paths ending at the current
+#         node. If the sum of any sub-path is equal to ‘S’ we will increment our path count.
+#   3. We will traverse all paths and will not stop processing after finding the first path.
+#   4. Remove the current node from the current path before returning from the function. This is needed to Backtrack
+#      while we are going up the recursive call stack to process other paths.
+
+
