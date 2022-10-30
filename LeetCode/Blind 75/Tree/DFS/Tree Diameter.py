a879dda88cdb784b05135e2e8af296ad99857b7a
henrylin2008 <henrylin2008@yahoo.com>
2022-10-29 22:53:08 -0700

    Tree Diameter

--- LeetCode/Blind 75/Tree/DFS/Tree Diameter.py
+++ LeetCode/Blind 75/Tree/DFS/Tree Diameter.py
@@ -28,3 +28,16 @@
 #              10  11
 # Output: 7
 # Explanation: The diameter of the tree is: [10, 8, 5, 3, 6, 9, 11]
+
+# Solution
+#
+# This problem follows the Binary Tree Path Sum pattern. We can follow the same DFS approach. There will be a few
+# differences:
+#   1.At every step, we need to find the height of both children of the current node. For this, we will make two
+#     recursive calls similar to DFS.
+#   2.The height of the current node will be equal to the maximum of the heights of its left or right children, plus
+#     ‘1’ for the current node.
+#   3.The tree diameter at the current node will be equal to the height of the left child plus the height of the right
+#     child plus ‘1’ for the current node: diameter = leftTreeHeight + rightTreeHeight + 1. To find the overall tree
+#     diameter, we will use a class level variable. This variable will store the maximum diameter of all the nodes
+#     visited so far, hence, eventually, it will have the final tree diameter.
