401653d343de9e66af7be44e7fb0f275832c9677
henrylin2008 <henrylin2008@yahoo.com>
2022-10-31 23:51:43 -0700

    Pair with Target Sum

--- LeetCode/Blind 75/Two Pointers/Pattern/Pair with Target Sum.py
+++ LeetCode/Blind 75/Two Pointers/Pattern/Pair with Target Sum.py
@@ -30,3 +30,25 @@
 #      need a pair with a smaller sum. So, to try more pairs, we can decrement the end-pointer.
 #   2. If the sum of the two numbers pointed by the two pointers is smaller than the target sum, this means that we need
 #      a pair with a larger sum. So, to try more pairs, we can increment the start-pointer.
+
+
+def pair_with_targetsum(arr, target_sum):
+    left, right = 0, len(arr) - 1
+    while (left < right):
+        current_sum = arr[left] + arr[right]
+        if current_sum == target_sum:
+            return [left, right]
+
+        if target_sum > current_sum:
+            left += 1  # we need a pair with a bigger sum
+        else:
+            right -= 1  # we need a pair with a smaller sum
+    return [-1, -1]
+
+
+def main():
+    print(pair_with_targetsum([1, 2, 3, 4, 6], 6))
+    print(pair_with_targetsum([2, 5, 9, 11], 11))
+
+
+main()
