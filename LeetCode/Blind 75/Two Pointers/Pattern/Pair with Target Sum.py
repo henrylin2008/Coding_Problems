bf7a1200d90547f8327726323a6f57fbc4072578
henrylin2008 <henrylin2008@yahoo.com>
2022-10-31 23:53:38 -0700

    Pair with Target Sum

--- LeetCode/Blind 75/Two Pointers/Pattern/Pair with Target Sum.py
+++ LeetCode/Blind 75/Two Pointers/Pattern/Pair with Target Sum.py
@@ -69,3 +69,19 @@ main()
 #      pair.
 #   2. Otherwise, insert “X” in the HashTable, so that we can search it for the later numbers.
 
+def pair_with_targetsum(arr, target_sum):
+    nums = {}  # to store numbers and their indices
+    for i, num in enumerate(arr):
+        if target_sum - num in nums:
+            return [nums[target_sum - num], i]
+        else:
+            nums[arr[i]] = i
+    return [-1, -1]
+
+
+def main():
+    print(pair_with_targetsum([1, 2, 3, 4, 6], 6))
+    print(pair_with_targetsum([2, 5, 9, 11], 11))
+
+
+main()
