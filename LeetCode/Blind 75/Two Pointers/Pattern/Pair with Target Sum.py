3374b7351fcc55009fbf6e96b56b665bc0dcb762
henrylin2008 <henrylin2008@yahoo.com>
2022-10-31 23:51:25 -0700

    Pair with Target Sum

--- LeetCode/Blind 75/Two Pointers/Pattern/Pair with Target Sum.py
+++ LeetCode/Blind 75/Two Pointers/Pattern/Pair with Target Sum.py
@@ -15,3 +15,18 @@
 # Input: [2, 5, 9, 11], target=11
 # Output: [0, 2]
 # Explanation: The numbers at index 0 and 2 add up to 11: 2+9=11
+
+# Solution
+#
+# Since the given array is sorted, a brute-force solution could be to iterate through the array, taking one number at
+# a time and searching for the second number through Binary Search. The time complexity of this algorithm will be O(
+# N*logN)O(N∗logN). Can we do better than this?
+#
+# We can follow the Two Pointers approach. We will start with one pointer pointing to the beginning of the array and
+# another pointing at the end. At every step, we will see if the numbers pointed by the two pointers add up to the
+# target sum. If they do, we have found our pair; otherwise, we will do one of two things:
+
+#   1. If the sum of the two numbers pointed by the two pointers is greater than the target sum, this means that we
+#      need a pair with a smaller sum. So, to try more pairs, we can decrement the end-pointer.
+#   2. If the sum of the two numbers pointed by the two pointers is smaller than the target sum, this means that we need
+#      a pair with a larger sum. So, to try more pairs, we can increment the start-pointer.
