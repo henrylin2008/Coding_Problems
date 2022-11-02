4fce99d904c61c210c2ca0cc141f0f5c72ded3eb
henrylin2008 <henrylin2008@yahoo.com>
2022-11-01 23:06:58 -0700

    Remove Duplicates

--- LeetCode/Blind 75/Two Pointers/Pattern/Remove Duplicates.py
+++ LeetCode/Blind 75/Two Pointers/Pattern/Remove Duplicates.py
@@ -49,6 +49,7 @@ def main():
 
 main()
 
+
 # Time Complexity
 # The time complexity of the above algorithm will be O(N), where ‘N’ is the total number of elements in the given array.
 #
@@ -59,3 +60,22 @@ main()
 # Similar Questions
 # Problem 1: Given an unsorted array of numbers and a target ‘key’, remove all instances of ‘key’ in-place and return
 # the new length of the array.
+
+def remove_element(arr, key):
+    nextElement = 0  # index of the next element which is not 'key'
+    for i in range(len(arr)):
+        if arr[i] != key:
+            arr[nextElement] = arr[i]
+            nextElement += 1
+
+    return nextElement
+
+
+def main():
+    print("Array new length: " +
+          str(remove_element([3, 2, 3, 6, 3, 10, 9, 3], 3)))
+    print("Array new length: " +
+          str(remove_element([2, 11, 2, 2, 1], 2)))
+
+
+main()
