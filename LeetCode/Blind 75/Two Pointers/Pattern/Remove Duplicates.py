f14264a9f48766256b0a0fa8e959d75958c32753
henrylin2008 <henrylin2008@yahoo.com>
2022-11-01 23:03:48 -0700

    Remove Duplicates

---
+++ LeetCode/Blind 75/Two Pointers/Pattern/Remove Duplicates.py
@@ -0,0 +1,21 @@
+# Remove Duplicates (easy)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743424499_2Unit
+
+# Problem Statement
+#
+# Given an array of sorted numbers, remove all duplicate number instances from it in-place, such that each element
+# appears only once. The relative order of the elements should be kept the same and you should not use any extra
+# space so that that the solution have a space complexity of O(1).
+#
+# Move all the unique elements at the beginning of the array and after moving return the length of the subarray that
+# has no duplicate in it.
+#
+# Example 1:
+# Input: [2, 3, 3, 3, 6, 9, 9]
+# Output: 4
+# Explanation: The first four elements after removing the duplicates will be [2, 3, 6, 9].
+#
+# Example 2:
+# Input: [2, 2, 2, 11]
+# Output: 2
+# Explanation: The first two elements after removing the duplicates will be [2, 11].
