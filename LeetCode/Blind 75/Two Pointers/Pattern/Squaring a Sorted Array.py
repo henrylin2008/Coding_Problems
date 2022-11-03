d68c544e8f3094050aadd03410f61025d2e1fd4d
henrylin2008 <henrylin2008@yahoo.com>
2022-11-02 23:35:10 -0700

    Squaring a Sorted Array

---
+++ LeetCode/Blind 75/Two Pointers/Pattern/Squaring a Sorted Array.py
@@ -0,0 +1,14 @@
+# Squaring a Sorted Array (easy)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743435284_3Unit
+
+# Problem Statement
+#
+# Given a sorted array, create a new array containing squares of all the numbers of the input array in the sorted order.
+#
+# Example 1:
+# Input: [-2, -1, 0, 2, 3]
+# Output: [0, 1, 4, 4, 9]
+#
+# Example 2:
+# Input: [-3, -1, 0, 1, 2]
+# Output: [0, 1, 1, 4, 9]
