cd415e9a14f11c6be676e308a48b47831b6e3863
henrylin2008 <henrylin2008@yahoo.com>
2022-11-03 23:16:52 -0700

    Triplet Sum to Zero

--- LeetCode/Blind 75/Two Pointers/Pattern/Triplet Sum to Zero.py
+++ LeetCode/Blind 75/Two Pointers/Pattern/Triplet Sum to Zero.py
@@ -13,3 +13,18 @@
 # Input: [-5, 2, -1, -2, 3]
 # Output: [[-5, 2, 3], [-2, -1, 3]]
 # Explanation: There are two unique triplets whose sum is equal to zero.
+
+# Solution
+#
+# This problem follows the Two Pointers pattern and shares similarities with Pair with Target Sum. A couple of
+# differences are that the input array is not sorted and instead of a pair we need to find triplets with a target sum
+# of zero.
+#
+# To follow a similar approach, first, we will sort the array and then iterate through it taking one number at a
+# time. Let’s say during our iteration we are at number ‘X’, so we need to find ‘Y’ and ‘Z’ such that X + Y + Z ==
+# 0X+Y+Z==0. At this stage, our problem translates into finding a pair whose sum is equal to “-X−X” (as from the
+# above equation Y + Z == -XY+Z==−X).
+#
+# Another difference from Pair with Target Sum is that we need to find all the unique triplets. To handle this,
+# we have to skip any duplicate number. Since we will be sorting the array, so all the duplicate numbers will be next
+# to each other and are easier to skip.
