1d93f2b5e1b3995d047f1d1a00b6cf7adfa4c0d9
henrylin2008 <henrylin2008@yahoo.com>
2022-11-05 23:38:57 -0700

    Triplets with Smaller Sum

--- LeetCode/Blind 75/Two Pointers/Pattern/Triplets with Smaller Sum.py
+++ LeetCode/Blind 75/Two Pointers/Pattern/Triplets with Smaller Sum.py
@@ -61,6 +61,7 @@ def main():
 
 main()
 
+
 # Time Complexity
 #
 # Sorting the array will take O(N * logN)O(N∗logN). The searchPair() will take O(N). So, overall searchTriplets()
@@ -70,3 +71,41 @@ main()
 #
 # The space complexity of the above algorithm will be O(N) which is required for sorting if we are not using an
 # in-place sorting algorithm.
+
+
+# Similar Problems
+#
+# Problem: Write a function to return the list of all such triplets instead of the count. How will the time
+# complexity change in this case?
+#
+# Solution: Following a similar approach we can create a list containing all the triplets. Here is the code - only
+# the highlighted lines have changed:
+
+def triplet_with_smaller_sum(arr, target):
+    arr.sort()
+    triplets = []
+    for i in range(len(arr) - 2):
+        search_pair(arr, target - arr[i], i, triplets)
+    return triplets
+
+
+def search_pair(arr, target_sum, first, triplets):
+    left = first + 1
+    right = len(arr) - 1
+    while left < right:
+        if arr[left] + arr[right] < target_sum:  # found the triplet
+            # since arr[right] >= arr[left], therefore, we can replace arr[right] by any
+            # number between left and right to get a sum less than the target sum
+            for i in range(right, left, -1):
+                triplets.append([arr[first], arr[left], arr[i]])
+            left += 1
+        else:
+            right -= 1  # we need a pair with a smaller sum
+
+
+def main():
+    print(triplet_with_smaller_sum([-1, 0, 2, 3], 3))
+    print(triplet_with_smaller_sum([-1, 4, 2, 1, 3], 5))
+
+
+main()
