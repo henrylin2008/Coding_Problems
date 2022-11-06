26aeae8420b1cabde42038fa0cba515d2c041fcc
henrylin2008 <henrylin2008@yahoo.com>
2022-11-05 23:37:06 -0700

    Triplets with Smaller Sum

--- LeetCode/Blind 75/Two Pointers/Pattern/Triplets with Smaller Sum.py
+++ LeetCode/Blind 75/Two Pointers/Pattern/Triplets with Smaller Sum.py
@@ -17,3 +17,16 @@
 # Output: 4
 # Explanation: There are four triplets whose sum is less than the target:
 #    [-1, 1, 4], [-1, 1, 3], [-1, 1, 2], [-1, 2, 3]
+
+
+# Solution
+#
+# This problem follows the Two Pointers pattern and shares similarities with Triplet Sum to Zero. The only difference
+# is that, in this problem, we need to find the triplets whose sum is less than the given target. To meet the
+# condition i != j != k we need to make sure that each number is not used more than once.
+#
+# Following a similar approach, first, we can sort the array and then iterate through it, taking one number at a
+# time. Let’s say during our iteration we are at number ‘X’, so we need to find ‘Y’ and ‘Z’ such that X + Y + Z <
+# targetX+Y+Z<target. At this stage, our problem translates into finding a pair whose sum is less than “target -
+# Xtarget−X” (as from the above equation Y + Z == target - XY+Z==target−X). We can use a similar approach as
+# discussed in Triplet Sum to Zero.
