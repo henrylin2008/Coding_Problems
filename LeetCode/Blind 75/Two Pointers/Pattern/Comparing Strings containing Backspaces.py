7f58cea0cade5f739104ca0003ef0798327473c2
henrylin2008 <henrylin2008@yahoo.com>
2022-11-10 23:25:18 -0800

    Comparing Strings containing Backspaces

--- LeetCode/Blind 75/Two Pointers/Pattern/Comparing Strings containing Backspaces.py
+++ LeetCode/Blind 75/Two Pointers/Pattern/Comparing Strings containing Backspaces.py
@@ -25,3 +25,13 @@
 # Input: str1="xywrrmp", str2="xywrrmu#p"
 # Output: true
 # Explanation: After applying backspaces the strings become "xywrrmp" and "xywrrmp" respectively.
+
+# Solution
+#
+# To compare the given strings, first, we need to apply the backspaces. An efficient way to do this would be from the
+# end of both the strings. We can have separate pointers, pointing to the last element of the given strings. We can
+# start comparing the characters pointed out by both the pointers to see if the strings are equal. If, at any stage,
+# the character pointed out by any of the pointers is a backspace (’#’), we will skip and apply the backspace until
+# we have a valid character available for comparison.
+
+
