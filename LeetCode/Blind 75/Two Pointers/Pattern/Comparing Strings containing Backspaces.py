482de4c03e14174bdb0ca0b58a83bd6ce853be38
henrylin2008 <henrylin2008@yahoo.com>
2022-11-10 23:24:57 -0800

    Comparing Strings containing Backspaces

---
+++ LeetCode/Blind 75/Two Pointers/Pattern/Comparing Strings containing Backspaces.py
@@ -0,0 +1,27 @@
+# Problem Challenge 2: Comparing Strings containing Backspaces (medium)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743506829_10Unit
+
+# Problem Statement
+#
+# Given two strings containing backspaces (identified by the character ‘#’), check if the two strings are equal.
+#
+# Example 1:
+# Input: str1="xy#z", str2="xzz#"
+# Output: true
+# Explanation: After applying backspaces the strings become "xz" and "xz" respectively.
+#
+# Example 2:
+# Input: str1="xy#z", str2="xyz#"
+# Output: false
+# Explanation: After applying backspaces the strings become "xz" and "xy" respectively.
+#
+# Example 3:
+# Input: str1="xp#", str2="xyz##"
+# Output: true
+# Explanation: After applying backspaces the strings become "x" and "x" respectively.
+# In "xyz##", the first '#' removes the character 'z' and the second '#' removes the character 'y'.
+#
+# Example 4:
+# Input: str1="xywrrmp", str2="xywrrmu#p"
+# Output: true
+# Explanation: After applying backspaces the strings become "xywrrmp" and "xywrrmp" respectively.
