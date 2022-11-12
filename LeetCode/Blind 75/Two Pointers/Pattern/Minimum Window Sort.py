4786f7ce3b130c1e1c2571e51ae55b174a856b3e
henrylin2008 <henrylin2008@yahoo.com>
2022-11-11 23:48:29 -0800

    Minimum Window Sort

---
+++ LeetCode/Blind 75/Two Pointers/Pattern/Minimum Window Sort.py
@@ -0,0 +1,26 @@
+# Problem Challenge 3: Minimum Window Sort (medium)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743511885_11Unit
+
+
+# Problem Statement
+
+# Given an array, find the length of the smallest subarray in it which when sorted will sort the whole array.
+# Example 1:
+# Input: [1, 2, 5, 3, 7, 10, 9, 12]
+# Output: 5
+# Explanation: We need to sort only the subarray [5, 3, 7, 10, 9] to make the whole array sorted
+#
+# Example 2:
+# Input: [1, 3, 2, 0, -1, 7, 10]
+# Output: 5
+# Explanation: We need to sort only the subarray [1, 3, 2, 0, -1] to make the whole array sorted
+#
+# Example 3:
+# Input: [1, 2, 3]
+# Output: 0
+# Explanation: The array is already sorted
+#
+# Example 4:
+# Input: [3, 2, 1]
+# Output: 3
+# Explanation: The whole array needs to be sorted.
