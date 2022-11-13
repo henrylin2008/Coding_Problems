b4c38a134b41cd38ec28ba5c39c2eee90ed786c7
henrylin2008 <henrylin2008@yahoo.com>
2022-11-12 23:49:59 -0800

    LinkedList Cycle

--- LeetCode/Blind 75/Fast & Slow Pointers/LinkedList Cycle.py
+++ LeetCode/Blind 75/Fast & Slow Pointers/LinkedList Cycle.py
@@ -29,3 +29,36 @@
 # This concludes that the two pointers will definitely meet if the LinkedList has a cycle. A similar analysis can be done where the slow pointer moves first. Here is a visual representation of the above discussion:
 
 
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+def has_cycle(head):
+    slow, fast = head, head
+    while fast is not None and fast.next is not None:
+        fast = fast.next.next
+        slow = slow.next
+        if slow == fast:
+        return True  # found the cycle
+    return False
+
+
+def main():
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+    print("LinkedList has cycle: " + str(has_cycle(head)))
+
+    head.next.next.next.next.next.next = head.next.next
+    print("LinkedList has cycle: " + str(has_cycle(head)))
+
+    head.next.next.next.next.next.next = head.next.next.next
+    print("LinkedList has cycle: " + str(has_cycle(head)))
+
+
+main()
