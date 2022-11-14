c0767ff0c2574cf5c335e120e15b3e9f1cfaca06
henrylin2008 <henrylin2008@yahoo.com>
2022-11-13 23:47:28 -0800

    Start of LinkedList Cycle

--- LeetCode/Blind 75/Fast & Slow Pointers/Start of LinkedList Cycle.py
+++ LeetCode/Blind 75/Fast & Slow Pointers/Start of LinkedList Cycle.py
@@ -22,3 +22,17 @@
 # head -> 1 --> 2 --> 3 --> 4 --> 5 --> 6
 #         ^                             |
 #         | --------------------------- |
+# 
+# Solution
+
+# If we know the length of the LinkedList cycle, we can find the start of the cycle through the following steps:
+#   1. Take two pointers. Let’s call them pointer1 and pointer2.
+#   2. Initialize both pointers to point to the start of the LinkedList.
+#   3. We can find the length of the LinkedList cycle using the approach discussed in  LinkedList Cycle. Let’s assume that the length of the cycle is ‘K’ nodes.
+#   4. Move pointer2 ahead by ‘K’ nodes.
+#   5. Now, keep incrementing pointer1 and pointer2 until they both meet.
+#   6. As pointer2 is ‘K’ nodes ahead of pointer1, which means, pointer2 must have completed one loop in the cycle when both pointers meet. Their meeting point will be the start of the cycle.
+# Let’s visually see this with the above-mentioned Example-1:
+
+
+# We can use the algorithm discussed in LinkedList Cycle to find the length of the cycle and then follow the above-mentioned steps to find the start of the cycle.
