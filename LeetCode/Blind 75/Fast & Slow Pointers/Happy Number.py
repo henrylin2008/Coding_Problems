8c0ab72d1832fff9958f3ed55288d5606618f124
henrylin2008 <henrylin2008@yahoo.com>
2022-11-14 23:17:11 -0800

    Happy Number

--- LeetCode/Blind 75/Fast & Slow Pointers/Happy Number.py
+++ LeetCode/Blind 75/Fast & Slow Pointers/Happy Number.py
@@ -49,3 +49,29 @@
 # of elements. As we have described above, each number will definitely have a cycle. Therefore, we will use the same
 # fast & slow pointer strategy to find the cycle and once the cycle is found, we will see if the cycle is stuck on
 # number ‘1’ to find out if the number is happy or not.
+
+def find_happy_number(num):
+    slow, fast = num, num
+    while True:
+        slow = find_square_sum(slow)  # move one step
+        fast = find_square_sum(find_square_sum(fast))  # move two steps
+        if slow == fast:  # found the cycle
+            break
+    return slow == 1  # see if the cycle is stuck on the number '1'
+
+
+def find_square_sum(num):
+    _sum = 0
+    while num > 0:
+        digit = num % 10
+        _sum += digit * digit
+        num //= 10
+    return _sum
+
+
+def main():
+    print(find_happy_number(23))
+    print(find_happy_number(12))
+
+
+main()
