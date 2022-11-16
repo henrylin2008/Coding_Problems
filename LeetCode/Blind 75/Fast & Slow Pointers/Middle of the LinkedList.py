2bc07f948466729a87acf18baf43f9f9d246986a
henrylin2008 <henrylin2008@yahoo.com>
2022-11-15 23:19:05 -0800

    Middle of the LinkedList

--- LeetCode/Blind 75/Fast & Slow Pointers/Middle of the LinkedList.py
+++ LeetCode/Blind 75/Fast & Slow Pointers/Middle of the LinkedList.py
@@ -18,3 +18,12 @@
 # Example 3:
 # Input: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> null
 # Output: 4
+
+# Solution
+#
+# One brute force strategy could be to first count the number of nodes in the LinkedList and then find the middle
+# node in the second iteration. Can we do this in one iteration?
+#
+# We can use the Fast & Slow pointers method such that the fast pointer is always twice the nodes ahead of the slow
+# pointer. This way, when the fast pointer reaches the end of the LinkedList, the slow pointer will be pointing at
+# the middle node.
