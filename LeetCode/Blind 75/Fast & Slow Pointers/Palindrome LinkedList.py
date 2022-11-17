2917153e5c6aa6928d8c22621f1de6b38e21ceeb
henrylin2008 <henrylin2008@yahoo.com>
2022-11-16 23:12:09 -0800

    Palindrome LinkedList

--- LeetCode/Blind 75/Fast & Slow Pointers/Palindrome LinkedList.py
+++ LeetCode/Blind 75/Fast & Slow Pointers/Palindrome LinkedList.py
@@ -15,3 +15,18 @@
 # Example 2:
 # Input: 2 -> 4 -> 6 -> 4 -> 2 -> 2 -> null
 # Output: false
+
+# Solution
+#
+# As we know, a palindrome LinkedList will have nodes values that read the same backward or forward. This means that
+# if we divide the LinkedList into two halves, the node values of the first half in the forward direction should be
+# similar to the node values of the second half in the backward direction. As we have been given a Singly LinkedList,
+# we can’t move in the backward direction. To handle this, we will perform the following steps:
+
+#   1. We can use the Fast & Slow pointers method similar to Middle of the LinkedList to find the middle node of the
+#      LinkedList.
+#   2. Once we have the middle of the LinkedList, we will reverse the second half.
+#   3. Then, we will compare the first half with the reversed second half to see if the LinkedList represents a
+#      palindrome.
+#   4. Finally, we will reverse the second half of the LinkedList again to revert and bring the LinkedList back to its
+#      original form.
