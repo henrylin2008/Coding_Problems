ffd1c55329f5b06641d7f5d6186adf28ac84259e
henrylin2008 <henrylin2008@yahoo.com>
2022-11-16 23:11:07 -0800

    Palindrome LinkedList

---
+++ LeetCode/Blind 75/Fast & Slow Pointers/Palindrome LinkedList.py
@@ -0,0 +1,17 @@
+# Problem Challenge 1: Palindrome LinkedList (medium)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743582805_17Unit
+
+# Problem Statement
+#
+# Given the head of a Singly LinkedList, write a method to check if the LinkedList is a palindrome or not.
+#
+# Your algorithm should use constant space and the input LinkedList should be in the original form once the algorithm
+# is finished. The algorithm should have O(N)O(N) time complexity where ‘N’ is the number of nodes in the LinkedList.
+#
+# Example 1:
+# Input: 2 -> 4 -> 6 -> 4 -> 2 -> null
+# Output: true
+#
+# Example 2:
+# Input: 2 -> 4 -> 6 -> 4 -> 2 -> 2 -> null
+# Output: false
