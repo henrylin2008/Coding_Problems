d1ec5da3d52bbd82987e38c4dd0c84e49d7b8130
henrylin2008 <henrylin2008@yahoo.com>
2022-11-18 23:03:03 -0800

    Cycle in a Circular Array

--- LeetCode/Blind 75/Fast & Slow Pointers/Cycle in a Circular Array.py
+++ LeetCode/Blind 75/Fast & Slow Pointers/Cycle in a Circular Array.py
@@ -28,3 +28,17 @@
 # Input: [2, 1, -1, -2]
 # Output: false
 # Explanation: The array does not have any cycle.
+
+# Solution
+#
+# This problem involves finding a cycle in the array and, as we know, the Fast & Slow pointer method is an efficient
+# way to do that. We can start from each index of the array to find the cycle. If a number does not have a cycle we
+# will move forward to the next element. There are a couple of additional things we need to take care of:
+#   1. As mentioned in the problem, the cycle should have more than one element. This means that when we move a pointer
+#      forward, if the pointer points to the same element after the move, we have a one-element cycle. Therefore,
+#      we can finish our cycle search for the current element.
+#   2. The other requirement mentioned in the problem is that the cycle should not contain both forward and backward
+#      movements. We will handle this by remembering the direction of each element while searching for the cycle. If the
+#      number is positive, the direction will be forward and if the number is negative, the direction will be backward.
+#      So whenever we move a pointer forward, if there is a change in the direction, we will finish our cycle search
+#      right there for the current element.
