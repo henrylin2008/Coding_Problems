6f9e12e999c9afaee498e0c669e6ac57f0b63b30
henrylin2008 <henrylin2008@yahoo.com>
2022-11-21 00:46:39 -0800

    Order-agnostic Binary Search

---
+++ Coding Patterns/Modified Binary Search/Order-agnostic Binary Search.py
@@ -0,0 +1,25 @@
+# Order-agnostic Binary Search (easy)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628744135675_77Unit
+
+# Problem Statement
+# Given a sorted array of numbers, find if a given number ‘key’ is present in the array. Though we know that the
+# array is sorted, we don’t know if it’s sorted in ascending or descending order. You should assume that the array
+# can have duplicates.
+#
+# Write a function to return the index of the ‘key’ if it is present in the array, otherwise return -1.
+#
+# Example 1:
+# Input: [4, 6, 10], key = 10
+# Output: 2
+#
+# Example 2:
+# Input: [1, 2, 3, 4, 5, 6, 7], key = 5
+# Output: 4
+#
+# Example 3:
+# Input: [10, 6, 4], key = 10
+# Output: 0
+#
+# Example 4:
+# Input: [10, 6, 4], key = 4
+# Output: 2
