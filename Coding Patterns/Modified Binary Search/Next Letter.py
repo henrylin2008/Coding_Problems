ff76dd46f3faa295adaeed01522dd3836a1ab750
henrylin2008 <henrylin2008@yahoo.com>
2022-11-26 23:43:39 -0800

    Next Letter

--- Coding Patterns/Modified Binary Search/Next Letter.py
+++ Coding Patterns/Modified Binary Search/Next Letter.py
@@ -31,3 +31,19 @@
 # Input: ['a', 'c', 'f', 'h'], key = 'h'
 # Output: 'a'
 # Explanation: As the array is assumed to be circular, the smallest letter greater than 'h' is 'a'.
+
+# Solution
+#
+# The problem follows the Binary Search pattern. Since Binary Search helps us find an element in a sorted array
+# efficiently, we can use a modified version of it to find the next letter.
+#
+# We can use a similar approach as discussed in Ceiling of a Number. There are a couple of differences though:
+#   1. The array is considered circular, which means if the ‘key’ is bigger than the last letter of the array or if it
+#      is smaller than the first letter of the array, the key’s next letter will be the first letter of the array.
+#   2. The other difference is that we have to find the next biggest letter which can’t be equal to the ‘key’. This
+#      means that we will ignore the case where key == arr[middle]. To handle this case, we can update our start range
+#      to start = middle +1.
+#
+# In the end, instead of returning the element pointed out by start, we have to return the letter pointed out by
+# start % array_length. This is needed because of point 2 discussed above. Imagine that the last letter of the array
+# is equal to the ‘key’. In that case, we have to return the first letter of the input array.
