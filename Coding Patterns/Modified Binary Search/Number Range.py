b196fc81735293bc185131bc2383d9e637167455
henrylin2008 <henrylin2008@yahoo.com>
2022-11-27 22:43:55 -0800

    Number Range

---
+++ Coding Patterns/Modified Binary Search/Number Range.py
@@ -0,0 +1,21 @@
+# Number Range (medium)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628744155307_80Unit
+
+# Problem Statement
+#
+# Given an array of numbers sorted in ascending order, find the range of a given number ‘key’. The range of the ‘key’
+# will be the first and last position of the ‘key’ in the array.
+#
+# Write a function to return the range of the ‘key’. If the ‘key’ is not present return [-1, -1].
+#
+# Example 1:
+# Input: [4, 6, 6, 6, 9], key = 6
+# Output: [1, 3]
+#
+# Example 2:
+# Input: [1, 3, 8, 10, 15], key = 10
+# Output: [3, 3]
+#
+# Example 3:
+# Input: [1, 3, 8, 10, 15], key = 12
+# Output: [-1, -1]
