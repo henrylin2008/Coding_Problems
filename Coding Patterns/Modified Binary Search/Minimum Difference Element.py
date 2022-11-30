de6e32f96b56469df663985c19406f8f82e40682
henrylin2008 <henrylin2008@yahoo.com>
2022-11-29 23:09:59 -0800

    Minimum Difference Element

---
+++ Coding Patterns/Modified Binary Search/Minimum Difference Element.py
@@ -0,0 +1,25 @@
+# Minimum Difference Element (medium)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628744174253_82Unit
+
+# Problem Statement
+#
+# Given an array of numbers sorted in ascending order, find the element in the array that has the minimum difference
+# with the given ‘key’.
+#
+# Example 1:
+# Input: [4, 6, 10], key = 7
+# Output: 6
+# Explanation: The difference between the key '7' and '6' is minimum than any other number in the array
+#
+# Example 2:
+# Input: [4, 6, 10], key = 4
+# Output: 4
+#
+# Example 3:
+# Input: [1, 3, 8, 10, 15], key = 12
+# Output: 10
+#
+# Example 4:
+# Input: [4, 6, 10], key = 17
+# Output: 10
+
