2c31fd853a045f834424c8338b58fd59f953952a
henrylin2008 <henrylin2008@yahoo.com>
2022-11-30 22:57:22 -0800

    Bitonic Array Maximum

--- Coding Patterns/Modified Binary Search/Bitonic Array Maximum.py
+++ Coding Patterns/Modified Binary Search/Bitonic Array Maximum.py
@@ -38,3 +38,25 @@
 #      the required number will be after middle. This means we will be doing: start = middle + 1.
 # We can break when start == end. Due to the two points mentioned above, both start and end will be pointing at the
 # maximum number of the bitonic array.
+
+def find_max_in_bitonic_array(arr):
+    start, end = 0, len(arr) - 1
+    while start < end:
+        mid = start + (end - start) // 2
+        if arr[mid] > arr[mid + 1]:
+            end = mid
+        else:
+            start = mid + 1
+
+    # at the end of the while loop, 'start == end'
+    return arr[start]
+
+
+def main():
+    print(find_max_in_bitonic_array([1, 3, 8, 12, 4, 2]))
+    print(find_max_in_bitonic_array([3, 8, 3, 1]))
+    print(find_max_in_bitonic_array([1, 3, 8, 12]))
+    print(find_max_in_bitonic_array([10, 9, 8]))
+
+
+main()
