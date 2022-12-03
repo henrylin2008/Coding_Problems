5332533ad28ec9a32806dc3e8afdae1fb713b1bb
henrylin2008 <henrylin2008@yahoo.com>
2022-12-02 23:51:52 -0800

    Search in Rotated Array

--- Coding Patterns/Modified Binary Search/Search in Rotated Array.py
+++ Coding Patterns/Modified Binary Search/Search in Rotated Array.py
@@ -75,6 +75,7 @@ def main():
 
 main()
 
+
 # Time Complexity
 # Since we are reducing the search range by half at every step, this means that the time complexity of our algorithm
 # will be O(logN) where ‘N’ is the total elements in the given array.
@@ -102,3 +103,39 @@ main()
 # The only problematic scenario is when the numbers at indices start, middle, and end are the same, as in this case,
 # we can’t decide which part of the array is sorted. In such a case, the best we can do is to skip one number from
 # both ends: start = start + 1 & end = end - 1
+
+# The code is quite similar to the above solution. Only the highlighted lines have changed
+def search_rotated_with_duplicates(arr, key):
+    start, end = 0, len(arr) - 1
+    while start <= end:
+        mid = start + (end - start) // 2
+        if arr[mid] == key:
+            return mid
+
+        # the only difference from the previous solution,
+        # if numbers at indexes start, mid, and end are same, we can't choose a side
+        # the best we can do, is to skip one number from both ends as key != arr[mid]
+        if arr[start] == arr[mid] and arr[end] == arr[mid]:
+            start += 1
+            end -= 1
+        elif arr[start] <= arr[mid]:  # left side is sorted in ascending order
+            if arr[start] <= key < arr[mid]:
+                end = mid - 1
+            else:  # key > arr[mid]
+                start = mid + 1
+
+        else:  # right side is sorted in ascending order
+            if arr[mid] < key <= arr[end]:
+                start = mid + 1
+            else:
+                end = mid - 1
+
+    # we are not able to find the element in the given array
+    return -1
+
+
+def main():
+    print(search_rotated_with_duplicates([3, 7, 3, 3, 3], 7))
+
+
+main()
