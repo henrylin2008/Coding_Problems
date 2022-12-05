ef8cb63c33b017fdc4801132736f984e19168aa1
henrylin2008 <henrylin2008@yahoo.com>
2022-12-04 23:45:29 -0800

    Reverse a LinkedList

---
+++ Coding Patterns/In-place Reversal of a linked list/Reverse a LinkedList.py
@@ -0,0 +1,11 @@
+# Reverse a LinkedList (easy)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743784192_38Unit
+
+# Problem Statement
+#
+# Given the head of a Singly LinkedList, reverse the LinkedList. Write a function to return the new head of the
+# reversed LinkedList.
+# Ex:
+#
+# Original List: head -> 2 -> 4 -> 6 -> 8 -> 10 -> null
+# Reversed list: null <- 2 <- 4 <- 6 <- 8 <- 10 <- head
