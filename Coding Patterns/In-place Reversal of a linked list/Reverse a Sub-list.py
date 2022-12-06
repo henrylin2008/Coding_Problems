a0bee8c6e80854fdc3c4230e6b9934fddde00f00
henrylin2008 <henrylin2008@yahoo.com>
2022-12-05 23:03:58 -0800

    Reverse a Sub-list

--- Coding Patterns/In-place Reversal of a linked list/Reverse a Sub-list.py
+++ Coding Patterns/In-place Reversal of a linked list/Reverse a Sub-list.py
@@ -8,3 +8,11 @@
 # Original List:   head -> 1 -> 2 -> 3 -> 4 -> 5 -> null
 #  p=2, q=4        head -> 1 -> 4 -> 3 -> 2 -> 5 -> null
 
+# Solution
+#
+# The problem follows the In-place Reversal of a LinkedList pattern. We can use a similar approach as discussed in
+# Reverse a LinkedList. Here are the steps we need to follow:
+#   1. Skip the first p-1 nodes, to reach the node at position p.
+#   2. Remember the node at position p-1 to be used later to connect with the reversed sub-list.
+#   3. Next, reverse the nodes from p to q using the same approach discussed in Reverse a LinkedList.
+#   4. Connect the p-1 and q+1 nodes to the reversed sub-list.
