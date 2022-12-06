355c1e49f8951e97c7ddd3907403b9f298fc8bc5
henrylin2008 <henrylin2008@yahoo.com>
2022-12-05 23:03:34 -0800

    Reverse a Sub-list

---
+++ Coding Patterns/In-place Reversal of a linked list/Reverse a Sub-list.py
@@ -0,0 +1,10 @@
+# Reverse a Sub-list (medium)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743790633_39Unit
+
+# Problem Statement
+#
+# Given the head of a LinkedList and two positions ‘p’ and ‘q’, reverse the LinkedList from position ‘p’ to ‘q’.
+# Ex:
+# Original List:   head -> 1 -> 2 -> 3 -> 4 -> 5 -> null
+#  p=2, q=4        head -> 1 -> 4 -> 3 -> 2 -> 5 -> null
+
