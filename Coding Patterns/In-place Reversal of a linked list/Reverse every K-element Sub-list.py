2c151c33023b02d9f22764c91b7f1d82a50d809b
henrylin2008 <henrylin2008@yahoo.com>
2022-12-06 23:35:58 -0800

    Reverse every K-element Sub-list

---
+++ Coding Patterns/In-place Reversal of a linked list/Reverse every K-element Sub-list.py
@@ -0,0 +1,15 @@
+# Reverse every K-element Sub-list (medium)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743796880_40Unit
+
+# Problem Statement
+#
+# Given the head of a LinkedList and a number ‘k’, reverse every ‘k’ sized sub-list starting from the head.
+#
+# If, in the end, you are left with a sub-list with less than ‘k’ elements, reverse it too.
+
+# Ex:
+# k = 3
+# Original List:        head -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> null
+#
+# Reversed Sub-list:    head -> 3 -> 2 -> 1 -> 6 -> 5 -> 4 -> 8 -> 7 -> null
+
