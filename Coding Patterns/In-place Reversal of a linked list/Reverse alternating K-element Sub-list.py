ac6c4488b1ed078b3243cd6077efe825c0364184
henrylin2008 <henrylin2008@yahoo.com>
2022-12-07 23:14:53 -0800

    Reverse alternating K-element Sub-list

---
+++ Coding Patterns/In-place Reversal of a linked list/Reverse alternating K-element Sub-list.py
@@ -0,0 +1,13 @@
+# Problem Challenge 1: Reverse alternating K-element Sub-list (medium)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743804225_41Unit
+
+# Problem Statement
+#
+# Given the head of a LinkedList and a number ‘k’, reverse every alternating ‘k’ sized sub-list starting from the head.
+#
+# If, in the end, you are left with a sub-list with less than ‘k’ elements, reverse it too.
+
+# Ex:
+# k = 2
+# Original List:        head -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> 8 -> null
+# Reversed Sub-list:    head -> 2 -> 1 -> 3 -> 4 -> 6 -> 5 -> 7 -> 8 -> null
