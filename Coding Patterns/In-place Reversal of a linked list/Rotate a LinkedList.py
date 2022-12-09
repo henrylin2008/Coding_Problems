d734b602c1498ffb0e0686bcb60cc21b58c1066b
henrylin2008 <henrylin2008@yahoo.com>
2022-12-08 23:46:29 -0800

    Rotate a LinkedList

---
+++ Coding Patterns/In-place Reversal of a linked list/Rotate a LinkedList.py
@@ -0,0 +1,16 @@
+# Problem Challenge 2: Rotate a LinkedList (medium)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743812259_42Unit
+
+# Problem Statement
+#
+# Given the head of a Singly LinkedList and a number ‘k’, rotate the LinkedList to the right by ‘k’ nodes.
+
+# Ex 1:
+# k = 3
+# Original List:        head -> 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null
+# Rotated LinkedList:   head -> 4 -> 5 -> 6 -> 1 -> 2 -> 3 -> null
+
+# Ex 2:
+# k = 8
+# Original List:        head -> 1 -> 2 -> 3 -> 4 -> 5 -> null
+# Rotated LinkedList:   head -> 3 -> 4 -> 5 -> 1 -> 2 -> null
