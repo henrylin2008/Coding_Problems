a2db7701ec7b00a916736191c4bff35ca6caa985
henrylin2008 <henrylin2008@yahoo.com>
2022-12-08 23:47:09 -0800

    Rotate a LinkedList

--- Coding Patterns/In-place Reversal of a linked list/Rotate a LinkedList.py
+++ Coding Patterns/In-place Reversal of a linked list/Rotate a LinkedList.py
@@ -14,3 +14,11 @@
 # k = 8
 # Original List:        head -> 1 -> 2 -> 3 -> 4 -> 5 -> null
 # Rotated LinkedList:   head -> 3 -> 4 -> 5 -> 1 -> 2 -> null
+
+# Solution
+# Another way of defining the rotation is to take the sub-list of ‘k’ ending nodes of the LinkedList and connect them
+# to the beginning. Other than that we have to do three more things:
+#   1. Connect the last node of the LinkedList to the head, because the list will have a different tail after the
+#      rotation.
+#   2. The new head of the LinkedList will be the node at the beginning of the sublist.
+#   3. The node right before the start of sub-list will be the new tail of the rotated LinkedList.
