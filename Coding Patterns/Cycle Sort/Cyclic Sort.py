a27551f59353bcf577c58c8c0f69c4a5c51a9564
henrylin2008 <henrylin2008@yahoo.com>
2022-12-09 23:18:40 -0800

    Cyclic Sort

--- Coding Patterns/Cycle Sort/Cyclic Sort.py
+++ Coding Patterns/Cycle Sort/Cyclic Sort.py
@@ -37,3 +37,22 @@
 # place them at their correct indices, hence, sorting the whole array.
 #
 # Let’s see this visually with the above-mentioned Example-2:
+
+def cyclic_sort(nums):
+    i = 0
+    while i < len(nums):
+        j = nums[i] - 1
+        if nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]  # swap
+        else:
+            i += 1
+    return nums
+
+
+def main():
+    print(cyclic_sort([3, 1, 5, 4, 2]))
+    print(cyclic_sort([2, 6, 4, 3, 1, 5]))
+    print(cyclic_sort([1, 5, 6, 4, 3, 2]))
+
+
+main()
