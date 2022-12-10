92fabb75ac0b1689371727813c50c8e1060534d1
henrylin2008 <henrylin2008@yahoo.com>
2022-12-09 23:17:59 -0800

    Cyclic Sort

--- Coding Patterns/Cycle Sort/Cyclic Sort.py
+++ Coding Patterns/Cycle Sort/Cyclic Sort.py
@@ -23,3 +23,17 @@
 # Input: [1, 5, 6, 4, 3, 2]
 # Output: [1, 2, 3, 4, 5, 6]
 
+# Solution
+#
+# As we know, the input array contains numbers from the range 1 to n. We can use this fact to devise an efficient way
+# to sort the numbers. Since all numbers are unique, we can try placing each number at its correct place, i.e.,
+# placing 1 at index ‘0’, placing 2 at index ‘1’, and so on.
+#
+# To place a number (or an object in general) at its correct index, we first need to find that number. If we first
+# find a number and then place it at its correct place, it will take us O(N^2), which is not acceptable.
+#
+# Instead, what if we iterate the array one number at a time, and if the current number we are iterating is not at
+# the correct index, we swap it with the number at its correct index. This way, we will go through all numbers and
+# place them at their correct indices, hence, sorting the whole array.
+#
+# Let’s see this visually with the above-mentioned Example-2:
