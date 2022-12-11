12a70f303a7bc2a78cd44b9bfe8d9e749be1e37e
henrylin2008 <henrylin2008@yahoo.com>
2022-12-10 23:41:06 -0800

    Find the Missing Number

--- Coding Patterns/Cycle Sort/Find the Missing Number.py
+++ Coding Patterns/Cycle Sort/Find the Missing Number.py
@@ -34,3 +34,27 @@
 #      problem as we have one extra number due to the larger range. Therefore, before swapping we will check if the
 #      number at index i is within the permissible range i.e., it is less than the length of the input array, if not,
 #      we will skip ahead.
+
+def find_missing_number(nums):
+    i, n = 0, len(nums)
+    while i < n:
+        j = nums[i]
+        if nums[i] < n and nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]  # swap
+        else:
+            i += 1
+
+    # find the first number missing from its index, that will be our required number
+    for i in range(n):
+        if nums[i] != i:
+            return i
+
+    return n
+
+
+def main():
+    print(find_missing_number([4, 0, 3, 1]))
+    print(find_missing_number([8, 3, 5, 2, 4, 6, 0, 1]))
+
+
+main()
