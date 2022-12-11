a473dd13eacb3e736297cbb811d14337318b9383
henrylin2008 <henrylin2008@yahoo.com>
2022-12-10 23:38:34 -0800

    Find the Missing Number

---
+++ Coding Patterns/Cycle Sort/Find the Missing Number.py
@@ -0,0 +1,15 @@
+# Find the Missing Number (easy)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743705616_30Unit
+
+# Problem Statement
+#
+# We are given an array containing n distinct numbers taken from the range 0 to n. Since the array has only n numbers
+# out of the total n+1 numbers, find the missing number.
+#
+# Example 1:
+# Input: [4, 0, 3, 1]
+# Output: 2
+#
+# Example 2:
+# Input: [8, 3, 5, 2, 4, 6, 0, 1]
+# Output: 7
