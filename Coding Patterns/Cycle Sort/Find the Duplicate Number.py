c6234a32cfbe412325a7708db9cea7a809a5eb28
henrylin2008 <henrylin2008@yahoo.com>
2022-12-12 23:45:50 -0800

    Find the Duplicate Number

--- Coding Patterns/Cycle Sort/Find the Duplicate Number.py
+++ Coding Patterns/Cycle Sort/Find the Duplicate Number.py
@@ -25,3 +25,25 @@
 # similar approach, we will try to place each number on its correct index. Since there is only one duplicate,
 # if while swapping the number with its index both the numbers being swapped are same, we have found our duplicate!
 
+def find_duplicate(nums):
+    i = 0
+    while i < len(nums):
+        if nums[i] != i + 1:
+            j = nums[i] - 1
+            if nums[i] != nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]  # swap
+            else:  # we have found the duplicate
+                return nums[i]
+        else:
+            i += 1
+
+    return -1
+
+
+def main():
+    print(find_duplicate([1, 4, 4, 3, 2]))
+    print(find_duplicate([2, 1, 3, 3, 5, 4]))
+    print(find_duplicate([2, 4, 1, 4, 4]))
+
+
+main()
