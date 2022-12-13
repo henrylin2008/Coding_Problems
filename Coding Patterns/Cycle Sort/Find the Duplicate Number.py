70fdf096c07629d605cab102bc535447894838ec
henrylin2008 <henrylin2008@yahoo.com>
2022-12-12 23:46:40 -0800

    Find the Duplicate Number

--- Coding Patterns/Cycle Sort/Find the Duplicate Number.py
+++ Coding Patterns/Cycle Sort/Find the Duplicate Number.py
@@ -54,3 +54,12 @@ main()
 #
 # Space Complexity
 # The algorithm runs in constant space O(1) but modifies the input array.
+
+
+# Similar Problems
+#
+# Problem 1: Can we solve the above problem in O(1)O(1) space and without modifying the input array?
+#
+# Solution: While doing the cyclic sort, we realized that the array will have a cycle due to the duplicate number and
+# that the start of the cycle will always point to the duplicate number. This means that we can use the fast & the
+# slow pointer method to find the duplicate number or the start of the cycle similar to Start of LinkedList Cycle.
