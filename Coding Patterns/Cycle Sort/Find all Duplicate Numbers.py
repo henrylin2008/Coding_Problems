96ec26278860e96bbd927d112f46d40486c327ea
henrylin2008 <henrylin2008@yahoo.com>
2022-12-13 23:14:13 -0800

    Find all Duplicate Numbers

--- Coding Patterns/Cycle Sort/Find all Duplicate Numbers.py
+++ Coding Patterns/Cycle Sort/Find all Duplicate Numbers.py
@@ -13,3 +13,10 @@
 # Example 2:
 # Input: [5, 4, 7, 2, 3, 5, 3]
 # Output: [3, 5]
+
+# Solution
+#
+# This problem follows the Cyclic Sort pattern and shares similarities with Find the Duplicate Number. Following a
+# similar approach, we will place each number at its correct index. After that, we will iterate through the array to
+# find all numbers that are not at the correct indices. All these numbers are duplicates.
+
