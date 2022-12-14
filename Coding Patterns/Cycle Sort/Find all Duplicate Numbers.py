9b4aa5b145c305ce039ddccfe36c4aa4ffc6f2d3
henrylin2008 <henrylin2008@yahoo.com>
2022-12-13 23:13:54 -0800

    Find all Duplicate Numbers

---
+++ Coding Patterns/Cycle Sort/Find all Duplicate Numbers.py
@@ -0,0 +1,15 @@
+# Find all Duplicate Numbers (easy)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743734011_33Unit
+
+# Problem Statement
+#
+# We are given an unsorted array containing n numbers taken from the range 1 to n. The array has some numbers
+# appearing twice, find all these duplicate numbers using constant space.
+#
+# Example 1:
+# Input: [3, 4, 4, 5, 5]
+# Output: [4, 5]
+#
+# Example 2:
+# Input: [5, 4, 7, 2, 3, 5, 3]
+# Output: [3, 5]
