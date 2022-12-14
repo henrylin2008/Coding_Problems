5e245661a673a99f208e1cf0b75f37e522fc3048
henrylin2008 <henrylin2008@yahoo.com>
2022-12-13 23:14:31 -0800

    Find all Duplicate Numbers

--- Coding Patterns/Cycle Sort/Find all Duplicate Numbers.py
+++ Coding Patterns/Cycle Sort/Find all Duplicate Numbers.py
@@ -20,3 +20,27 @@
 # similar approach, we will place each number at its correct index. After that, we will iterate through the array to
 # find all numbers that are not at the correct indices. All these numbers are duplicates.
 
+
+def find_all_duplicates(nums):
+    i = 0
+    while i < len(nums):
+        j = nums[i] - 1
+        if nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]  # swap
+        else:
+            i += 1
+
+    duplicateNumbers = []
+    for i in range(len(nums)):
+        if nums[i] != i + 1:
+            duplicateNumbers.append(nums[i])
+
+    return duplicateNumbers
+
+
+def main():
+    print(find_all_duplicates([3, 4, 4, 5, 5]))
+    print(find_all_duplicates([5, 4, 7, 2, 3, 5, 3]))
+
+
+main()
