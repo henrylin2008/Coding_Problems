8c5f171c0bf6e899fa3de582451f2f7d260ab632
henrylin2008 <henrylin2008@yahoo.com>
2022-12-15 23:49:19 -0800

    Find the Smallest Missing Positive Number

---
+++ Coding Patterns/Cycle Sort/Find the Smallest Missing Positive Number.py
@@ -0,0 +1,24 @@
+# Problem Challenge 2: Find the Smallest Missing Positive Number (medium)
+# https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743750267_35Unit
+
+# Problem Statement
+#
+# Given an unsorted array containing numbers, find the smallest missing positive number in it.
+# Note: Positive numbers start from '1'.
+#
+# Example 1:
+# Input: [-3, 1, 5, 4, 2]
+# Output: 3
+# Explanation: The smallest missing positive number is '3'
+#
+# Example 2:
+# Input: [3, -2, 0, 1, 2]
+# Output: 4
+#
+# Example 3:
+# Input: [3, 2, 5, 1]
+# Output: 4
+#
+# Example 4:
+# Input: [33, 37, 5]
+# Output: 1
