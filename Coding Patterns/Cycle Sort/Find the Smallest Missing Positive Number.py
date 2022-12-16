b7e478cadcb503f3ea378cc63af33ce6977b34aa
henrylin2008 <henrylin2008@yahoo.com>
2022-12-15 23:50:06 -0800

    Find the Smallest Missing Positive Number

--- Coding Patterns/Cycle Sort/Find the Smallest Missing Positive Number.py
+++ Coding Patterns/Cycle Sort/Find the Smallest Missing Positive Number.py
@@ -33,3 +33,26 @@
 # all numbers greater than the length of the array). Once we are done with the cyclic sort we will iterate the array
 # and the first index that does not have the correct number will be the smallest missing positive number!
 
+def find_first_smallest_missing_positive(nums):
+    i, n = 0, len(nums)
+    while i < n:
+        j = nums[i] - 1
+        if nums[i] > 0 and nums[i] <= n and nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]  # swap
+        else:
+            i += 1
+
+    for i in range(n):
+        if nums[i] != i + 1:
+            return i + 1
+
+    return len(nums) + 1
+
+
+def main():
+    print(find_first_smallest_missing_positive([-3, 1, 5, 4, 2]))
+    print(find_first_smallest_missing_positive([3, -2, 0, 1, 2]))
+    print(find_first_smallest_missing_positive([3, 2, 5, 1]))
+
+
+main()
