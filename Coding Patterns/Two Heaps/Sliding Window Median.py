fee159e7e1e49e83fc92a990903cb716816804f1
henrylin2008 <henrylin2008@yahoo.com>
2022-12-18 23:53:37 -0800

    Sliding Window Median

--- Coding Patterns/Two Heaps/Sliding Window Median.py
+++ Coding Patterns/Two Heaps/Sliding Window Median.py
@@ -21,3 +21,13 @@
 #   * [1, 2, -1, 3, 5] -> median is 1.0
 #   * [1, 2, -1, 3, 5] -> median is 2.0
 #   * [1, 2, -1, 3, 5] -> median is 3.0
+
+# Solution
+#
+# This problem follows the Two Heaps pattern and share similarities with Find the Median of a Number Stream. We can
+# follow a similar approach of maintaining a max-heap and a min-heap for the list of numbers to find their median.
+#
+# The only difference is that we need to keep track of a sliding window of ‘k’ numbers. This means,
+# in each iteration, when we insert a new number in the heaps, we need to remove one number from the heaps which is
+# going out of the sliding window. After the removal, we need to rebalance the heaps in the same way that we did
+# while inserting.
