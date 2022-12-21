1f7f4aa56c95e6f94a4499fb574b4f5fff456cd8
henrylin2008 <henrylin2008@yahoo.com>
2022-12-20 23:29:28 -0800

    Next Interval

--- Coding Patterns/Two Heaps/Next Interval.py
+++ Coding Patterns/Two Heaps/Next Interval.py
@@ -20,3 +20,23 @@
 # Output: [2, -1, -1]
 # Explanation: The next interval of [3,4] is [4,6] which has index ‘2’. There is no next interval for [1,5] and [4,6].
 
+# Solution
+#
+# A brute force solution could be to take one interval at a time and go through all the other intervals to find the
+# next interval. This algorithm will take O(N^2) where ‘N’ is the total number of intervals. Can we do
+# better than that?
+#
+# We can utilize the Two Heaps approach. We can push all intervals into two heaps: one heap to sort the intervals on
+# maximum start time (let’s call it maxStartHeap) and the other on maximum end time (let’s call it maxEndHeap). We
+# can then iterate through all intervals of the `maxEndHeap’ to find their next interval. Our algorithm will have the
+# following steps:
+#   1. Take out the top (having highest end) interval from the maxEndHeap to find its next interval. Let’s call this
+#      interval topEnd.
+#   2. Find an interval in the maxStartHeap with the closest start greater than or equal to the start of topEnd. Since
+#      maxStartHeap is sorted by ‘start’ of intervals, it is easy to find the interval with the highest ‘start’. Let’s
+#      call this interval topStart.
+#   3. Add the index of topStart in the result array as the next interval of topEnd. If we can’t find the next interval,
+#      add ‘-1’ in the result array.
+#   4. Put the topStart back in the maxStartHeap, as it could be the next interval of other intervals.
+#   5. Repeat the steps 1-4 until we have no intervals left in maxEndHeap.
+
