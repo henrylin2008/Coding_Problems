a4f0480c527aaebe3c1675dd7208e2d92bb7cd56
henrylin2008 <henrylin2008@yahoo.com>
2022-12-21 23:23:49 -0800

    Number of Islands - BFS

--- Coding Patterns/Islands (Matrix Traversal)/Number of Islands.py
+++ Coding Patterns/Islands (Matrix Traversal)/Number of Islands.py
@@ -130,3 +130,59 @@ main()
 # Space Complexity
 # DFS recursion stack can go M*N deep when the whole matrix is filled with '1's. Hence, the space complexity will be
 # O(M*N), where ‘M’ is the number of rows and 'N' is the number of columns of the input matrix.
+
+
+# Code  (BFS)
+# Here is what our BFS algorithm will look like. We will update the input matrix to mark cells visited.
+from collections import deque
+
+
+def countIslandsBFS(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    totalIslands = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] == 1:  # only if the cell is a land
+                # we have found an island
+                totalIslands += 1
+                visitIslandBFS(matrix, i, j)
+    return totalIslands
+
+
+def visitIslandBFS(matrix,  x,  y):
+    neighbors = deque([(x, y)])
+    while neighbors:
+        row, col = neighbors.popleft()
+
+        if row < 0 or row >= len(matrix) or col < 0 or col >= len(matrix[0]):
+            continue  # continue, if it is not a valid cell
+        if matrix[row][col] == 0:
+            continue  # continue if it is a water cell
+
+        matrix[row][col] = 0  # mark the cell visited by making it a water cell
+
+        # insert all neighboring cells to the queue for BFS
+        neighbors.extend([(row + 1, col)])  # lower cell
+        neighbors.extend([(row - 1, col)])  # upper cell
+        neighbors.extend([(row, col + 1)])  # right cell
+        neighbors.extend([(row, col - 1)])  # left cell
+
+
+def main():
+    print(countIslandsBFS([[0, 1, 1, 1, 0], [0, 0, 0, 1, 1], [
+          0, 1, 1, 1, 0], [0, 1, 1, 0, 0], [0, 0, 0, 0, 0]]))
+    print(countIslandsBFS([[1, 1, 1, 0, 0], [0, 1, 0, 0, 1], [
+          0, 0, 1, 1, 0], [0, 0, 1, 0, 0], [0, 0, 1, 0, 0]]))
+
+
+main()
+
+# Time Complexity
+# Time complexity of the above algorithm will be O(M*N), where ‘M’ is the number of rows and 'N' is the number of
+# columns.
+
+# Space Complexity
+# Space complexity of the above algorithm will be O(min(M,N). In the worst case, when the matrix is completely filled
+# with land cells, the size of the queue can grow up to min(M,N).
